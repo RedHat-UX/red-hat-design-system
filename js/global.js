421baab3d14994c36681a3778c7a3c97b093137d
Wes Ruvalcaba <wesruv@gmail.com>
2020-07-14 14:32:08 -0400

    Adding page transition, polishing menu styles & page styles
    Added page transition to hide component layout thrashing
    Added styles to make page header and page content layout nicer
    Added active styles for menu links

--- js/global.js
+++ js/global.js
@@ -1,3 +1,6 @@
 'use strict';
 
-console.log('it\'s alive!');
+// As soon as the page loads toggle JS class
+var $html = document.querySelector('html');
+$html.classList.remove('no-js');
+$html.classList.add('js');
