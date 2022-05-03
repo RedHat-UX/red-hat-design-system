bae5ca0c9fd45b066ce7720d4b72a4c526d65792
Benny Powers <web@bennypowers.com>
2022-05-03 21:51:11 +0300

    docs: update web-dev-server config

--- web-dev-server.config.js
+++ web-dev-server.config.js
@@ -1,6 +1,12 @@
 import { pfeDevServerConfig } from '@patternfly/pfe-tools/dev-server.js';
 
 export default pfeDevServerConfig({
+  site: {
+    title: 'Red Hat Design System',
+    logoUrl: '/docs/assets/red-hat-digital-design-system.svg',
+    githubUrl: 'https://github.com/redhat-ux/red-hat-design-system/',
+    description: 'Red Hat Design System',
+  },
   litcssOptions: {
     include: /elements\/rh-[\w-]+\/[\w-]+\.css$/,
   },
