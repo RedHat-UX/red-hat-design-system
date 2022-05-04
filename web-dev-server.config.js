232350a878392ad0b5535c0567c924dc1dfb846d
Benny Powers <web@bennypowers.com>
2022-05-04 11:32:54 +0300

    chore: add eslintrc

--- web-dev-server.config.js
+++ web-dev-server.config.js
@@ -1,14 +1,17 @@
 import { pfeDevServerConfig } from '@patternfly/pfe-tools/dev-server.js';
 
+export const litcssOptions = {
+  include: /elements\/rh-[\w-]+\/[\w-]+\.css$/,
+  exclude: /lightdom/,
+};
+
 export default pfeDevServerConfig({
+  litcssOptions,
   site: {
     title: 'Red Hat Design System',
     logoUrl: '/docs/assets/red-hat-digital-design-system.svg',
     githubUrl: 'https://github.com/redhat-ux/red-hat-design-system/',
     description: 'Red Hat Design System',
-  },
-  litcssOptions: {
-    include: /elements\/rh-[\w-]+\/[\w-]+\.css$/,
   },
 });
 
