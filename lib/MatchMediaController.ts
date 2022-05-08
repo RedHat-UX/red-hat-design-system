cd742ddfdba86d21bb4985f7cc3b08f75f5dc01a
Benny Powers <web@bennypowers.com>
2022-05-08 16:05:22 +0300

    fix(lib): remove ResizeObserver from MatchMediaController

--- lib/MatchMediaController.ts
+++ lib/MatchMediaController.ts
@@ -1,13 +1,13 @@
 import type { ReactiveControllerHost, ReactiveController } from 'lit';
 
+import { bound } from '@patternfly/pfe-core/decorators/bound.js';
+
 export class MatchMediaController implements ReactiveController {
   /**
    * The matchMedia result
    * @see https://developer.mozilla.org/en-US/docs/Web/API/Window/matchMedia
    */
-  public matches = false;
-
-  private resizeObserver = new ResizeObserver(this.evaluate.bind(this));
+  public mediaQueryList: MediaQueryList | null = null;
 
   private onChange?(list: MediaQueryList): void
 
@@ -15,35 +15,26 @@ export class MatchMediaController implements ReactiveController {
     /** reference to the host element using this controller */
     public host: ReactiveControllerHost & Element,
     private mediaQuery = '',
-    private options?: { onChange?(list: MediaQueryList): void }
+    options?: {
+      onChange?(list: MediaQueryList): void
+    }
   ) {
     this.host.addController(this);
-    this.mediaQuery = mediaQuery;
+    this.mediaQueryList = matchMedia(mediaQuery);
     this.onChange = options?.onChange;
   }
 
   hostConnected() {
-    if (this.host) {
-      this.resizeObserver.observe(this.host);
-    }
+    this.mediaQueryList?.addEventListener('change', this.evaluate);
   }
 
   hostDisconnected() {
-    if (this.resizeObserver) {
-      this.resizeObserver.disconnect();
-    }
+    this.mediaQueryList?.removeEventListener('change', this.evaluate);
   }
 
-  evaluate() {
-    // use matchMedia to evaluate if the current media query is a match.
-    const value = window.matchMedia(this.mediaQuery);
-    const { matches } = value;
-    // dirty check value to determine to update or not
-    if (this.matches !== matches) {
-      this.matches = matches;
-      // request a render update
-      this.host.requestUpdate();
-      this.onChange?.(value);
-    }
+  /** Requests a render and calls the onChange callback */
+  @bound evaluate() {
+    this.host.requestUpdate();
+    this.onChange?.(this.mediaQueryList ?? matchMedia(this.mediaQuery));
   }
 }
