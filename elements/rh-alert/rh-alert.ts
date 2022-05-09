4eb34119c3dabf1883f33c9376c7345945aa2539
Benny Powers <web@bennypowers.com>
2022-05-09 23:23:16 +0300

    fix(alert)!: change HTML APIs
    - `action-groups` slot -> `actions`
    - `description` slot -> anonymous
    - `title` slot -> header
    
    These are in keeping with the conventions we've developed in PFE
    
    - use plain element selectors (e.g. `header`) when possible
    - use ID selectors when selecting single elements
    - remove the requirement to use specific classes in light DOM
    
    This is in keeping with the proto-style-guide we developed in Google
    Docs for PatternFly 2022
    
    - Prefer h2-6 tags for header, in keeping with semantic HTML

--- elements/rh-alert/rh-alert.ts
+++ elements/rh-alert/rh-alert.ts
@@ -1,25 +1,10 @@
 import { LitElement, html, svg } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 
-import { pfelement, observed } from '@patternfly/pfe-core/decorators.js';
+import { pfelement } from '@patternfly/pfe-core/decorators.js';
 
 import styles from './rh-alert.css';
 
-
-/**
- * An alert to display information on a website.
- *
- * By default it is black text on a black bacground
- *
- * @summary An alert to display information on a website.
- *
- *
- * @slot title - Provide a title for the alert message.
- * @slot description - Provide a description for the alert message
- * @slot action-group - Provide actions that the user can take for the alert
- * @slot text - Optionally override the button's label {@deprecated use `label`}.
- *
- */
 const ICONS = new Map(Object.entries({
   default: svg`<svg style="vertical-align:-0.125em" fill="currentColor" height="1em" width="1em" viewBox="0 0 896 1024" aria-hidden="true" role="img"><path d="M448,0 C465.333333,0 480.333333,6.33333333 493,19 C505.666667,31.6666667 512,46.6666667 512,64 L512,106 L514.23,106.45 C587.89,121.39 648.48,157.24 696,214 C744,271.333333 768,338.666667 768,416 C768,500 780,568.666667 804,622 C818.666667,652.666667 841.333333,684 872,716 C873.773676,718.829136 875.780658,721.505113 878,724 C890,737.333333 896,752.333333 896,769 C896,785.666667 890,800.333333 878,813 C866,825.666667 850.666667,832 832,832 L63.3,832 C44.9533333,831.84 29.8533333,825.506667 18,813 C6,800.333333 0,785.666667 0,769 C0,752.333333 6,737.333333 18,724 L24,716 L25.06,714.9 C55.1933333,683.28 77.5066667,652.313333 92,622 C116,568.666667 128,500 128,416 C128,338.666667 152,271.333333 200,214 C248,156.666667 309.333333,120.666667 384,106 L384,63.31 C384.166667,46.27 390.5,31.5 403,19 C415.666667,6.33333333 430.666667,0 448,0 Z M576,896 L576,897.08 C575.74,932.6 563.073333,962.573333 538,987 C512.666667,1011.66667 482.666667,1024 448,1024 C413.333333,1024 383.333333,1011.66667 358,987 C332.666667,962.333333 320,932 320,896 L576,896 Z"></path></svg>`,
   error: svg`<svg style="vertical-align:-0.125em" fill="currentColor" height="1em" width="1em" viewBox="0 0 512 512" aria-hidden="true" role="img"><path d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 110c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z"></path></svg>`,
@@ -30,8 +15,17 @@ const ICONS = new Map(Object.entries({
 }));
 
 /**
- * Alert
- * @slot - Place element content here
+ * An alert to display information on a website.
+ *
+ * By default it is black text on a black background
+ *
+ * @summary An alert to display information on a website.
+ *
+ *
+ * @slot         - Provide a description for the alert message
+ * @slot header  - Provide a header for the alert message.
+ * @slot actions - Provide actions that the user can take for the alert
+ *
  */
 @customElement('rh-alert') @pfelement()
 export class RhAlert extends LitElement {
@@ -39,46 +33,37 @@ export class RhAlert extends LitElement {
 
   static readonly styles = styles;
 
-  get icon() {
+  private get icon() {
     return ICONS.get(this.state) ?? ``;
   }
 
-  @observed
-  @property({ reflect: true }) state: 'default'|'error'|'success'|'warning'|'danger'|'info' = 'default';
+  @property({ reflect: true }) state: 'default' | 'error' | 'success' | 'warning' | 'danger' | 'info' = 'default';
 
-  @observed
   @property({ reflect: true, type: Boolean }) variant = false;
 
-  @observed
   @property({ reflect: true, type: Boolean }) toast = false;
 
   render() {
     return html`
-      <div class="rh-alert__container" role="alert" aria-hidden="false">
-        <div class="rh-alert__left-column">
-            <div class="rh-alert__icon">
-                ${this.icon}
-            </div>
+      <div id="container" role="alert" aria-hidden="false">
+        <div id="left-column">
+          <div id="icon">${this.icon}</div>
         </div>
-        <div class="rh-alert__middle-column">
-            <div class="rh-alert__header">
-                <div class="rh-alert__title">
-                    <slot name="title"></slot>
-                </div>
-                <div class="rh-alert__header-actions">
-                    <button id="close-button" class="rh-alert__close-button" type="button" aria-label="Close" confirm>
-                        <svg style="vertical-align:-0.125em" fill="currentColor" height="1em" width="1em" viewBox="0 0 352 512" aria-hidden="true" role="img"><path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg>
-                    </button>
-                </div>
+        <div id="middle-column">
+          <header>
+            <div id="header"><slot name="header"></slot></div>
+            <div id="header-actions">
+              <button id="close-button" aria-label="Close" confirm>
+                <svg style="vertical-align:-0.125em" fill="currentColor" height="1em" width="1em" viewBox="0 0 352 512" aria-hidden="true" role="img">
+                  <path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path>
+                </svg>
+              </button>
             </div>
-            <div class="rh-alert__description">
-                <slot name="description"></slot>
-            </div>
-            <div class="rh-alert__action-group">
-                <slot id="action-group" name="action-group"></slot>
-            </div>
+          </header>
+          <div id="description"><slot></slot></div>
+          <footer><slot name="actions"></slot></footer>
         </div>
-    </div>
+      </div>
     `;
   }
 }
@@ -88,3 +73,4 @@ declare global {
     'rh-alert': RhAlert;
   }
 }
+
