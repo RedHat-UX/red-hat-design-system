54764ef956f2011b3d9931c5b3a8e42846f11d74
Benny Powers <web@bennypowers.com>
2022-05-09 18:36:57 +0300

    chore: add build script

--- scripts/build.js
+++ scripts/build.js
@@ -1,2 +1,50 @@
 #!/usr/bin/env node
-console.log('TODO: implement a build script, with pfe-tools');
+import { readdir } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+
+import { build } from 'esbuild';
+import { minifyHTMLLiteralsPlugin } from 'esbuild-plugin-minify-html-literals';
+import { litCssPlugin } from 'esbuild-plugin-lit-css';
+
+const elements = await readdir(new URL('../elements', import.meta.url));
+
+const entryPoints = elements.map(x =>
+  fileURLToPath(new URL(`../elements/${x}/${x}.ts`, import.meta.url)));
+
+await build({
+  entryPoints,
+  outfile: 'rhds.min.js',
+  format: 'esm',
+  minify: true,
+  bundle: true,
+  legalComments: 'linked',
+  sourcemap: 'linked',
+  plugins: [
+    minifyHTMLLiteralsPlugin(),
+    litCssPlugin({
+      include: /elements\/rh-(.*)\/(.*)\.css$/,
+      uglify: true,
+    }),
+  ],
+});
+
+await build({
+  entryPoints,
+  outdir: 'elements',
+  outbase: 'elements',
+  entryNames: '[dir]/[name]',
+  bundle: true,
+  external: ['@*', 'prism*', 'lit*', '@patternfly/*', 'tslib'],
+  format: 'esm',
+  sourcemap: 'linked',
+  minify: true,
+  legalComments: 'linked',
+  plugins: [
+    minifyHTMLLiteralsPlugin(),
+    litCssPlugin({
+      include: /elements\/rh-(.*)\/(.*)\.css$/,
+      uglify: true,
+    }),
+  ],
+});
+
