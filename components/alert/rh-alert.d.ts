2934ad468541005fd059dcd3d3e47060d4d70945
bennypowers <bennypowers@users.noreply.github.com>
2022-05-10 15:15:23 +0000

    deploy: 79832c9affa9737a6dff0191d6b5ad25fca90e6c

--- components/alert/rh-alert.d.ts
+++ components/alert/rh-alert.d.ts
@@ -1,12 +1,21 @@
 import { LitElement } from 'lit';
 /**
- * Alert
- * @slot - Place element content here
+ * An alert to display information on a website.
+ *
+ * By default it is black text on a black background
+ *
+ * @summary An alert to display information on a website.
+ *
+ *
+ * @slot         - Provide a description for the alert message
+ * @slot header  - Provide a header for the alert message.
+ * @slot actions - Provide actions that the user can take for the alert
+ *
  */
 export declare class RhAlert extends LitElement {
     static readonly version = "{{version}}";
     static readonly styles: import("lit").CSSResult;
-    get icon(): import("lit-html").TemplateResult<2> | "";
+    private get icon();
     state: 'default' | 'error' | 'success' | 'warning' | 'danger' | 'info';
     variant: boolean;
     toast: boolean;
