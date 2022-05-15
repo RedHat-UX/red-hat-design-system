cbe572cbc1750a97519cfc76c32a545c543d9fec
bennypowers <bennypowers@users.noreply.github.com>
2022-05-15 08:11:29 +0000

    deploy: edc74c1f7cba4e4321366ef5ec1dbf7281f55ae8

--- components/alert/demo/rh-alert.js
+++
@@ -1,7 +0,0 @@
-import '@patternfly/pfe-band';
-import '@rhds/elements/rh-alert/rh-alert.js';
-
-const root = document.querySelector('[data-demo="rh-alert"]')?.shadowRoot ?? document;
-
-root.querySelector('rh-alert');
-
