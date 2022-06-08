d27cd2851b8c8fc050c489e7752ed07f8992160c
Em Nouveau <enouveau@redhat.com>
2022-06-08 14:28:07 -0700

    fix: text style bugs

--- elements/rh-blockquote/rh-blockquote.ts
+++ elements/rh-blockquote/rh-blockquote.ts
@@ -5,11 +5,6 @@ import { pfelement } from '@patternfly/pfe-core/decorators.js';
 
 import styles from './rh-blockquote.css';
 
-const quote = new URL('../../assets/rh-quotemark-open.svg', import.meta.url).href;
-const text = "In open source, we feel strongly that to really do something well, you have to get a lot of people involved.";
-const author = "Linus Torvalds";
-
-
 /**
  * A blockquote for displaying quote, author, and author title.
  *
@@ -17,7 +12,7 @@ const author = "Linus Torvalds";
  * @summary  A blockquote for displaying quote, author, and author title.
  *
  *
- * @slot         - Provide a quote for the blockquote
+ * @slot quote   - Provide a quote for the blockquote
  * @slot author  - Provide an author for the blockquote
  * @slot title   - Provide an author title for the blockquote
  *
@@ -45,15 +40,15 @@ export class RhBlockquote extends LitElement {
   @property({ reflect: true, attribute: 'align' })
     align: 'center'|'left' = 'left';
 
-  /** Optional boolean attribute that, when present, shows a highlight on side of blockquote. */
-  @property({ type: Boolean, attribute: 'highlight', reflect: true })
-    highlight = false;
+  /** Optional highlight attribute that, when present, shows a highlight on side of blockquote. */
+  @property({ reflect: true, attribute: 'highlight' })
+    highlight: 'none'|'left' = 'none';
 
   render() {
     const { align, highlight, theme } = this;
     return html`
       <figure id="container">
-        <svg style="vertical-align:-0.125em" fill="#e00" height="1em" width="1.25em" aria-hidden="true" role="img">
+        <svg fill="#e00" height="1.5em" width="1.75em" aria-hidden="true" role="img">
           <g>
             <path class="bf58b4e1-dd3d-459d-a217-877551437f03" d="M10,15.38H5.63A5.9,5.9,0,0,1,12,9.64a.61.61,0,0,0,.67-.57c.05-.63-.63-.73-1.12-.69A7.12,7.12,0,0,0,4.38,15.5c0,.68,0,10.54,0,11.5a.61.61,0,0,0,.62.62H16a.61.61,0,0,0,.62-.62V22A6.63,6.63,0,0,0,10,15.38Zm5.38,11H5.62V16.62H10A5.39,5.39,0,0,1,15.38,22Z"/>
             <path class="bf58b4e1-dd3d-459d-a217-877551437f03" d="M25,15.38H20.63A5.9,5.9,0,0,1,27,9.64a.62.62,0,0,0,.67-.57c.05-.63-.63-.73-1.12-.69a7.12,7.12,0,0,0-7.12,7.12c0,.68,0,10.54,0,11.5a.61.61,0,0,0,.62.62H31a.61.61,0,0,0,.62-.62V22A6.63,6.63,0,0,0,25,15.38Zm5.38,11H20.62V16.62H25A5.39,5.39,0,0,1,30.38,22Z"/>
@@ -63,8 +58,8 @@ export class RhBlockquote extends LitElement {
           <slot name="quote"></slot>
         </blockquote>
         <figcaption>
-          <span id="author"><slot name="author"></slot></span>,
-          <span id="title"><slot name="title"></slot></span>
+          <p id="author"><slot name="author"></slot></p>
+          <p id="title"><slot name="title"></slot></p>
         </figcaption>
       </figure>
     `;
