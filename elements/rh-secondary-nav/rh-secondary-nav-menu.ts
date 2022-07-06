ccc08ab9e59e27d5fbabb6eb9ade7e967bae9191
Steven Spriggs <steven.spriggs@gmail.com>
2022-07-06 11:06:25 -0400

    refactor: remove menu level CTA api

--- elements/rh-secondary-nav/rh-secondary-nav-menu.ts
+++ elements/rh-secondary-nav/rh-secondary-nav-menu.ts
@@ -29,11 +29,6 @@ export class RhSecondaryNavMenu extends LitElement {
 
   @property({ reflect: true }) layout: 'fixed-width' | 'full-width' = 'full-width';
 
-  @queryAssignedNodes('cta', true)
-  private _ctaNodes!: NodeListOf<HTMLElement>;
-
-  @query('#cta') _cta?: HTMLElement;
-
   @query('#container') _container?: HTMLElement;
 
   #screenSize = new RHDSScreenSizeController(this);
@@ -45,12 +40,6 @@ export class RhSecondaryNavMenu extends LitElement {
    */
   @observed
   @state() private _isMobile = false;
-
-  /**
-   * `_hasCtaNodes` property is true when the cta slot has children. If true
-   * the class of `visible` is added via a class map on render
-   */
-  @state() private _hasCtaNodes = false;
 
   /**
    * `visible` property is false initially then when a dropdown is clicked is toggled
@@ -74,16 +63,12 @@ export class RhSecondaryNavMenu extends LitElement {
 
   render() {
     const classes = { 'compact': !this.#screenSize.matches.has('tabletLandscape'), 'visible': this.visible };
-    const ctaClasses = { 'visible': this._hasCtaNodes };
 
     return html`
       <div id="container" class="${classMap(classes)}">${this.layout === 'full-width' ? html`
         <div id="full-width" part="full-width">
           <div id="sections" part="sections">
             <slot></slot>
-          </div>
-          <div id="cta" part="cta" class="${classMap(ctaClasses)}">
-            <slot name="cta" @slotchange="${this.#onCtaSlotChange}"></slot>
           </div>
         </div>` : html`
         <div id="fixed-width" part="fixed-width">
@@ -93,15 +78,6 @@ export class RhSecondaryNavMenu extends LitElement {
         </div>`}
       </div>
     `;
-  }
-
-  /**
-   * When CTA slot is changed, check to see if it has children
-   * if it does then remove hidden attributes
-   * @returns {void}
-   */
-  #onCtaSlotChange(): void {
-    this._hasCtaNodes = this._ctaNodes.length !== 0;
   }
 }
 
