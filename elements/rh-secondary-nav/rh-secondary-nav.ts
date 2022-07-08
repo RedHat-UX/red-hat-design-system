b4e634bf18048faac877456bcb5bcf5e43c022c7
Steven Spriggs <steven.spriggs@gmail.com>
2022-07-08 10:15:15 -0400

    fix: missed a rename of screensize import

--- elements/rh-secondary-nav/rh-secondary-nav.ts
+++ elements/rh-secondary-nav/rh-secondary-nav.ts
@@ -14,7 +14,7 @@ import type { RhSecondaryNavOverlay } from './rh-secondary-nav-overlay.js';
 import { SecondaryNavOverlayChangeEvent } from './rh-secondary-nav-overlay.js';
 import { RhSecondaryNavDropdown, SecondaryNavDropdownExpandEvent } from './rh-secondary-nav-dropdown.js';
 
-import { RHDSScreenSizeController } from '../../lib/ScreenSizeController.js';
+import { ScreenSizeController } from '../../lib/ScreenSizeController.js';
 
 import styles from './rh-secondary-nav.css';
 
