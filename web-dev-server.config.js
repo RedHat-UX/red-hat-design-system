0573466034342e5991856aebe9b2e8d8c68f5610
Benny Powers <bennypowers@users.noreply.github.com>
2022-08-01 16:10:39 +0300

    docs: create minimal theme sheet for demos (#447)
    * docs: create minimal theme sheet for demos
    
    * docs: add a basic demos stylesheet, load lightdom css
    
    * docs: load demo styles in live site
    
    * chore: update package lock
    
    * chore: update dependencies

--- web-dev-server.config.js
+++ web-dev-server.config.js
@@ -1,3 +1,4 @@
+// @ts-check
 import { pfeDevServerConfig } from '@patternfly/pfe-tools/dev-server.js';
 
 export const litcssOptions = {
@@ -11,11 +12,25 @@ export default pfeDevServerConfig({
   demoURLPrefix: 'https://ux.redhat.com/',
   tagPrefix: 'rh',
   site: {
-    tagPrefix: 'rh-',
     title: 'Red Hat Design System',
     logoUrl: '/docs/assets/logo-red-hat.svg',
-    githubUrl: 'https://github.com/redhat-ux/red-hat-design-system/',
+    favicon: '/docs/assets/logo-red-hat.svg',
     description: 'Red Hat Design System',
+    stylesheets: [
+      '/docs/assets/base.css',
+      '/docs/assets/demos.css',
+    ]
   },
+  middleware: [
+    /** redirect requests for lightdom css to /elements */
+    function(ctx, next) {
+      const match = ctx.path.match(/^\/components\/(?<slug>[-\w]+)\/(?<path>.*)\.css$/);
+      if (match) {
+        const { slug, path } = match.groups;
+        ctx.redirect(`/elements/rh-${slug}/${path}.css`);
+      }
+      return next();
+    }
+  ]
 });
 
