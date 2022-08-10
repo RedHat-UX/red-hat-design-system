f0162c4ed6e49f1ba3aec26c53bdcb3b587ac457
Benny Powers <web@bennypowers.com>
2022-08-10 12:32:10 +0300

    test(cta): visual regression and a11y contrast

--- elements/rh-cta/test/rh-cta.e2e.ts
+++ elements/rh-cta/test/rh-cta.e2e.ts
@@ -6,7 +6,56 @@ const tagName = 'rh-cta';
 test.describe(tagName, () => {
   test('snapshot', async ({ page }) => {
     const componentPage = new PfeDemoPage(page, tagName);
-    await componentPage.navigate();
+    await componentPage.navigate('/components/cta/demo/');
     await componentPage.snapshot();
+
+    async function elementSuite(selector: string, variant: string) {
+      const element = await componentPage.page.$(selector);
+      test.expect(await element.screenshot()).toMatchSnapshot({ name: `${variant} 0` });
+
+      // hover
+      await element.hover();
+      await componentPage.updateComplete(selector);
+      test.expect(await element.screenshot()).toMatchSnapshot({ name: `${variant} 1 hover` });
+      await page.mouse.click(0, 0);
+      // focus
+      await page.focus(`${selector} *`);
+      await componentPage.updateComplete(selector);
+      test.expect(await element.screenshot()).toMatchSnapshot({ name: `${variant} 2 focus` });
+      await page.mouse.click(0, 0);
+      // active
+      const { x, width, y, height } = await element.boundingBox();
+      await page.mouse.move(x + width / 2, y + height / 2);
+      await page.mouse.down();
+      await componentPage.updateComplete(selector);
+      test.expect(await element.screenshot()).toMatchSnapshot({ name: `${variant} 3 active` });
+      await page.mouse.up();
+      await page.mouse.click(0, 0);
+    }
+
+    for (const selector of Array.from({ length: 7 }, (_, i) => `rh-cta:nth-of-type(${i + 1})`)) {
+      const [, nth] = selector.match(/nth-of-type\((\d)\)/);
+      await elementSuite(selector, [
+        'default',
+        'default video',
+        'primary',
+        'primary video',
+        'secondary',
+        'brick',
+        'brick video',
+      ][parseInt(nth) - 1]);
+    }
+    for (const selector of Array.from({ length: 7 }, (_, i) => `shadow-root rh-cta:nth-of-type(${i + 1})`)) {
+      const [, nth] = selector.match(/nth-of-type\((\d)\)/);
+      await elementSuite(selector, [
+        'shadow rtl default',
+        'shadow rtl default video',
+        'shadow rtl primary',
+        'shadow rtl primary video',
+        'shadow rtl secondary',
+        'shadow rtl brick',
+        'shadow rtl brick video',
+      ][parseInt(nth) - 1]);
+    }
   });
 });
--- elements/rh-cta/test/rh-cta.spec.ts
+++ elements/rh-cta/test/rh-cta.spec.ts
@@ -2,17 +2,35 @@ import { expect, html } from '@open-wc/testing';
 import { createFixture } from '@patternfly/pfe-tools/test/create-fixture.js';
 import { RhCta } from '../rh-cta.js';
 
-const template = html`
-  <rh-cta></rh-cta>
-`;
-
 describe('<rh-cta>', function() {
   it('should upgrade', async function() {
-    const element = await createFixture<RhCta>(template);
+    const element = await createFixture<RhCta>(html`<rh-cta></rh-cta>`);
     const klass = customElements.get('rh-cta');
     expect(element)
       .to.be.an.instanceOf(klass)
       .and
       .to.be.an.instanceOf(RhCta);
+  });
+
+  describe('on default context', function() {
+    let element: RhCta;
+    describe('default variant', function() {
+      beforeEach(async function() {
+        element = await createFixture<RhCta>(html`<rh-cta></rh-cta>`);
+      });
+      it('is accessible', function() {
+        expect(element).to.be.accessible();
+      });
+    });
+    for (const variant of ['primary', 'secondary', 'brick']) {
+      describe(`${variant} variant`, function() {
+        beforeEach(async function() {
+          element = await createFixture<RhCta>(html`<rh-cta variant="${variant}"></rh-cta>`);
+        });
+        it('is accessible', function() {
+          expect(element).to.be.accessible();
+        });
+      });
+    }
   });
 });
