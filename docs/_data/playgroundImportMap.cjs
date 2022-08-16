8b751284a2b97c25863853a845ac137c8910bad0
Benny Powers <web@bennypowers.com>
2022-08-16 16:11:13 +0300

    docs: debug playground

--- docs/_data/playgroundImportMap.cjs
+++ docs/_data/playgroundImportMap.cjs
@@ -3,18 +3,19 @@ module.exports = async function() {
   const { Generator } = await import('@jspm/generator');
 
   const generator = new Generator({
+    // defaultProvider: 'jspm',
     defaultProvider: 'unpkg', // this is the default defaultProvider
     env: ['production', 'browser', 'module'],
   });
 
   await generator.install([
-    'tslib',
-    'lit',
-    '@patternfly/pfe-accordion@next',
-    '@patternfly/pfe-band@next',
-    '@patternfly/pfe-button@next',
-    '@patternfly/pfe-cta@next',
-    '@patternfly/pfe-icon@next',
+    // 'tslib',
+    // 'lit',
+    // '@patternfly/pfe-accordion@next',
+    // '@patternfly/pfe-band@next',
+    // '@patternfly/pfe-button@next',
+    // '@patternfly/pfe-cta@next',
+    // '@patternfly/pfe-icon@next',
     { target: '@rhds/elements', subpaths: [
       './rh-footer/rh-footer.js',
     ] },
@@ -26,6 +27,7 @@ module.exports = async function() {
     return [
       k,
         v.startsWith('https://unpkg.com') ? `${v}?module`
+      // : v.startsWith('./elements') ? `https://ga.jspm.io/npm:@rhds/elements@1.0.0-beta.15/${v.replace('./elements/', 'elements/')}`
       : v.startsWith('./elements') ? `https://unpkg.com/@rhds/elements@1.0.0-beta.15/${v.replace('./elements/', 'elements/')}?module`
       : v
     ];
