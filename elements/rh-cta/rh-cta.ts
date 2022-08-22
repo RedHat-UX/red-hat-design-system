5051cadfcdc600876df49cba90efde02c4ece02c
Benny Powers <web@bennypowers.com>
2022-08-22 18:05:56 +0300

    docs(cta): remove deprecated event

--- elements/rh-cta/rh-cta.ts
+++ elements/rh-cta/rh-cta.ts
@@ -42,8 +42,6 @@ function isButton(element: Element): element is HTMLButtonElement {
  *
  * @slot - We expect an anchor tag, `<a>` with an `href`, to be the first child inside `rh-cta` element. Less preferred but allowed for specific use-cases include: `<button>` (note however that the `button` tag is not supported for the default CTA styles).
  *
- * @fires {CtaSelectEvent} select - This event is fired when a link is clicked and serves as a way to capture click events if necessary.
- *
  * @csspart container - container element for slotted CTA
  *
  */
