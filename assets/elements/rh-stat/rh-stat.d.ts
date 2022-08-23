d6de8d9ee83478bb1c5b35f20a42971400e94ec8
bennypowers <bennypowers@users.noreply.github.com>
2022-08-23 07:23:07 +0000

    deploy: 1f196b58cd204df0b9f4157a6f556dfb2e1f4a76

--- assets/elements/rh-stat/rh-stat.d.ts
+++ assets/elements/rh-stat/rh-stat.d.ts
@@ -1,24 +1,28 @@
 import type { ColorTheme } from '../../lib/context/color.js';
 import { LitElement } from 'lit';
-import { MatchMediaController } from '../../lib/MatchMediaController.js';
 /**
- * Stat
- * @slot - Place element content here
+ * A statistic showcases a data point or quick fact in a way that visually stands out.
+ * It consists of a number/percentage and body text in its simplest form.
+ * It can also include an icon, title, and a call to action.
+ *
+ * @slot icon - Optional icon
+ * @slot title - Statistic title
+ * @slot statistic - Statistic data
+ * @slot cta - Call to action
+ * @slot - Description of the stat
+ *
  */
 export declare class RhStat extends LitElement {
+    #private;
     static readonly version = "{{version}}";
     static readonly styles: import("lit").CSSResult[];
     on: ColorTheme;
-    icon: string;
+    icon?: string;
     top: 'default' | 'statistic';
     size: 'default' | 'large';
-    protected matchMedia: MatchMediaController;
     isMobile: boolean;
-    private slots;
-    constructor();
     connectedCallback(): void;
     render(): import("lit-html").TemplateResult<1>;
-    updateIcons(): void;
 }
 declare global {
     interface HTMLElementTagNameMap {
--- assets/elements/rh-stat/rh-stat.js
+++ assets/elements/rh-stat/rh-stat.js
@@ -1,55 +1,74 @@
-import { __decorate } from "tslib";
+var _RhStat_instances, _RhStat_screenSize, _RhStat_slots, _RhStat_mo, _RhStat_logger, _RhStat_updateIcons, _RhStat_onMutation;
+import { __classPrivateFieldGet, __decorate } from "tslib";
 import { LitElement, html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { classMap } from 'lit/directives/class-map.js';
-import { colorContextConsumer } from '../../lib/context/color.js';
+import { colorContextConsumer } from '@patternfly/pfe-core/decorators.js';
 import { SlotController } from '@patternfly/pfe-core/controllers/slot-controller.js';
+import { Logger } from '@patternfly/pfe-core/controllers/logger.js';
+import { ScreenSizeController } from '../../lib/ScreenSizeController.js';
 import styles from "./rh-stat.css.js";
-import { tabletLandscapeBreakpoint } from '../../lib/tokens.js';
-import { MatchMediaController } from '../../lib/MatchMediaController.js';
 /**
- * Stat
- * @slot - Place element content here
+ * A statistic showcases a data point or quick fact in a way that visually stands out.
+ * It consists of a number/percentage and body text in its simplest form.
+ * It can also include an icon, title, and a call to action.
+ *
+ * @slot icon - Optional icon
+ * @slot title - Statistic title
+ * @slot statistic - Statistic data
+ * @slot cta - Call to action
+ * @slot - Description of the stat
+ *
  */
 let RhStat = class RhStat extends LitElement {
     constructor() {
-        super();
+        super(...arguments);
+        _RhStat_instances.add(this);
         this.on = 'light';
-        this.matchMedia = new MatchMediaController(this, `(max-width: ${tabletLandscapeBreakpoint})`, {
-            onChange: ({ matches }) => this.isMobile = matches,
-        });
-        this.isMobile = false;
-        this.slots = new SlotController(this, {
-            slots: ['icon'],
-        });
-        this.icon = '';
         this.top = 'default';
         this.size = 'default';
+        this.isMobile = false;
+        _RhStat_screenSize.set(this, new ScreenSizeController(this));
+        _RhStat_slots.set(this, new SlotController(this, null, 'icon', 'title', 'statistic', 'cta'));
+        _RhStat_mo.set(this, new MutationObserver(() => __classPrivateFieldGet(this, _RhStat_instances, "m", _RhStat_onMutation).call(this)));
+        _RhStat_logger.set(this, new Logger(this));
     }
     connectedCallback() {
         super.connectedCallback();
-        this.updateIcons();
+        __classPrivateFieldGet(this, _RhStat_instances, "m", _RhStat_updateIcons).call(this);
+        __classPrivateFieldGet(this, _RhStat_mo, "f").observe(this, { childList: true });
+        __classPrivateFieldGet(this, _RhStat_instances, "m", _RhStat_onMutation).call(this);
     }
     render() {
-        const hasSlottedIcon = this.slots.hasSlotted('icon');
+        const hasIcon = __classPrivateFieldGet(this, _RhStat_slots, "f").hasSlotted('icon') || !!this.icon;
+        const hasTitle = __classPrivateFieldGet(this, _RhStat_slots, "f").hasSlotted('title');
+        const hasStatistic = __classPrivateFieldGet(this, _RhStat_slots, "f").hasSlotted('statistic');
+        const hasCta = __classPrivateFieldGet(this, _RhStat_slots, "f").hasSlotted('cta');
+        const isMobile = !__classPrivateFieldGet(this, _RhStat_screenSize, "f").matches.has('tabletPortrait');
         return html `
-          <slot class="${classMap({ hasIcon: hasSlottedIcon || this.icon.length > 0 })}" name="icon">
-            ${this.icon.length > 0 ?
-            html `
-                <pfe-icon size=${this.size === 'default' ? 'md' : 'lg'} icon=${this.icon}></pfe-icon>
-              ` : html ``}
-            </slot>
-          <slot name="title"></slot>
-          <slot name="statistic">Statistic Placeholder</slot>
-          <slot name="description">Description Placeholder</slot>
-          <slot name="cta"></slot>
+      <div class="${classMap({ isMobile, hasIcon, hasTitle, hasStatistic, hasCta })}">
+        <span id="icon">
+          <slot name="icon" @slotchange="${__classPrivateFieldGet(this, _RhStat_instances, "m", _RhStat_updateIcons)}">${!this.icon ? '' : html `
+            <pfe-icon size=${this.size === 'default' ? 'md' : 'lg'} icon=${this.icon}></pfe-icon>`}
+          </slot>
+        </span>
+        <span id="title"><slot name="title"></slot></span>
+        <span id="statistic"><slot name="statistic"></slot></span>
+        <span id="content"><slot></slot></span>
+        <span id="cta"><slot name="cta"></slot></span>
+      </div>
     `;
     }
-    updateIcons() {
-        if (this.querySelectorAll('pfe-icon')?.length > 0) {
-            const pfeIcon = this.querySelectorAll('pfe-icon')?.[0];
-            pfeIcon.setAttribute('size', this.size === 'default' ? 'md' : 'lg');
-        }
+};
+_RhStat_screenSize = new WeakMap(), _RhStat_slots = new WeakMap(), _RhStat_mo = new WeakMap(), _RhStat_logger = new WeakMap(), _RhStat_instances = new WeakSet(), _RhStat_updateIcons = function _RhStat_updateIcons() {
+    this.querySelector('pfe-icon[slot="icon"]')
+        ?.setAttribute?.('size', this.size === 'default' ? 'md' : 'lg');
+}, _RhStat_onMutation = function _RhStat_onMutation() {
+    if (!__classPrivateFieldGet(this, _RhStat_slots, "f").hasSlotted('stat')) {
+        __classPrivateFieldGet(this, _RhStat_logger, "f").warn('Must contain stat content');
+    }
+    if (!this.querySelectorAll(':not([slot])').length) {
+        __classPrivateFieldGet(this, _RhStat_logger, "f").warn('Must contain description content');
     }
 };
 RhStat.version = '{{version}}';
