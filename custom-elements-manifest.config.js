ccb37ae3dc9125c2509e4fad2fcad9887ade6731
Benny Powers <bennypowers@users.noreply.github.com>
2022-08-26 16:47:21 +0300

    chore: use pfe.config.json (#495)
    * chore: use pfe.config.json
    
    * chore: update deps

--- custom-elements-manifest.config.js
+++ custom-elements-manifest.config.js
@@ -3,7 +3,5 @@ import { pfeCustomElementsManifestConfig } from '@patternfly/pfe-tools/custom-el
 export default pfeCustomElementsManifestConfig({
   globs: ['elements/*/rh-*.ts', 'elements/*/Rh*.ts'],
   exclude: ['*.css.js'],
-  demoURLPrefix: 'https://ux.redhat.com/',
-  sourceControlURLPrefix: 'https://github.com/redhat-ux/red-hat-design-system/tree/main/',
 });
 
