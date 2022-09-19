a989125ac547dad70715e29aedce0f5bf9b9725f
Benny Powers <web@bennypowers.com>
2022-09-19 12:11:14 +0300

    docs(pagination): update demo

--- elements/rh-pagination/demo/rh-pagination.js
+++ elements/rh-pagination/demo/rh-pagination.js
@@ -1,15 +1,12 @@
 import '@rhds/elements/rh-pagination/rh-pagination.js';
-import '@patternfly/pfe-band';
 
-const root = document.querySelector('[data-demo="rh-pagination"]')?.shadowRoot ?? document;
-
-for (const element of root.querySelectorAll('rh-pagination')) {
+for (const element of document.querySelectorAll('rh-pagination')) {
   element.addEventListener('click', event => {
     const link = event.composedPath().find(x => x instanceof HTMLAnchorElement);
     if (link) {
       event.preventDefault();
       history.pushState(null, link.innerText, link.href);
-      element.querySelector('[aria-current]').removeAttribute('aria-current');
+      element.querySelector('[aria-current]')?.removeAttribute?.('aria-current');
       link.setAttribute('aria-current', 'page');
       element.requestUpdate();
     }
