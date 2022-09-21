be8d0f2fca8f67337ad8b46be58cdd13a74ce7d5
Benny Powers <web@bennypowers.com>
2022-09-21 15:49:41 +0300

    docs(pagination): update docs

--- elements/rh-pagination/demo/rh-pagination.js
+++ elements/rh-pagination/demo/rh-pagination.js
@@ -1,14 +1,31 @@
 import '@rhds/elements/rh-pagination/rh-pagination.js';
+// TODO: rh-button
+import '@patternfly/pfe-button';
 
-for (const element of document.querySelectorAll('rh-pagination')) {
+const $ = s => document.querySelector(s);
+const $$ = s => document.querySelectorAll(s);
+
+for (const element of $$('rh-pagination')) {
   element.addEventListener('click', event => {
     const link = event.composedPath().find(x => x instanceof HTMLAnchorElement);
     if (link) {
       event.preventDefault();
       history.pushState(null, link.innerText, link.href);
-      element.querySelector('[aria-current]')?.removeAttribute?.('aria-current');
-      link.setAttribute('aria-current', 'page');
       element.requestUpdate();
     }
   });
 }
+
+$('#add')?.addEventListener('click', function() {
+  const link = document.createElement('a');
+  const item = document.createElement('li');
+  item.append(link);
+  const i = $$('rh-pagination li').length + 1;
+  link.href = `#${i}`;
+  link.textContent = i;
+  $('rh-pagination ol').append(item);
+});
+
+$('#remove')?.addEventListener('click', function() {
+  $('rh-pagination li:last-child')?.remove?.();
+});
--- elements/rh-pagination/docs/index.md
+++ elements/rh-pagination/docs/index.md
@@ -1,5 +1,28 @@
 {% renderOverview %}
-  <rh-pagination></rh-pagination>
+  <rh-pagination>
+    <ol>
+      <li><a href="#">1</a></li>
+      <li><a href="#2">2</a></li>
+      <li><a href="#3">3</a></li>
+      <li><a href="#4">4</a></li>
+      <li><a href="#5">5</a></li>
+      <li><a href="#6">6</a></li>
+      <li><a href="#7">7</a></li>
+      <li><a href="#8">8</a></li>
+      <li><a href="#9">9</a></li>
+      <li><a href="#10">10</a></li>
+      <li><a href="#11">11</a></li>
+      <li><a href="#12">12</a></li>
+      <li><a href="#13">13</a></li>
+      <li><a href="#14">14</a></li>
+      <li><a href="#15">15</a></li>
+      <li><a href="#16">16</a></li>
+      <li><a href="#17">17</a></li>
+      <li><a href="#18">18</a></li>
+      <li><a href="#19">19</a></li>
+      <li><a href="#20">20</a></li>
+    </ol>
+  </rh-pagination>
 {% endrenderOverview %}
 
 {% band header="Usage" %}{% endband %}
