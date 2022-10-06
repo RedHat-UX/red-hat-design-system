d6de21aeea59cc48c60a15fe8977a2161e5ef324
Steven Spriggs <steven.spriggs@gmail.com>
2022-10-06 01:23:31 -0400

    fix: add tsconfig to pfeDevServerConfig (#571)
    * fix: add tsconfig to pfeDevServerConfig
    
    * chore: add changeset
    
    * fix: update pfe-tools version to next in package.json
    
    * chore: update changeset
    
    * fix: add tsconfig to web-test-runner.config.js
    
    * chore: update changeset
    
    Co-authored-by: Benny Powers <bennypowers@users.noreply.github.com>

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -3,6 +3,7 @@ import { litcssOptions } from './web-dev-server.config.js';
 
 export default pfeTestRunnerConfig({
   litcssOptions,
+  tsconfig: 'tsconfig.json',
   files: ['**/elements/**/*.spec.ts'],
   // uncomment to get default wtr reporter
   // reporter: 'default',
