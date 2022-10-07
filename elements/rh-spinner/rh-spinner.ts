644b7a471a27be9cb287fa1447140f90fbabeb3c
marionnegp <mapatel@redhat.com>
2022-10-07 13:39:32 -0400

    Updated README, spinner files, package-lock.json, and package.json

--- elements/rh-spinner/rh-spinner.ts
+++ elements/rh-spinner/rh-spinner.ts
@@ -2,16 +2,12 @@ import { LitElement, html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { styleMap } from 'lit/directives/style-map.js';
 
-import { pfelement } from '@patternfly/pfe-core/decorators.js';
+import { colorContextConsumer, ColorTheme } from '../../lib/context/color.js';
 
-import styles from './rh-spinner.css';
+import { pfelement } from '@patternfly/pfe-core/decorators.js';
 import { Logger } from '@patternfly/pfe-core/controllers/logger.js';
 
-export type SpinnerSize = (
-  | 'sm'
-  | 'md'
-  | 'lg'
-);
+import styles from './rh-spinner.css';
 
 /**
  * Spinner class
@@ -20,43 +16,26 @@ export type SpinnerSize = (
  *
  */
 
+
  @customElement('rh-spinner')
 export class RhSpinner extends LitElement {
   static readonly version = '{{version}}';
 
   static readonly styles = styles;
 
-  /** Size variant of spinner */
-  /*
-  @property({ reflect: true }) size: 'sm' | 'md' | 'lg';
-  */
+  /** Sets color theme based on parent context */
+   @colorContextConsumer()
+   @property({ reflect: true }) on?: ColorTheme;
 
-
-  /** Custom diameter of spinner set as CSS variable */
-  @property({ reflect: true }) diameter?: `${string}${'px'|'%'|'rem'|'em'|'fr'|'pt'}`;
-
-  render() {
-    return html`
-      <div>
-      <svg role="progressbar" viewBox="0 0 100 100" aria-live="polite" style=${styleMap({ '--pf-c-spinner--diameter': this.diameter })}>
+   render() {
+     return html`
+      <svg role="progressbar" viewBox="0 0 100 100" aria-live="polite">
           <circle class="track" cx="50" cy="50" r="40" fill="none" vector-effect="non-scaling-stroke" />
           <circle class="dash" cx="50" cy="50" r="40" fill="none" vector-effect="non-scaling-stroke" />
         </svg>
       <p><slot></slot></p>
-      </div>
     `;
-  }
-
-
-  /*
-  protected _diameterChanged() {
-    if (!this.diameter) {
-      return;
-    }
-
-    this.style.setProperty('--pf-c-spinner--diameter', this.diameter);
-  }
-  */
+   }
 }
 
 declare global {
@@ -64,3 +43,4 @@ declare global {
     'rh-spinner': RhSpinner;
   }
 }
+
