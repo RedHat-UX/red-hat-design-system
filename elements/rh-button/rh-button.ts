6a35f307e0f59934f718e38772a32335d3db53fa
Benny Powers <web@bennypowers.com>
2022-11-30 17:54:11 +0200

    chore: use upstream FACE from pfe-button

--- elements/rh-button/rh-button.ts
+++ elements/rh-button/rh-button.ts
@@ -1,28 +1,14 @@
-import { SlotController } from '@patternfly/pfe-core/controllers/slot-controller.js';
-import { Logger } from '@patternfly/pfe-core/controllers/logger.js';
 import { LitElement, html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
-import { classMap } from 'lit/directives/class-map.js';
-import { ifDefined } from 'lit/directives/if-defined.js';
-
-import { submit } from '../../lib/forms.js';
-
 import styles from './rh-button.css';
 
 /**
  */
 @customElement('rh-button')
-export class RhButton extends LitElement {
+export class RhButton extends BaseButton {
   static readonly version = '{{version}}';
 
   static readonly styles = [styles];
-
-  static readonly formAssociated = true;
-
-  static readonly shadowRootOptions = { ...LitElement.shadowRootOptions, delegatesFocus: true };
-
-  #initiallyDisabled = this.hasAttribute('disabled');
-  @property({ type: Boolean }) disabled = this.#initiallyDisabled;
 
   /**
    * Changes the style of the button.
@@ -35,82 +21,6 @@ export class RhButton extends LitElement {
    * - Tertiary: Tertiary buttons are flexible and can be used as needed.
    */
   @property({ reflect: true }) variant: 'primary'|'secondary'|'tertiary'|'close'|'play' = 'primary';
-
-  /**
-   * Use danger buttons for actions a user can take that are potentially
-   * destructive or difficult/impossible to undo, like deleting or removing
-   * user data.
-   */
-  @property({ type: Boolean, reflect: true }) danger = false;
-
-  @property({ reflect: true }) type?: 'button' | 'submit' | 'reset';
-
-  /** Shorthand for the `icon` slot, the value is icon name */
-  @property() icon = '';
-
-  @property() value?: string;
-
-  @property() name?: string;
-
-  /** Represents the state of the anonymous and icon slots */
-  #slots = new SlotController(this, null, 'icon');
-
-  #internals = this.attachInternals();
-
-  #logger = new Logger(this);
-
-  get form() { return this.#internals.form; }
-  get validity() { return this.#internals.validity; }
-  get validationMessage() { return this.#internals.validationMessage; }
-  get willValidate() { return this.#internals.willValidate; }
-  checkValidity() { return this.#internals.checkValidity(); }
-  reportValidity() { return this.#internals.reportValidity(); }
-
-  formAssociatedCallback(nullableForm: HTMLFormElement|null) {
-    this.#logger.log('formAssociatedCallback', nullableForm);
-  }
-
-  formDisabledCallback(disabled: boolean) {
-    this.#logger.log(`formDisabledCallback(${disabled})`);
-    this.disabled = disabled;
-  }
-
-  formResetCallback() {
-    this.#logger.log('formResetCallback()');
-    this.disabled = this.#initiallyDisabled;
-  }
-
-  formStateRestoreCallback(state: string, mode: unknown) {
-    this.#logger.log(`formStateRestoreCallback(${state}, ${mode})`);
-  }
-
-  override render() {
-    const hasIcon = !!this.icon || this.#slots.hasSlotted('icon');
-    return html`
-      <button type=${ifDefined(this.type)}
-              class=${classMap({ hasIcon })}
-              value=${ifDefined(this.value)}
-              @click=${this.#onClick}
-              ?disabled=${this.disabled}>
-        <span part="icon">
-          <p>Hi!</p>
-          <slot name="icon">${this.renderDefaultIcon()}</slot>
-        </span>
-        <span id="text">
-          <slot></slot>
-        </span>
-      </button>
-    `;
-  }
-
-  #onClick() {
-    switch (this.type) {
-      case 'reset':
-        return void this.#internals.form?.reset();
-      default:
-        return submit(this.#internals.form);
-    }
-  }
 
   protected renderDefaultIcon() {
     switch (this.variant) {
--- lib/forms.ts
+++
@@ -1,9 +0,0 @@
-export function submit(form?: HTMLFormElement|null) {
-  if (form?.reportValidity()) {
-    const event = new Event('submit', { cancelable: true });
-    form.dispatchEvent(event);
-    if (!event.defaultPrevented) {
-      form.submit();
-    }
-  }
-}
