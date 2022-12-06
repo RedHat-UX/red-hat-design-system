1a3ea51e6221c83cd5540d6080c07804fc0ea111
Nikki Massaro Kauffman <nmassaro@redhat.com>
2022-12-06 14:50:21 -0500

    feat: rh-audioplayer buttons and sliders working except menu

--- elements/rh-audio-player/rh-audio-player.ts
+++ elements/rh-audio-player/rh-audio-player.ts
@@ -5,6 +5,7 @@ import { pfelement } from '@patternfly/pfe-core/decorators.js';
 // import {msg} from '@lit/localize';
 
 import styles from './rh-audio-player.css';
+import '../rh-tooltip/rh-tooltip.js';
 
 /**
  * Audio Player
@@ -21,6 +22,7 @@ export class RhAudioPlayer extends LitElement {
   @property({ reflect: true, type: String }) mode = 'full';
   @property({ reflect: true, type: String }) poster = undefined;
   @property({ reflect: true, type: Boolean }) volume = 0.5;
+  @property({ reflect: true, type: Boolean }) playbackRate = 1;
   @state() private _currentTime = 0;
   @state() private _duration = 0;
   @state() private _readyState = 0;
@@ -29,8 +31,8 @@ export class RhAudioPlayer extends LitElement {
   @state() private _unmutedVolume = this.volume;
 
 
-  get mediaElement() {
-    return this.querySelector('audio');
+  get mediaElement():HTMLMediaElement {
+    return this.querySelector('audio') as HTMLMediaElement;
   }
 
   get currentTime() {
@@ -42,15 +44,11 @@ export class RhAudioPlayer extends LitElement {
   }
 
   get muted() {
-    return this._muted;
+    return this._muted || this.volume === 0;
   }
 
   get paused() {
     return this.mediaElement?.paused;
-  }
-
-  get playbackRate() {
-    return this.mediaElement?.playbackRate || 1;
   }
 
   get sliderVolume() {
@@ -60,7 +58,7 @@ export class RhAudioPlayer extends LitElement {
 
   render() {
     return html`
-    <div>${this._readyState}</div>
+      <div id="ready-state">Ready State: ${this._readyState}</div>
       <slot></slot>
       ${this.uiTemplate()}
       
@@ -81,6 +79,9 @@ export class RhAudioPlayer extends LitElement {
         'pause': this._handlePause,
         'play': this._handlePlay,
         'playing': this._handlePlaying,
+        'ratechange': this._handleRatechange,
+        'seeked': this._handleSeeked,
+        'seeking': this._handleSeeking,
         'timeupdate': this._handleTimeupdate,
         'volumechange': this._handleVolumechange
       };
@@ -106,6 +107,7 @@ export class RhAudioPlayer extends LitElement {
   private _handleCanplay() {
     this._duration = this.mediaElement?.duration || 0;
     this._readyState = this.mediaElement?.readyState || 0;
+    this.volume = this.mediaElement?.volume || 0.5;
   }
 
   private _handleCanplaythrough() {
@@ -130,6 +132,10 @@ export class RhAudioPlayer extends LitElement {
     this._readyState = this.mediaElement?.readyState || 0;
   }
 
+  private _handleMuteButton() {
+    !this.muted ? this.mute() : this.unmute();
+  }
+
   private _handlePause() {
     this._paused = true;
   }
@@ -138,8 +144,37 @@ export class RhAudioPlayer extends LitElement {
     this._paused = false;
   }
 
+  private _handlePlaybackRateInput(event:Event) {
+    const target = event?.target as HTMLInputElement;
+    const val = !target || !target.value ? 1 : parseFloat(target.value);
+    const pbr = Math.max(0, Math.min(val, 4));
+    if (this.mediaElement) { this.mediaElement.playbackRate = pbr; }
+  }
+
+  private _handlePlayButton() {
+    !this._paused ? this.pause() : this.play();
+  }
+
   private _handlePlaying() {
     this._paused = false;
+  }
+
+  private _handleRatechange() {
+    this.playbackRate = this.mediaElement?.playbackRate || 1;
+  }
+
+  private _handleSeeked() {
+    this._currentTime = this.mediaElement?.currentTime || 0;
+  }
+
+  private _handleSeeking() {
+    this._currentTime = this.mediaElement?.currentTime || 0;
+  }
+
+  private _handleTimeSlider(event:Event) {
+    const target = event?.target as HTMLInputElement;
+    const seconds = target?.value || -1;
+    if (seconds > 0) { this.seek(seconds as number); }
   }
 
   private _handleTimeupdate() {
@@ -151,6 +186,18 @@ export class RhAudioPlayer extends LitElement {
       this._muted = this.mediaElement?.muted;
       this.volume = this.mediaElement?.volume;
     }
+  }
+
+  private _handleVolumeSlider(event:Event) {
+    const target = event?.target as HTMLInputElement;
+    const level = (target?.value || -1) as number;
+    if (this.mediaElement) {
+      this.mediaElement.volume = level / 100;
+    }
+  }
+
+  private _getTimeString(seconds = 0) {
+    return `${Math.floor(seconds % 3600 / 60).toString().padStart(2, '0')}:${Math.floor(seconds % 60).toString().padStart(2, '0')}`;
   }
 
   /**
@@ -196,16 +243,10 @@ export class RhAudioPlayer extends LitElement {
       </div>
       <div id="controls-other">
         ${this.playbackRateTemplate()}
-        <button id="rewind" ?disabled="${this._readyState < 1}" @click="${this.rewind}">
-          Rewind
-        </button>
-        <button id="play" ?disabled="${this._readyState < 3}" @click="${this._paused ? this.pause : this.play}">
-          ${!this._paused ? 'Pause' : 'Play'}
-        </button>
-        <button id="forward" ?disabled="${this._readyState < 1}" @click="${this.forward}">
-          Rewind
-        </button>
-        <button id="menu">&hellip;</button>
+        ${this.rewindButtonTemplate()}
+        ${this.playButtonTemplate()}
+        ${this.forwardButtonTemplate()}
+        ${this.menuButtonTemplate()}
       </div>
     </div>
     `;
@@ -213,55 +254,107 @@ export class RhAudioPlayer extends LitElement {
 
   timeTemplate() {
     return html`<div id="time-controls">
-      <input 
-        id="volume" 
-        ?disabled="${this.duration === 0}"
-        type="range" 
-        min="0" 
-        max="${this.duration}" 
-        @input="${this._handleTimeSlider}"
-        value="${this.currentTime}">
+      <rh-tooltip>
+        <label for="time">Seek</label>
+        <input 
+          id="time" 
+          ?disabled="${this.duration === 0}"
+          type="range" 
+          min="0" 
+          max="${this.duration}" 
+          @input="${this._handleTimeSlider}"
+          value="${this._currentTime}">
+        <span slot="content">Seek</span>
+      </rh-tooltip>
       <div id="time-labels">
-        <span id="current">${this.currentTime}</span>
-        <span id="duration">${this.duration}</span>
+        <span class="sr-only">Elapsed time</span>
+        <span id="current">${this._getTimeString(this._currentTime)}</span>
+        <span class="sr-only">/</span>
+        <span id="duration">${this._getTimeString(this.duration)}</span>
       </div>
     </div>`;
   }
 
+  buttonTemplate(id = '', icon = html``, label = '', callback = this._handlePlay, disabled = false) {
+    return html`
+      <rh-tooltip>
+        <button id="${id}" ?disabled=${disabled} @click="${callback}">
+          ${icon}
+        </button>
+        <span slot="content">${label}</span>
+      </rh-tooltip>
+    `;
+  }
+
   volumeTemplate() {
-    const max = !this.mediaElement ? 0 : 100;
     return html`<div id="volume-controls">
-      <button id="mute" @click="${!this.muted ? this.mute : this.unmute}">
-        ${!this.muted ? 'Mute' : 'Unmute'}
-      </button>
-      <input 
-        id="volume" 
-        ?disabled="${max === 0}"
-        type="range" 
-        min="0" 
-        max="${max}" 
-        step="1" 
-        @input="${this._handleVolumeSlider}"
-        value="${this.sliderVolume}">
+      ${this.muteButtonTemplate()}
+      ${this.volumeSliderTemplate()}
     </div>`;
   }
 
+  muteButtonTemplate() {
+    const icon = !this.muted ? html`&#128264;` : html`&#128263;`;
+    const label = !this.muted ? 'Mute' : 'Unmute';
+    return this.buttonTemplate(
+      'mute',
+      icon,
+      label,
+      this._handleMuteButton);
+  }
+
+  volumeSliderTemplate() {
+    const max = !this.mediaElement ? 0 : 100;
+    return html`  
+      <rh-tooltip>
+        <label for="volume">Volume</label>
+        <input 
+          id="volume" 
+          ?disabled="${max === 0}"
+          type="range" 
+          min="0" 
+          max="${max}" 
+          step="1" 
+          @input="${this._handleVolumeSlider}"
+          value="${this.sliderVolume}">
+        <span slot="content">Volume</span>
+      </rh-tooltip>`;
+  }
+
   playbackRateTemplate() {
-    return html`<input id="playback-rate" type="number" step="0.25" min="0.5" max="2" value="${this.playbackRate}"/>x`;
+    return html`
+      <rh-tooltip>
+        <label for="playback-rate">Playback rate</label>
+        <input id="playback-rate" 
+          type="number" 
+          step="0.25" 
+          min="0.25" 
+          max="2" 
+          value="${this.playbackRate}" 
+          @change="${this._handlePlaybackRateInput}"/>x
+        <span slot="content">Playback rate</span>
+      </rh-tooltip>`;
   }
 
-  _handleTimeSlider(event:Event) {
-    const target = event?.target as HTMLInputElement;
-    const seconds = target?.value || -1;
-    if (seconds > 0) { this.seek(seconds as number); }
+  rewindButtonTemplate() {
+    const disabled = this._readyState < 1 || this._currentTime === 0;
+    return this.buttonTemplate('rewind', html`&#9194;`, 'Rewind 15 seconds', this.rewind, disabled);
   }
 
-  _handleVolumeSlider(event:Event) {
-    const target = event?.target as HTMLInputElement;
-    const level = (target?.value || -1) as number;
-    if (this.mediaElement) {
-      this.mediaElement.volume = level / 100;
-    }
+  playButtonTemplate() {
+    const icon = !this._paused ? html`&#9208;` : html`&#9654;`;
+    const label = !this._paused ? 'Pause' : 'Play';
+    const disabled = this._readyState < 3;
+    return this.buttonTemplate('play', icon, label, this._handlePlayButton, disabled);
+  }
+
+  forwardButtonTemplate() {
+    const disabled = this._readyState < 1 || this._currentTime === this.duration;
+    return this.buttonTemplate('forward', html`&#9193;`, 'Advance 15 seconds', this.forward, disabled);
+  }
+
+  menuButtonTemplate() {
+    return this.buttonTemplate('menu', html`&hellip;`, 'Menu', this.toggleMenu);
   }
 
   mute() {
@@ -286,13 +379,12 @@ export class RhAudioPlayer extends LitElement {
   }
 
   play() {
-    this.volume = this.mediaElement?.volume || 0.5;
     if (this.mediaElement?.play) { this.mediaElement.play(); }
   }
 
   seek(seconds:number) {
     const time = Math.max(0, Math.min(seconds, this.duration));
-    if (!!time && this.mediaElement) { this.mediaElement.currentTime = time; }
+    if (this.mediaElement) { this.mediaElement.currentTime = time; }
   }
 
   seekFromCurrentTime(seconds = 0) {
@@ -304,11 +396,11 @@ export class RhAudioPlayer extends LitElement {
   }
 
   rewind() {
-    this.seekFromCurrentTime(-15);
+    this.seekFromCurrentTime(-150);
   }
 
   forward() {
-    this.seekFromCurrentTime(15);
+    this.seekFromCurrentTime(150);
   }
 }
 
