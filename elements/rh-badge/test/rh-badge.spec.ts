d08fa052a41c06e7203b3cdabbe51a22105c29e3
marionnegp <mapatel@redhat.com>
2022-12-07 15:59:15 -0500

    feat(badge): add tests for border radius, padding, and colors

--- elements/rh-badge/test/rh-badge.spec.ts
+++ elements/rh-badge/test/rh-badge.spec.ts
@@ -1,6 +1,17 @@
 import { expect, html } from '@open-wc/testing';
 import { createFixture } from '@patternfly/pfe-tools/test/create-fixture.js';
+import { hexToRgb, getColor } from '@patternfly/pfe-tools/test/hex-to-rgb.js';
 import { RhBadge } from '@rhds/elements/rh-badge/rh-badge.js';
+
+// Background colors for the various states
+const states = {
+  default: '#f0f0f0',
+  info: '#0066cc',
+  success: '#3e8635',
+  moderate: '#f0ab00',
+  important: '#be0000',
+  critical: '#8f0000'
+};
 
 const element = html`
   <rh-badge></rh-badge>
@@ -14,5 +25,31 @@ describe('<rh-badge>', function() {
       .to.be.an.instanceOf(klass)
       .and
       .to.be.an.instanceOf(RhBadge);
+  });
+});
+
+it('should have a pill shape and padding on left and right only', async function() {
+  const element = await createFixture<RhBadge>(html`
+    <rh-badge></rh-badge>
+  `);
+  const elStyles = getComputedStyle(element);
+
+  expect(elStyles.getPropertyValue('border-radius')).to.equal('64px');
+  expect(elStyles.getPropertyValue('padding-top')).to.equal('0px');
+  expect(elStyles.getPropertyValue('padding-right')).to.equal('8px');
+  expect(elStyles.getPropertyValue('padding-bottom')).to.equal('0px');
+  expect(elStyles.getPropertyValue('padding-left')).to.equal('8px');
+});
+
+Object.entries(states).forEach(([state, color]) => {
+  it(`should have a background color of '${color}' when state is ${state}`, async function() {
+    const el = await createFixture<RhBadge>(element);
+
+    if (state !== 'default') {
+      el.setAttribute('state', state);
+    }
+
+    const [r, g, b] = getColor(el, 'background-color');
+    expect([r, g, b]).to.deep.equal(hexToRgb(color));
   });
 });
