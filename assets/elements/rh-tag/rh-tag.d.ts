4308b67ce6a903e19dd60c76f3071dad3f9e3fcf
zeroedin <zeroedin@users.noreply.github.com>
2022-12-12 13:07:20 +0000

    deploy: 99cf19e93c457415c4e13dccf9b4f68c42f4e2d2

---
+++ assets/elements/rh-tag/rh-tag.d.ts
@@ -0,0 +1,34 @@
+import { BaseLabel } from '@patternfly/pfe-label/BaseLabel.js';
+import '@patternfly/pfe-icon';
+export declare type LabelVariant = ('filled');
+export declare type LabelColor = ('blue' | 'cyan' | 'green' | 'orange' | 'purple' | 'red' | 'grey');
+/**
+ * Tooltip
+ * @slot icon - Contains the labels's icon, e.g. web-icon-alert-success.
+ * @slot -  Must contain the text for the label.
+ *
+ * @csspart icon - container for the label icon
+ *
+ * @cssprop {<length>} --rh-tag-padding-block-start   {@default `4px`}
+ * @cssprop {<length>} --rh-tag-padding-inline-end    {@default `8px`}
+ * @cssprop {<length>} --rh-tag-padding-block-end     {@default `4px`}
+ * @cssprop {<length>} --rh-tag-padding-inline-start  {@default `8px`}
+ *
+ * @cssprop {<length>} --rh-tag-margin-inline-end     {@default `4px`}
+ *
+ *
+ */
+export declare class RhTag extends BaseLabel {
+    static readonly styles: import("lit").CSSResult[];
+    /**
+     * RhIcon does not yet exist, so we are using pfe-icon until available
+     * <rh-icon ?hidden=${!this.icon} icon=${this.icon} set="${this.set}" size="sm"></rh-icon>
+     */
+    protected renderDefaultIcon(): import("lit-html").TemplateResult<1> | "";
+    protected renderSuffix(): import("lit-html").TemplateResult<1>;
+}
+declare global {
+    interface HTMLElementTagNameMap {
+        'rh-tag': RhTag;
+    }
+}
---
+++ assets/elements/rh-tag/rh-tag.js
@@ -0,0 +1,42 @@
+import { __decorate } from "tslib";
+import { html } from 'lit';
+import { customElement } from 'lit/decorators.js';
+import { BaseLabel } from '@patternfly/pfe-label/BaseLabel.js';
+import '@patternfly/pfe-icon';
+import styles from "./rh-tag.css.js";
+/**
+ * Tooltip
+ * @slot icon - Contains the labels's icon, e.g. web-icon-alert-success.
+ * @slot -  Must contain the text for the label.
+ *
+ * @csspart icon - container for the label icon
+ *
+ * @cssprop {<length>} --rh-tag-padding-block-start   {@default `4px`}
+ * @cssprop {<length>} --rh-tag-padding-inline-end    {@default `8px`}
+ * @cssprop {<length>} --rh-tag-padding-block-end     {@default `4px`}
+ * @cssprop {<length>} --rh-tag-padding-inline-start  {@default `8px`}
+ *
+ * @cssprop {<length>} --rh-tag-margin-inline-end     {@default `4px`}
+ *
+ *
+ */
+let RhTag = class RhTag extends BaseLabel {
+    /**
+     * RhIcon does not yet exist, so we are using pfe-icon until available
+     * <rh-icon ?hidden=${!this.icon} icon=${this.icon} set="${this.set}" size="sm"></rh-icon>
+     */
+    renderDefaultIcon() {
+        return !this.icon ? '' : html `
+      <pfe-icon ?hidden=${!this.icon} icon=${this.icon}></pfe-icon>
+    `;
+    }
+    renderSuffix() {
+        return html ``;
+    }
+};
+RhTag.styles = [styles];
+RhTag = __decorate([
+    customElement('rh-tag')
+], RhTag);
+export { RhTag };
+//# sourceMappingURL=rh-tag.js.map
