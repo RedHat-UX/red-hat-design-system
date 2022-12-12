de49a5c0841991e44fd9b3d9993e8f7b7be5480b
Steven Spriggs <steven.spriggs@gmail.com>
2022-12-12 08:05:51 -0500

    feat(tag): add <rh-tag> (#597)
    * feat(label): add rh-label
    
    * test(label): rename e2e tests
    
    * fix(label): fix package-lock installed under incorrect npm version
    
    * docs(label): improve docs
    
    * fix(label): incorrect dep version in package.json
    
    * fix(label): use latest pfe-icon api and correct icon size
    
    * fix(label): revert dep version shorthand, increment
    
    * style(label): linting
    
    * refactor(label): rename to rh-tag
    
    * test(tag): fix import of RhTag
    
    * fix(tags): fix broken package-lock
    
    * docs(tag): import pfe-icon in demo js
    
    * fix(tags): add pfe-icon as a dependency
    
    * fix(tag): remove pfe-codeblock
    
    * docs(tag): improve README.md
    
    * fix(tag): remove orphaned pfe-band slots
    
    * fix(tag): remove set attribute
    
    * test(tag): improve spec tests
    
    * docs(tag): add changeset
    
    * docs(tag): imporove README.md

--- elements/rh-tag/test/rh-tag.spec.ts
+++ elements/rh-tag/test/rh-tag.spec.ts
@@ -1,4 +1,5 @@
-import { expect, html, aTimeout } from '@open-wc/testing';
+import { expect, html } from '@open-wc/testing';
+import { PfeIcon } from '@patternfly/pfe-icon';
 import { createFixture } from '@patternfly/pfe-tools/test/create-fixture.js';
 import { getColor, hexToRgb } from '@patternfly/pfe-tools/test/hex-to-rgb.js';
 import { RhTag } from '../rh-tag.js';
@@ -25,27 +26,16 @@ let element: RhTag;
 let beforeStyles: CSSStyleDeclaration;
 let container: HTMLElement;
 
-  function getStyles(element) {
-    return getComputedStyle(element);
-  }
+function getBeforeStyles() {
+  container = element.shadowRoot!.querySelector('#container')!;
+  return getComputedStyle(container, '::before');
+}
 
-  function getBeforeStyles(element) {
-    return getComputedStyle(element, '::before');
-  }
-
-  function normalizeColor(color?: string|number) {
-    return new Color(color as string).toString();
-  }
-
-  describe('default', async function() {
-    beforeEach(async function() {
-      element = await createFixture <RhTag>(html`
-        <rh-tag>Default</rh-tag>
-      `);
-      container = element.shadowRoot!.querySelector('#container')!;
-      styles = getStyles(container);
-      beforeStyles = getBeforeStyles(container);
-    });
+describe('<rh-tag>', async function() {
+  beforeEach(async function() {
+    element = await createFixture <RhTag>(TEMPLATE);
+    await element.updateComplete;
+  });
 
   it('should upgrade', async function() {
     const klass = customElements.get('rh-tag');
@@ -78,15 +68,13 @@ describe('default', async function() {
   });
 });
 
-  describe('red', async function() {
-    beforeEach(async function() {
-      element = await createFixture <RhTag>(html`
-        <rh-tag color="red">red</rh-tag>
-      `);
-      container = element.shadowRoot!.querySelector('#container')!;
-      styles = getStyles(container);
-      beforeStyles = getBeforeStyles(container);
-    });
+describe('red', async function() {
+  beforeEach(async function() {
+    element = await createFixture <RhTag>(TEMPLATE);
+    element.color = 'red';
+    await element.updateComplete;
+    beforeStyles = getBeforeStyles();
+  });
 
   it('should have correct background color', async function() {
     expect(getColor(element, 'background-color')).to.deep.equal(hexToRgb('#faeae8'));
@@ -99,68 +87,56 @@ describe('default', async function() {
   });
 });
 
-  describe('with icon attribute', async function() {
-    let unslotted: RhTag;
-    let containerWithIcon: HTMLElement;
-    beforeEach(async function() {
-      unslotted = await createFixture<RhTag>(html`<rh-tag>Default</rh-tag>`);
-      element = await createFixture <RhTag>(html`
-        <rh-tag icon="info-circle">Default</rh-tag>
-      `);
-      container = unslotted.shadowRoot!.querySelector('#container')!;
-      containerWithIcon = element.shadowRoot!.querySelector('#container')!;
-    });
-
-    it('should display the icon', function() {
-      expect(containerWithIcon.getBoundingClientRect().width)
-        .to.be
-        .greaterThan(container.getBoundingClientRect().width);
-    });
+describe('icon with attribute', async function() {
+  beforeEach(async function() {
+    element = await createFixture <RhTag>(TEMPLATE);
+    element.icon = 'cirlce-info';
+    await element.updateComplete;
+    beforeStyles = getBeforeStyles();
   });
 
-  describe('slotted icon', async function() {
-    let unslotted: RhTag;
-    let containerWithIcon: HTMLElement;
-    beforeEach(async function() {
-      unslotted = await createFixture<RhTag>(html`<rh-tag>Default</rh-tag>`);
-      element = await createFixture<RhTag>(html`
-        <rh-tag>
-          Default
-          <pfe-icon slot="icon" icon="info-circle"></pfe-icon>
-        </rh-tag>
-      `);
-      container = unslotted.shadowRoot!.querySelector('#container')!;
-      containerWithIcon = element.shadowRoot!.querySelector('#container')!;
-    });
-
-    it('should display the icon', function() {
-      expect(containerWithIcon.getBoundingClientRect().width)
-        .to.be
-        .greaterThan(container.getBoundingClientRect().width);
-    });
+  it('should have icon class', async function() {
+    expect(container.classList.contains('hasIcon')).to.be.true;
   });
 
-  describe('slotted svg', async function() {
-    let unslotted: RhTag;
-    let containerWithIcon: HTMLElement;
-    beforeEach(async function() {
-      unslotted = await createFixture<RhTag>(html`<rh-tag>Default</rh-tag>`);
-      element = await createFixture<RhTag>(html`
-        <rh-tag>
-          Default
-          <svg slot="icon" viewBox="0 0 512 512">
-            <path d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 110c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z"/>
-          </svg>
-        </rh-tag>
-      `);
-      container = unslotted.shadowRoot!.querySelector('#container')!;
-      containerWithIcon = element.shadowRoot!.querySelector('#container')!;
-    });
+  it('should have slotted icon', async function() {
+    const icon = element.shadowRoot!.querySelector('pfe-icon') as PfeIcon;
+    expect(icon).to.exist;
+  });
+});
 
-    it('should display the icon', function() {
-      expect(containerWithIcon.getBoundingClientRect().width)
-        .to.be
-        .greaterThan(container.getBoundingClientRect().width);
-    });
+describe('slotted icon', async function() {
+  beforeEach(async function() {
+    element = await createFixture <RhTag>(SLOTTED_PFEICON);
+    await element.updateComplete;
+    beforeStyles = getBeforeStyles();
+  });
+
+  it('should have icon class', async function() {
+    expect(container.classList.contains('hasIcon')).to.be.true;
+  });
+
+  it('should have slotted pfe-icon', async function() {
+    const slot = element.shadowRoot!.querySelector('slot[name="icon"]') as HTMLSlotElement;
+    const [pfeIcon] = slot!.assignedElements();
+    expect(pfeIcon.tagName).to.be.equal('PFE-ICON');
+  });
+});
+
+describe('slotted svg', async function() {
+  beforeEach(async function() {
+    element = await createFixture <RhTag>(SLOTTED_SVG);
+    await element.updateComplete;
+    beforeStyles = getBeforeStyles();
+  });
+
+  it('should have icon class', async function() {
+    expect(container.classList.contains('hasIcon')).to.be.true;
+  });
+
+  it('should have slotted pfe-icon', async function() {
+    const slot = element.shadowRoot!.querySelector('slot[name="icon"]') as HTMLSlotElement;
+    const [svg] = slot!.assignedElements();
+    expect(svg.tagName).to.be.equal('svg');
   });
 });
