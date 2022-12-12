7677e966d41a8923097c40ac29744f724d6055be
Benny Powers <web@bennypowers.com>
2022-12-12 15:31:14 +0200

    fix(badge): declare state property

--- elements/rh-badge/rh-badge.ts
+++ elements/rh-badge/rh-badge.ts
@@ -1,16 +1,8 @@
-import { customElement } from 'lit/decorators.js';
+import { customElement, property } from 'lit/decorators.js';
 
 import { BaseBadge } from '@patternfly/pfe-badge/BaseBadge.js';
 
 import styles from './rh-badge.css';
-
-export type state = (
-  | 'info'
-  | 'success'
-  | 'moderate'
-  | 'important'
-  | 'critical'
-);
 
 /**
  * A badge is used to annotate other information with numerical content.
@@ -21,6 +13,12 @@ export class RhBadge extends BaseBadge {
   static readonly version = '{{version}}';
 
   static readonly styles = [...BaseBadge.styles, styles];
+
+  /**
+   * Denotes the state-of-affairs this badge represents
+   * Options include read and unread
+   */
+  @property({ reflect: true }) state?: 'info'|'success'|'moderate'|'important'|'critical';
 }
 
 declare global {
