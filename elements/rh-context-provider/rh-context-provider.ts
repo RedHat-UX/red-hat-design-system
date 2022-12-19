393c3ab07f29ff01f58bdb778c6e36b6e32d3425
Benny Powers <web@bennypowers.com>
2022-12-19 11:02:29 +0200

    fix(context-provider): remove wrapping div

--- elements/rh-context-provider/rh-context-provider.ts
+++ elements/rh-context-provider/rh-context-provider.ts
@@ -1,10 +1,9 @@
-import type { ColorPalette, ColorTheme } from '../../lib/context/color.js';
+import type { ColorPalette } from '../../lib/context/color.js';
 
 import { html, LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
-import { classMap } from 'lit/directives/class-map.js';
 
-import { colorContextConsumer, colorContextProvider } from '../../lib/context/color.js';
+import { colorContextProvider } from '../../lib/context/color.js';
 
 import contextStyle from '../../lib/context/context-color.css';
 import style from './rh-context-provider.css';
@@ -22,12 +21,7 @@ export class ContextProvider extends LitElement {
   @colorContextProvider()
   @property({ reflect: true, attribute: 'color-palette' }) colorPalette?: ColorPalette;
 
-  /**
-   * Sets color theme based on parent context
-   */
-  @colorContextConsumer() on: ColorTheme = 'light';
-
   render() {
-    return html`<div class="${classMap({ [this.on]: !!this.on })}"><slot></slot></div>`;
+    return html`<slot></slot>`;
   }
 }
