76eec67b4424109e664469bebf8fe5f9ce4d55ac
Benny Powers <web@bennypowers.com>
2022-12-21 16:24:01 +0200

    docs: update demos

--- elements/rh-accordion/demo/rh-accordion.js
+++ elements/rh-accordion/demo/rh-accordion.js
@@ -1,8 +1,3 @@
 import '@rhds/elements/rh-context-provider/rh-context-provider.js';
-import '@patternfly/pfe-accordion';
 import '@rhds/elements/rh-cta/rh-cta.js';
 import '@rhds/elements/rh-accordion/rh-accordion.js';
-
-const root = document.querySelector('[data-demo="rh-accordion"]')?.shadowRoot ?? document;
-
-root.querySelector('rh-accordion');
