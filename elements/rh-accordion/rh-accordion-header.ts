71d8a067195af9261ef74de725cbc6d5a341a4d7
Benny Powers <web@bennypowers.com>
2022-12-22 10:04:45 +0200

    fix(accordion): remove pfe icon

--- elements/rh-accordion/rh-accordion-header.ts
+++ elements/rh-accordion/rh-accordion-header.ts
@@ -43,6 +43,15 @@ export class RhAccordionHeader extends BaseAccordionHeader {
       <div id="container" class="${classMap({ [on]: !!on })}">${super.render()}</div>
     `;
   }
+
+  renderAfterButton() {
+    // Font-Awesome free angle-right
+    return html`
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+        <path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z" />
+      </svg>
+    `;
+  }
 }
 
 declare global {
