ff88bcbcc914c6a008d7ede83894374eba5dfba1
Brian Ferry <bferry@redhat.com>
2023-01-03 11:20:44 -0500

    fix: including jpeg files in passthrough, updating card demo

--- elements/rh-card/demo/rh-card.js
+++ elements/rh-card/demo/rh-card.js
@@ -1,6 +1,4 @@
-import '@patternfly/pfe-band';
-import '@patternfly/pfe-cta';
-import '@patternfly/pfe-select';
+import '@rhds/elements/rh-cta/rh-cta.js';
 import '@rhds/elements/rh-card/rh-card.js';
 
 const root = document.querySelector('[data-demo="rh-card"]')?.shadowRoot ?? document;
