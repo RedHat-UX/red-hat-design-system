21c4361d1c03a004b0a9088227787eb11e5dc8f9
Marionne Patel <95588923+marionnegp@users.noreply.github.com>
2023-01-06 15:53:07 -0500

    fix(docs): remove spinner demo page from ux.redhat.com (#674)
    * fix(docs): removed spinner demo page from ux dot nav
    
    * docs(spinner): update package-lock
    
    * fix(docs): update package-lock and node modules
    
    * test(tag): don't test shadow dom
    
    * perf(tag): optimize css
    
    * fix(tag): implement abstract props
    
    Co-authored-by: Michael Potter <mgp140@gmail.com>
    Co-authored-by: Benny Powers <web@bennypowers.com>

--- elements/rh-tag/test/rh-tag.spec.ts
+++ elements/rh-tag/test/rh-tag.spec.ts
@@ -2,7 +2,6 @@ import { expect, html } from '@open-wc/testing';
 import { PfeIcon } from '@patternfly/pfe-icon';
 import { createFixture } from '@patternfly/pfe-tools/test/create-fixture.js';
 import { getColor, hexToRgb } from '@patternfly/pfe-tools/test/hex-to-rgb.js';
-import { getColor, hexToRgb } from '@patternfly/pfe-tools/test/hex-to-rgb.js';
 import { RhTag } from '../rh-tag.js';
 
 const TEMPLATE = html`
@@ -27,32 +26,6 @@ let element: RhTag;
 let beforeStyles: CSSStyleDeclaration;
 let container: HTMLElement;
 
-const TEMPLATE = html`
-  <rh-tag>Default</rh-tag>
-`;
-
-const SLOTTED_PFEICON = html`
-  <rh-tag>
-    Default
-    <pfe-icon slot="icon" icon="info-cirle"></pfe-icon>
-  </rh-tag>
-`;
-
-const SLOTTED_SVG = html`
-  <rh-tag>
-    Default
-    <svg slot="icon" height="1em" width: "1em" fill="currentColor" viewBox="0 0 512 512" aria-hidden="true" role="img"><path d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 110c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z"></path></svg>
-  </rh-tag>
-`;
-
-let element: RhTag;
-let beforeStyles: CSSStyleDeclaration;
-let container: HTMLElement;
-
-function getBeforeStyles() {
-  container = element.shadowRoot!.querySelector('#container')!;
-  return getComputedStyle(container, '::before');
-}
 function getBeforeStyles() {
   container = element.shadowRoot!.querySelector('#container')!;
   return getComputedStyle(container, '::before');
