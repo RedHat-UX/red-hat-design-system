ac2c1bdd0cac5ea01806378f3930d154c3715a8f
Brian Ferry <bferry@redhat.com>
2023-01-10 11:53:31 -0500

    fix(accordion): dark theme border color, arrow direction, rotation, border right length on header

--- elements/rh-accordion/rh-accordion-header.ts
+++ elements/rh-accordion/rh-accordion-header.ts
@@ -46,11 +46,11 @@ export class RhAccordionHeader extends BaseAccordionHeader {
   }
 
   renderAfterButton() {
-    // Font-Awesome free angle-right
+    // Font-Awesome free angle-down
     // TODO: use rh-icon when it's ready
     return html`
-      <svg id="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
-        <path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z" />
+      <svg id="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+        <path d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/>
       </svg>
     `;
   }
