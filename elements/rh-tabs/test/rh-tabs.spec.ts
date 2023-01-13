0b928934a9a72074fdf504b296e1f962c5e17d10
Steven Spriggs <steven.spriggs@gmail.com>
2023-01-13 15:15:22 -0500

    test(tabs): add spec tests

--- elements/rh-tabs/test/rh-tabs.spec.ts
+++ elements/rh-tabs/test/rh-tabs.spec.ts
@@ -1,18 +1,179 @@
-import { expect, html } from '@open-wc/testing';
+import { expect, html, nextFrame, aTimeout } from '@open-wc/testing';
 import { createFixture } from '@patternfly/pfe-tools/test/create-fixture.js';
-import { RhTabs } from '@rhds/elements/rh-tabs/rh-tabs.js';
+import { setViewport, sendKeys } from '@web/test-runner-commands';
 
-const element = html`
-  <rh-tabs></rh-tabs>
-`;
+import { RhTabs } from '@rhds/elements/rh-tabs/rh-tabs.js';
+import { RhTab } from '@rhds/elements/rh-tabs/rh-tab.js';
+import { tokens } from '@rhds/tokens';
+
+import Color from 'colorjs.io';
+
+function normalizeColor(color?: string|number) {
+  return new Color(color as string).toString();
+}
+
+async function allUpdates(element: ReactiveElement) {
+  let count = 0;
+  do {
+    if (count > 100) {
+      throw new Error('Too Many Updates');
+    }
+    await element.updateComplete;
+    count++;
+  } while (!await element.updateComplete);
+}
 
 describe('<rh-tabs>', function() {
+  let element: RhTabs;
+
+  beforeEach(async function() {
+    element = await createFixture<RhTabs>(html`
+      <rh-tabs>
+        <rh-tab id="users" slot="tab" active>Users</rh-tab>
+        <rh-tab-panel>Users</rh-tab-panel>
+        <rh-tab slot="tab">Containers</rh-tab>
+        <rh-tab-panel>Containers <a href="#">Focusable element</a></rh-tab-panel>
+        <rh-tab slot="tab">Database</rh-tab>
+        <rh-tab-panel>Database</rh-tab-panel>
+        <rh-tab slot="tab">Servers</rh-tab>
+        <rh-tab-panel>Servers</rh-tab-panel>
+        <rh-tab slot="tab">Cloud</rh-tab>
+        <rh-tab-panel>Cloud</rh-tab-panel>
+      </rh-tabs>
+    `);
+    await allUpdates(element);
+  });
+
   it('should upgrade', async function() {
-    const el = await createFixture <RhTabs>(element);
     const klass = customElements.get('rh-tabs');
-    expect(el)
+    expect(element)
       .to.be.an.instanceOf(klass)
       .and
       .to.be.an.instanceOf(RhTabs);
+  });
+
+  describe('vertical tabs', function() {
+    it('should have vertical styles', async function() {
+      element.setAttribute('vertical', '');
+      await nextFrame();
+      const tabs = element.shadowRoot!.querySelector('[part="tabs"]')!;
+      const tabsVerticalStyles = getComputedStyle(tabs).flexDirection;
+      expect(tabsVerticalStyles).to.be.equal('column');
+    });
+  });
+
+  describe('base theme tabs', function() {
+    it('should have base theme styles', async function() {
+      element.setAttribute('theme', 'base');
+      await nextFrame();
+      const tab = element.querySelector('rh-tab');
+      const button = tab.shadowRoot!.querySelector('button')!;
+      const buttonAfterStyles = getComputedStyle(button, '::after').borderColor;
+      expect(normalizeColor(buttonAfterStyles)).to.be.equal(normalizeColor(tokens.get('--rh-color-border-interactive-on-light')));
+    });
+  });
+
+  describe('box tabs', function() {
+    it('should have box styles', async function() {
+      element.setAttribute('box', '');
+      await nextFrame();
+      const tab = element.querySelector('rh-tab');
+      const button = tab.shadowRoot!.querySelector('button')!;
+      const buttonBeforeStyles = getComputedStyle(button, '::before').borderBlockEndWidth;
+      const buttonAfterStyles = getComputedStyle(button, '::after').borderBlockStartWidth;
+      expect(buttonBeforeStyles).to.be.equal(tokens.get('--rh-border-width-sm'));
+      expect(buttonAfterStyles).to.be.equal(tokens.get('--rh-border-width-lg'));
+    });
+  });
+
+  describe('inset tabs', function() {
+    it('should have inset styles', async function() {
+      element.setAttribute('inset', 'lg');
+      await nextFrame();
+      const insetEl = element.shadowRoot!.querySelector(`[part="tabs"]`)!;
+      const insetStyles = getComputedStyle(insetEl).marginInline;
+      expect(insetStyles).to.be.equal(tokens.get(`--rh-space-lg`));
+    });
+  });
+
+  describe('keyboard navigation', function() {
+    let firstItem: RhTab;
+    let secondItem: RhTab;
+    let lastItem: RhTab;
+    let initialActiveElement: Element|null;
+
+    beforeEach(async function() {
+      [firstItem, secondItem] = element.querySelectorAll<RhTab>('rh-tab');
+      lastItem = [...element.querySelectorAll<RhTab>('rh-tab')].pop();
+      await sendKeys({ press: 'Tab' });
+      initialActiveElement = document.activeElement;
+    });
+
+    it('should focus a this first jump-links-item', function() {
+      expect(document.activeElement).to.equal(firstItem);
+    });
+
+    describe('pressing right arrow key', function() {
+      beforeEach(async function() {
+        await sendKeys({ press: 'ArrowRight' });
+        await allUpdates(element);
+        await nextFrame();
+      });
+
+      it('should focus a rh-tab button', function() {
+        expect(document.activeElement).to.be.an.instanceof(RhTab);
+      });
+
+      it('should change focus when keyboard navigation is used', function() {
+        expect(document.activeElement).to.not.equal(initialActiveElement);
+      });
+
+      it('should focus the second rh-tab item', function() {
+        expect(document.activeElement).to.equal(secondItem);
+      });
+    });
+
+    describe('pressing left arrow key', function() {
+      beforeEach(async function() {
+        await sendKeys({ press: 'ArrowLeft' });
+        await allUpdates(element);
+        await nextFrame();
+      });
+
+      it('should focus a rh-tab button', function() {
+        expect(document.activeElement).to.be.an.instanceof(RhTab);
+      });
+
+      it('should change focus when keyboard navigation is used', function() {
+        expect(document.activeElement).to.not.equal(initialActiveElement);
+      });
+
+      it('should focus the last rh-tab item', function() {
+        expect(document.activeElement).to.equal(lastItem);
+      });
+    });
+  });
+
+  describe('on small screen', function() {
+    beforeEach(async function() {
+      await setViewport({ width: 320, height: 640 });
+      await allUpdates(element);
+    });
+
+    it('should overflow if too wide', async function() {
+      const tabs = element.shadowRoot!.querySelector('[part="tabs"]')!;
+      const tabsOverflow = getComputedStyle(tabs).overflowX === 'auto';
+      expect(tabsOverflow).to.be.equal(true);
+    });
+
+    it('should have visible scroll buttons if overflowed', async function() {
+      await aTimeout(150);
+      const previousTab = element.shadowRoot!.querySelector('#previousTab')!;
+      const nextTab = element.shadowRoot!.querySelector('#nextTab')!;
+      const prevDisplayStyle = getComputedStyle(previousTab).display;
+      const nextDisplayStyle = getComputedStyle(nextTab).display;
+      expect(prevDisplayStyle ).to.not.equal('none');
+      expect(nextDisplayStyle).to.not.equal('none');
+    });
   });
 });
