572464f3f0baa852d33e6ee77ea14705b3c8f146
Nikki Massaro Kauffman <nmassaro@redhat.com>
2023-01-24 15:05:58 -0500

    docs(audio-player): updated demos

--- elements/rh-audio-player/demo/rh-audio-player.js
+++ elements/rh-audio-player/demo/rh-audio-player.js
@@ -2,4 +2,23 @@ import '@rhds/elements/rh-audio-player/rh-audio-player.js';
 
 const root = document.querySelector('[data-demo="rh-audio-player"]')?.shadowRoot ?? document;
 
-root.querySelector('rh-audio-player');
+const form = root.querySelector('form');
+const player = root.querySelector('rh-audio-player');
+const mediaseries = player.querySelector('[slot="series"]');
+const mediatitle = player.querySelector('[slot="title"]');
+const { poster } = player;
+
+form.addEventListener('input', sync);
+
+function sync() {
+  const formData = new FormData(form);
+  const formObj = Object.fromEntries(formData);
+  player.on = formObj.dark ? 'dark' : 'light';
+  player.mode = formObj.mode;
+  player.poster = !formObj.poster ? undefined : poster;
+
+  mediaseries.innerHTML = formObj?.series || '';
+  mediatitle.innerHTML = formObj?.title || '';
+}
+
+sync();
