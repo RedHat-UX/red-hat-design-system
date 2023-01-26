5fce0f0f924728083f4d10d4ac4fc3a038b40439
Benny Powers <web@bennypowers.com>
2023-01-26 18:27:26 +0200

    docs(context): demo nested context bug

--- elements/rh-context-provider/demo/rh-context-provider.js
+++ elements/rh-context-provider/demo/rh-context-provider.js
@@ -4,3 +4,24 @@ import '@rhds/elements/rh-spinner/rh-spinner.js';
 import '@rhds/elements/rh-stat/rh-stat.js';
 
 import '@rhds/elements/rh-context-provider/rh-context-provider.js';
+
+import { ColorContextConsumer } from '../../../lib/context/consumer';
+
+import { LitElement, html } from 'lit';
+
+customElements.define('test-context-consumer', class ContextConsumer extends LitElement {
+  #consumer = new ColorContextConsumer(this);
+  on = this.#consumer.value;
+  render() {
+    return html`
+      <div>
+        <p>Child on: ${this.on}</p>
+      </div>
+    `;
+  }
+
+  willUpdate() {
+    this.on = this.#consumer.value;
+  }
+});
+
