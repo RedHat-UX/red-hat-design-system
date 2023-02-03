2fe8366735ce1f81780758b6808091d286a83b6d
Nikki Massaro Kauffman <nmassaro@redhat.com>
2023-02-02 20:43:48 -0500

    docs(audio-player): made demo work for deploy preview

--- elements/rh-audio-player/demo/rh-audio-player.js
+++ elements/rh-audio-player/demo/rh-audio-player.js
@@ -3,13 +3,20 @@ import '@rhds/elements/rh-audio-player/rh-audio-player.js';
 const root = document.querySelector('[data-demo="rh-audio-player"]')?.shadowRoot ?? document;
 
 const form = root.querySelector('form');
-const player = root.querySelector('rh-audio-player');
+const player = root.querySelector('rh-audio-player#player');
+const playerRTL = root.querySelector('rh-audio-player#rtl');
 const transcript = root.querySelector('rh-audio-player-transcript#regular');
+const transcriptRTL = root.querySelector('#rtl rh-audio-player-transcript#regular');
 const detail = root.querySelector('rh-audio-player-transcript#detail');
+const detailRTL = root.querySelector('#rtl rh-audio-player-transcript#detail');
 const about = root.querySelector('rh-audio-player-about');
+const aboutRTL = root.querySelector('#rtl rh-audio-player-about');
 const subscribe = root.querySelector('rh-audio-player-subscribe');
-const mediaseries = player.querySelector('[slot="series"]');
-const mediatitle = player.querySelector('[slot="title"]');
+const subscribeRTL = root.querySelector('#rtl rh-audio-player-subscribe');
+const mediaseries = root.querySelector('[slot="series"]');
+const mediaseriesRTL = root.querySelector('#rtl [slot="series"]');
+const mediatitle = root.querySelector('[slot="title"]');
+const mediatitleRTL = root.querySelector('#rtl [slot="title"]');
 const { poster } = player;
 
 if (form) { form.addEventListener('input', sync); }
@@ -27,29 +34,38 @@ function sync() {
     if (slot) { slot.innerHTML = text || ''; }
     panel.label = text;
   };
+  const hidden = formObj.rtl ? player : playerRTL;
+  const shown = !formObj.rtl ? player : playerRTL;
   // source.src = formObj.transcript ? './sample2.mp3' : './sample.mp3';
-  transcript.slot = formObj.transcript ? '' : 'transcript';
-  detail.slot = !formObj.transcript ? '' : 'transcript';
+  transcript.slot = transcriptRTL.slot = formObj.transcript ? '' : 'transcript';
+  detail.slot = detailRTL.slot = !formObj.transcript ? '' : 'transcript';
   setLabel(transcript, formObj?.transcript);
   setLabel(detail, formObj?.transcript);
   setLabel(subscribe, formObj?.subscribe);
   setLabel(about, formObj?.about);
-  player.poster = !formObj.poster || formObj.palette === 'purple-img' ? undefined : poster;
-  mediaseries.innerHTML = formObj?.series || '';
-  mediatitle.innerHTML = formObj?.title || '';
-  mediaseries.slot = mediaseries.innerHTML.length > 0 ? 'series' : '';
-  mediatitle.slot = mediatitle.innerHTML.length > 0 ? 'title' : '';
-  player.mediaseries = mediaseries.innerHTML.length > 0 ? mediaseries.innerHTML : undefined;
-  player.mediatitle = mediatitle.innerHTML.length > 0 ? mediatitle.innerHTML : undefined;
-  player.mode = formObj.mode;
+  setLabel(transcriptRTL, formObj?.transcript);
+  setLabel(detailRTL, formObj?.transcript);
+  setLabel(subscribeRTL, formObj?.subscribe);
+  setLabel(aboutRTL, formObj?.about);
+  player.poster = playerRTL.poster = !formObj.poster || formObj.palette === 'purple-img' ? undefined : poster;
+  mediaseries.innerHTML = mediaseriesRTL.innerHTML = formObj?.series || '';
+  mediatitle.innerHTML = mediatitleRTL.innerHTML = formObj?.title || '';
+  mediaseries.slot = mediaseriesRTL.slot = mediaseries.innerHTML.length > 0 ? 'series' : '';
+  mediatitle.slot = mediatitleRTL.slot = mediatitle.innerHTML.length > 0 ? 'title' : '';
+  player.mediaseries = playerRTL.mediaseries = mediaseriesRTL.innerHTML.length > 0 ? mediaseriesRTL.innerHTML : undefined;
+  player.mediatitle = playerRTL.mediatitle = mediatitleRTL.innerHTML.length > 0 ? mediatitleRTL.innerHTML : undefined;
+  player.mode = playerRTL.mode = formObj.mode;
   if (on === player.on) {
     const oldOn = player.on;
-    player.on = oldOn === 'dark' ? 'light' : 'dark';
+    player.on = playerRTL.on = oldOn === 'dark' ? 'light' : 'dark';
   }
-  player.on = on;
+  player.on = playerRTL.on = on;
   player.setAttribute('class', formObj.palette);
-  setTimeout(()=>{ player.expanded = !player.expanded; }, 10);
-  setTimeout(()=>{ player.expanded = !player.expanded; }, 10);
+  playerRTL.setAttribute('class', formObj.palette);
+  setTimeout(()=>{ player.expanded = playerRTL.expanded = !player.expanded; }, 10);
+  setTimeout(()=>{ player.expanded = playerRTL.expanded = !player.expanded; }, 10);
+  hidden.setAttribute('hidden', 'hidden');
+  shown.removeAttribute('hidden');
 }
 
 if (form) { sync(); }
