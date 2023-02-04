dc7556beeb2d4080b5e92aae9cef894a8af55fa3
Benny Powers <web@bennypowers.com>
2023-02-04 19:57:27 +0200

    chore: update 11ty

---
+++ docs/_plugins/rhds.cjs
@@ -0,0 +1,87 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const slugify = require('slugify');
+const { exec: _exec } = require('node:child_process');
+const { promisify } = require('node:util');
+const exec = promisify(_exec);
+const RHDSAlphabetizeTagsPlugin = require('./alphabetize-tags.cjs');
+const RHDSShortcodesPlugin = require('./shortcodes.cjs');
+
+
+/** @param {import('@11ty/eleventy/src/UserConfig')} eleventyConfig */
+module.exports = function(eleventyConfig, { tagsToAlphabetize }) {
+  eleventyConfig.addPlugin(RHDSAlphabetizeTagsPlugin, { tagsToAlphabetize });
+
+  /** add `section`, `example`, `demo`, etc. shortcodes */
+  eleventyConfig.addPlugin(RHDSShortcodesPlugin);
+
+  /** format date strings */
+  eleventyConfig.addFilter('prettyDate', function(dateStr, options = {}) {
+    const { dateStyle = 'medium' } = options;
+    return new Intl.DateTimeFormat('en-US', { dateStyle })
+      .format(new Date(dateStr));
+  });
+
+  // Rewrite DEMO lightdom css relative URLs
+  const LIGHTDOM_HREF_RE = /href="\.(?<pathname>.*-lightdom\.css)"/g;
+  const LIGHTDOM_PATH_RE = /href="\.(.*)"/;
+  eleventyConfig.addTransform('demo-lightdom-css',
+    /**
+     * @param {string} content
+     * @this {{outputPath: string, inputPath: string}}
+     */
+    function(content) {
+      const { outputPath, inputPath } = this;
+      if (inputPath === './docs/components/demos.html') {
+        const matches = content.match(LIGHTDOM_HREF_RE);
+        if (matches) {
+          for (const match of matches) {
+            const [, path] = match.match(LIGHTDOM_PATH_RE) ?? [];
+            const { pathname } = new URL(path, `file:///${outputPath}`);
+            content = content.replace(`.${path}`, pathname
+              .replace('/_site/components/', '/assets/elements/rh-')
+              .replace('/demo/', '/'));
+          }
+        }
+      }
+      return content;
+    });
+
+  // generate a bundle that packs all of rhds with all dependencies
+  // into a single large javascript file
+  eleventyConfig.on('eleventy.before', async () =>
+    import('./scripts/bundle.js')
+      .then(m => m.build({
+        outfile: '_site/assets/rhds.min.js',
+        external: [],
+        additionalPackages: [
+          'lit',
+          ...Object.keys(require(path.join(__dirname, 'package.json')).dependencies)
+            .filter(k => k.startsWith('@patternfly')),
+        ],
+      })));
+
+  eleventyConfig.on('eleventy.before', async ({ runMode }) => {
+    if (runMode === 'watch') {
+      await exec('npx cem analyze');
+    }
+  });
+
+  // Copy element demo files
+  const repoRoot = process.cwd();
+  const elements = fs.readdirSync(path.join(repoRoot, 'elements'));
+
+  const config = require('./.pfe.config.json');
+  const aliases = config.aliases ?? {};
+  const getSlug = tagName => slugify(aliases[tagName] ?? tagName.replace('rh-', '')).toLowerCase();
+
+  eleventyConfig.addPassthroughCopy(Object.fromEntries(elements.flatMap(element => {
+    const slug = getSlug(element);
+    return [
+      [
+        `elements/${element}/demo/**/*.{css,js,png,svg,jpg,webp}`,
+        `components/${slug}/demo`,
+      ],
+    ];
+  })));
+};
---
+++ eleventy.config.cjs
@@ -0,0 +1,104 @@
+// @ts-check
+Error.stackTraceLimit = 50;
+
+const SyntaxHighlightPlugin = require('@11ty/eleventy-plugin-syntaxhighlight');
+const DirectoryOutputPlugin = require('@11ty/eleventy-plugin-directory-output');
+const AnchorsPlugin = require('@patternfly/pfe-tools/11ty/plugins/anchors.cjs');
+const CustomElementsManifestPlugin = require('@patternfly/pfe-tools/11ty/plugins/custom-elements-manifest.cjs');
+const OrderTagsPlugin = require('@patternfly/pfe-tools/11ty/plugins/order-tags.cjs');
+const TodosPlugin = require('@patternfly/pfe-tools/11ty/plugins/todos.cjs');
+const TOCPlugin = require('@patternfly/pfe-tools/11ty/plugins/table-of-contents.cjs');
+const SassPlugin = require('eleventy-plugin-dart-sass');
+const RHDSPlugin = require('./docs/_plugins/rhds.cjs');
+
+const path = require('node:path');
+
+/** @param {import('@11ty/eleventy/src/UserConfig')} eleventyConfig */
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(SassPlugin, {
+    sassLocation: `${path.join(__dirname, 'docs', 'scss')}/`,
+    sassIndexFile: 'styles.scss',
+    includePaths: ['node_modules', '**/*.{scss,sass}'],
+    domainName: '',
+    outDir: path.join(__dirname, '_site'),
+  });
+
+  /** Table of Contents Shortcode */
+  eleventyConfig.addPlugin(TOCPlugin, {
+    tags: ['h2', 'h3', 'h4', 'h5', 'h6'],
+    wrapperClass: 'table-of-contents',
+    headingText: 'Table of Contents'
+  });
+
+  /** Generate and consume custom elements manifests */
+  eleventyConfig.addPlugin(CustomElementsManifestPlugin);
+
+  /** Collections to organize by order instead of date */
+  eleventyConfig.addPlugin(OrderTagsPlugin, { tags: ['develop'] });
+
+  /** list todos */
+  eleventyConfig.addPlugin(TodosPlugin);
+
+  /** fancy syntax highlighting with diff support */
+  eleventyConfig.addPlugin(SyntaxHighlightPlugin);
+
+  /** Add IDs to heading elements */
+  eleventyConfig.addPlugin(AnchorsPlugin, {
+    exclude: /\/components\/.*\/demo\//,
+    formatter($, existingids) {
+      if (
+        !existingids.includes($.attr('id')) &&
+        $.attr('slot') &&
+        $.closest('pf-card')
+      ) {
+        return null;
+      } else {
+        return eleventyConfig.getFilter('slug')($.text())
+          .replace(/[&,+()$~%.'":*?!<>{}]/g, '');
+      }
+    },
+  });
+
+  eleventyConfig.addPlugin(DirectoryOutputPlugin, {
+    // Customize columns
+    columns: {
+      filesize: true, // Use `false` to disable
+      benchmark: true, // Use `false` to disable
+    },
+
+    // Will show in yellow if greater than this number of bytes
+    warningFileSize: 400 * 1000,
+  });
+
+  /**
+   * Collections to organize by 'order' value in front matter, then alphabetical by title;
+   * instead of by date
+   */
+  eleventyConfig.addPlugin(RHDSPlugin, {
+    tagsToAlphabetize: [
+      'component',
+      'foundations',
+      'getstarted',
+    ]
+  });
+
+  eleventyConfig.amendLibrary('md', md => md.use(markdownItAnchor));
+  eleventyConfig.setQuietMode(true);
+
+  eleventyConfig.addPassthroughCopy('docs/public/red-hat-outfit.css');
+  eleventyConfig.addPassthroughCopy('docs/CNAME');
+  eleventyConfig.addPassthroughCopy('docs/.nojekyll');
+  eleventyConfig.addPassthroughCopy('docs/robots.txt');
+  eleventyConfig.addPassthroughCopy('docs/assets/**/*');
+  eleventyConfig.addPassthroughCopy('docs/js/**/*');
+  eleventyConfig.addPassthroughCopy({ 'elements': 'assets/elements/' });
+  eleventyConfig.addPassthroughCopy({ 'lib': 'assets/lib/' });
+
+  return {
+    templateFormats: ['html', 'md', 'njk', '11ty.cjs'],
+    markdownTemplateEngine: 'njk',
+    dir: {
+      input: './docs',
+    },
+  };
+};
