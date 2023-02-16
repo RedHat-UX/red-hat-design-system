2a00979ba57d4732e38273e4126d4ac7b003a526
Benny Powers <web@bennypowers.com>
2023-02-16 12:46:30 +0200

    refactor(audio-player)!: range event

--- elements/rh-audio-player/rh-audio-player-range.ts
+++ elements/rh-audio-player/rh-audio-player-range.ts
@@ -5,57 +5,66 @@ import { classMap } from 'lit/directives/class-map.js';
 import { colorContextConsumer, type ColorTheme } from '../../lib/context/color/consumer.js';
 import styles from './rh-audio-player-range.css';
 
+export class AudioPlayerRangeInputEvent extends Event {
+  constructor(
+    public value: number,
+    public originalEvent: Event,
+  ) {
+    super('input', { bubbles: true });
+  }
+}
 /**
  * Audio Player Range Slider
  * @cssprop --rh-audio-player-range-thumb-color -  - {@default var(--rh-color-brand-red-on-light, #ee0000)}
  * @cssprop --rh-audio-player-range-progress-color -  - {@default var(--rh-audio-player-range-thumb-color, #ee0000)}
+ * @fires {AudioPlayerRangeInputEvent} input - when the range changes
  */
 @customElement('rh-audio-player-range')
 export class RhAudioPlayerRange extends LitElement {
   static readonly styles = [styles];
 
   @property({ reflect: true, type: Boolean }) disabled = false;
+
   @property({ reflect: true, type: Boolean }) hidden = false;
+
   @property({ reflect: true, type: Boolean }) readonly = false;
+
   @property({ type: Number }) min = 0;
+
   @property({ type: Number }) max = 1;
-  @property({ type: Number }) step = undefined;
-  @property({ type: Number }) value = undefined;
+
+  @property({ type: Number }) step?: number;
+
+  @property({ type: Number }) value?: number;
 
   @colorContextConsumer()
-  @property() on?:ColorTheme;
+  @property() on?: ColorTheme;
 
-  get #stepAttribute():number|undefined {
-    return this.step ? this.step : undefined;
-  }
-
-  get #valueAttribute():number|undefined {
-    return this.value ? this.value : undefined;
-  }
+  #style = getComputedStyle(this);
 
   render() {
-    const dir = getComputedStyle(this).direction || 'auto';
-    return html`<input 
-      class="${classMap({ [this.on || 'light']: !!this.on, [dir]: !!dir })}"
-      ?disabled="${this.disabled}"
-      ?readonly="${this.readonly}"
-      type="range" 
-      min="${this.min}" 
-      max="${this.max}" 
-      @input=${this.#handleInput}
-      step=${ifDefined(this.#stepAttribute)}
-      value=${ifDefined(this.#valueAttribute)}>
+    const { on = '' } = this;
+    const { direction } = this.#style;
+    return html`
+      <input class="${classMap({ [on]: !!on, [direction || 'auto']: true })}"
+             type="range"
+             min="${this.min}"
+             max="${this.max}"
+             step=${ifDefined(this.step || undefined)}
+             value=${ifDefined(this.value || undefined)}
+             ?disabled="${this.disabled}"
+             ?readonly="${this.readonly}"
+             @input=${this.#handleInput}>
     `;
   }
 
   /**
    * handles time input changes by seeking to input value
    */
-  #handleInput(event:Event):void {
+  #handleInput(event: Event & { target: HTMLInputElement }) {
     event.stopPropagation();
-    const target = event.target as HTMLInputElement;
-    this.setAttribute('value', target?.value);
-    this.dispatchEvent(new CustomEvent('input', { ...event, detail: parseFloat(target?.value) as number }));
+    this.setAttribute('value', event.target?.value);
+    return this.dispatchEvent(new AudioPlayerRangeInputEvent(parseFloat(event.target.value), event));
   }
 }
 
