a3bc12fa1960da63fe55f33325a18be432afca14
Benny Powers <web@bennypowers.com>
2023-02-16 10:00:55 +0200

    refactor(audio-player): player about

--- elements/rh-audio-player/rh-audio-player-about.ts
+++ elements/rh-audio-player/rh-audio-player-about.ts
@@ -1,11 +1,13 @@
 import { LitElement, html } from 'lit';
-import { customElement, property, query, queryAssignedElements } from 'lit/decorators.js';
+import { customElement, property, queryAll, queryAssignedElements } from 'lit/decorators.js';
+
 import { HeadingController } from '../../lib/HeadingController.js';
 import { RhAudioPlayerScrollingTextOverflow } from './rh-audio-player-scrolling-text-overflow.js';
+
 import './rh-audio-player-profile.js';
+
 import panelStyles from './rh-audio-player-panel-styles.css';
 import styles from './rh-audio-player-about.css';
-
 
 /**
  * Audio Player About Panel
@@ -17,48 +19,49 @@ import styles from './rh-audio-player-about.css';
 export class RhAudioPlayerAbout extends LitElement {
   static readonly styles = [panelStyles, styles];
 
-  @property({ type: String }) label = 'About the Episode';
-  @property({ type: String, attribute: 'series' }) mediaseries?:string;
-  @property({ type: String, attribute: 'mediatitle' }) mediatitle?:string;
-  @query('.media-info') mediaInfo?:HTMLElement;
-  @query('#mediaseries') private _seriesScroller?: RhAudioPlayerScrollingTextOverflow;
-  @query('#mediatitle') private _mediatitleScroller?: RhAudioPlayerScrollingTextOverflow;
-  @query('#title') private _titleScroller?: RhAudioPlayerScrollingTextOverflow;
-  @queryAssignedElements() private _body!: HTMLElement[];
+  /** Default label content */
+  @property() label = 'About the Episode';
+
+  /** Series this track belongs to, if applicable */
+  @property({ attribute: 'series' }) mediaseries?: string;
+
+  /** Title of the audio track */
+  @property({ attribute: 'mediatitle' }) mediatitle?: string;
+
+  @queryAll('rh-audio-player-scrolling-text-overflow')
+  private scrollers?: NodeListOf<RhAudioPlayerScrollingTextOverflow>;
+
+  @queryAssignedElements() private content!: HTMLElement[];
 
   #headingLevelController = new HeadingController(this);
 
   override render() {
+    const { label, mediaseries, mediatitle } = this;
+    const hasContent = this.content.length >= 1;
+    const heading = this.#headingLevelController.headingTemplate(mediatitle ?? '', {
+      level: this.#headingLevelController.headingLevel - 1
+    });
+
     return html`
       <rh-audio-player-scrolling-text-overflow id="title" part="heading">
-        <slot name="heading">${this.label}</slot>
-      </rh-audio-player-scrolling-text-overflow>
-      ${this.#headingContent()}
-      <div part="body" ?hidden="${this._body.length < 1}"><slot></slot></div>
+        <slot name="heading">${label}</slot>
+      </rh-audio-player-scrolling-text-overflow>${!mediatitle ? `` : !mediaseries ? heading : html`
+      <hgroup class="media-info" part="heading">${!mediaseries ? '' : html`
+        <rh-audio-player-scrolling-text-overflow id="mediaseries">
+          ${mediaseries}
+        </rh-audio-player-scrolling-text-overflow>`}
+        <rh-audio-player-scrolling-text-overflow id="mediatitle">
+          ${heading}
+        </rh-audio-player-scrolling-text-overflow>
+      </hgroup>`}
+      <div part="body" ?hidden="${!hasContent}"><slot></slot></div>
       <slot part="profile" name="profile"></slot>`;
   }
 
-  #headingContent() {
-    const heading = this.#headingLevelController.headingTemplate(html`${this.mediatitle}`, { level: this.#headingLevelController.headingLevel - 1 });
-
-    return !this.mediatitle ? ``
-      : !this.mediaseries ? heading
-      : html`
-        <hgroup class="media-info" part="heading">
-          ${!this.mediaseries ? '' : html`
-            <rh-audio-player-scrolling-text-overflow id="mediaseries">
-              ${this.mediaseries}
-            </rh-audio-player-scrolling-text-overflow>
-          `}
-          <rh-audio-player-scrolling-text-overflow id="mediatitle">
-            ${heading}
-          </rh-audio-player-scrolling-text-overflow>
-        </hgroup>
-      `;
-  }
-
   scrollText() {
-    [this._titleScroller, this._seriesScroller, this._mediatitleScroller].forEach(scroller=>scroller?.startScrolling());
+    for (const scroller of this.scrollers ?? []) {
+      scroller?.startScrolling();
+    }
   }
 }
 
