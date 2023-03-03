0ed6700c0ab7ed9b43b0c822d5141e9bb785e73e
Steven Spriggs <steven.spriggs@gmail.com>
2023-03-03 10:58:10 -0500

    feat(tabs): add `<rh-tabs>` (#634)
    * chore(tabs): update package.json add initial files
    
    * fix(tabs): add variant and color context and basic styles
    
    * fix(tabs): rename e2e test
    
    * fix(tabs): missing extension on import
    
    * fix(tabs): more missing extension on import
    
    * fix(tabs): add inset and box styles
    
    * fix(tabs): text color context
    
    * refactor(tabs): remove reflected inset property
    
    * fix(tabs): implement abstract properties
    
    * docs(tabs): fix initial surface color
    
    * fix(tabs): update package-lock.json
    
    * docs(tabs): add slotted icon example to demo
    
    * test(tabs): add spec tests
    
    * fix(tabs): correct css token values
    
    * docs(tabs): add changeset
    
    * docs(tabs): add set and size
     to icons in demo
    
    * docs(tabs): remove extraneous close tag
    
    * docs(tabs): add pfe-icon to demo js
    
    * docs: add and update demos, readme
    
    * fix: make `on` private
    
    * docs(tabs): context-range width
    
    * fix(tabs): first and last tab border
    
    * fix(tabs): removes extra border from overflow when box variant
    
    * fix(tabs): disabled and inset styles
    
    * fix(tabs): padding and indent panel margins
    
    * fix(tabs): vertical panel grid
    
    * fix(tabs): vertical box button padding
    
    * fix(tabs): panel when not inset
    
    * fix(tabs): panel spacing based off host variant
    
    * fix(tabs): hover color for next/previous buttonsa
    
    * chore(tabs): update package.json getting latest pfe-tabs
    
    * fix(tabs): adds box inset, removes inset as attribute
    
    * fix(tabs): set default context css vars
    
    * chore(tabs): add element internals polyfill
    
    * fix: add element-internals-polyfill to importMap
    
    * chore: update tokens to latest version
    
    * test(tabs): fix tab with updated box inset attribute
    
    * fix(tabs): add centered attribute, left align default
    
    * fix(tabs): update imports
    
    * fix(tabs): styles with context refactor
    
    * fix(tabs): fix psuedo element selectors
    
    * test(tabs): remove broken tests
    
    * fix(tabs): update pf imports
    
    * fix(tabs): update demo links
    
    * test(tabs): correct import for RhTab
    
    * fix(tabs): correct pf import in demo
    
    ---------
    
    Co-authored-by: Benny Powers <web@bennypowers.com>
    Co-authored-by: Benny Powers <bennypowers@users.noreply.github.com>

---
+++ elements/rh-tabs/rh-tab-panel.ts
@@ -0,0 +1,50 @@
+import { html } from 'lit';
+import { customElement } from 'lit/decorators/custom-element.js';
+import { property } from 'lit/decorators/property.js';
+
+import { classMap } from 'lit/directives/class-map.js';
+
+import { BaseTabPanel } from '@patternfly/elements/pf-tabs/BaseTabPanel.js';
+
+import { colorContextConsumer, type ColorTheme } from '../../lib/context/color/consumer.js';
+import { colorContextProvider, type ColorPalette } from '../../lib/context/color/provider.js';
+
+import styles from './rh-tab-panel.css';
+
+/**
+ * Tabs
+ * @slot - Place element content here
+ */
+@customElement('rh-tab-panel')
+export class RhTabPanel extends BaseTabPanel {
+  static readonly version = '{{version}}';
+
+  static readonly styles = [styles];
+
+  /**
+   * Sets color theme based on parent context
+   */
+  @colorContextConsumer() private on?: ColorTheme;
+
+  /**
+   * Sets color palette, which affects the element's styles as well as descendants' color theme.
+   * Overrides parent color context.
+   * Your theme will influence these colors so check there first if you are seeing inconsistencies.
+   * See [CSS Custom Properties](#css-custom-properties) for default values
+   */
+  @colorContextProvider()
+  @property({ reflect: true, attribute: 'color-palette' }) colorPalette?: ColorPalette;
+
+  render() {
+    const { on = '' } = this;
+    return html`
+      <div id="rhds-container" class="${classMap({ [on]: !!on })}">${super.render()}</div>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'rh-tab-panel': RhTabPanel;
+  }
+}
