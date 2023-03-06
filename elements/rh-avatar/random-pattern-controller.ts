f7ecb343212f1da94b97414916cb2db0d8d3f6ec
Benny Powers <web@bennypowers.com>
2023-03-06 17:53:50 +0200

    perf(avatar): lazy load the pattern generator

---
+++ elements/rh-avatar/random-pattern-controller.ts
@@ -0,0 +1,237 @@
+import type { ReactiveController, ReactiveElement } from 'lit';
+
+import { Logger } from '@patternfly/pfe-core/controllers/logger.js';
+
+import { hash } from './lib/djb-hash.js';
+import { hsl2rgb, rgb2hsl, type RGBTriple } from './lib/hslrgb.js';
+
+type Vector2D = [x: number, y: number];
+
+export interface Options {
+  name: string;
+  pattern: 'squares'|'triangles';
+  size: number;
+  canvas: HTMLCanvasElement;
+  colors: string[];
+}
+
+const HEX_PARSERS = {
+  // ex: "#0fc"
+  [4 as number]: {
+    regexp: /^#([A-f0-9])([A-f0-9])([A-f0-9])$/,
+    parser: (c: string) => parseInt(`${c}${c}`, 16),
+  },
+  // ex: "#00ffcc"
+  [7 as number]: {
+    regexp: /^#([A-f0-9]{2})([A-f0-9]{2})([A-f0-9]{2})$/,
+    parser: (c: string) => parseInt(c, 16),
+  },
+};
+
+function adjustColor(color: RGBTriple): RGBTriple {
+  const dark = 0.1;
+  const lAdj = 0.1; // luminance adjustment
+  const hsl = rgb2hsl(...color);
+
+  // if luminance is too dark already, then lighten the alternate color
+  // instead of darkening it.
+  hsl[2] += hsl[2] > dark ? -lAdj : lAdj;
+
+  return hsl2rgb(...hsl);
+}
+
+export class RandomPatternController implements ReactiveController {
+  #colorTuples: [string, string][];
+
+  #colors: string[];
+
+  #name: string;
+
+  #pattern: 'squares'|'triangles' = 'squares';
+
+  #squareSize = 0;
+
+  #triangleSize = 0;
+
+  #color1 = '';
+
+  #color2 = '';
+
+  #canvas: HTMLCanvasElement|null;
+
+  #ctx: CanvasRenderingContext2D;
+
+  #logger: Logger;
+
+  constructor(host: ReactiveElement, { name, pattern, size, canvas, colors }: Options) {
+    this.#logger = new Logger(host);
+
+    this.#name = name;
+    this.#pattern = pattern ?? 'squares';
+    this.#colors = colors;
+    this.#canvas = canvas;
+
+    this.#canvas.width = size;
+    this.#canvas.height = size;
+
+    this.#squareSize = this.#canvas.width / 8;
+    this.#triangleSize = this.#canvas.width / 4;
+
+    this.#ctx = this.#canvas.getContext('2d') as CanvasRenderingContext2D;
+    this.#colorTuples = this.#initColorTuples();
+    this.#update();
+  }
+
+  hostConnected?(): void
+
+  #update(options?: Options) {
+    this.#name = options?.name ?? this.#name;
+    this.#colors = options?.colors ?? this.#colors;
+    this.#pattern = options?.pattern ?? this.#pattern;
+    if (options?.colors) { this.#colorTuples = this.#initColorTuples(options.colors); }
+    const bitPattern = hash(this.#name).toString(2);
+    const arrPattern = bitPattern.split('').map(n => Number(n)) as Vector2D;
+    const index = Math.floor((this.#colorTuples.length * parseInt(bitPattern, 2)) / (2 ** 32));
+    [this.#color1, this.#color2] = this.#colorTuples[index] ?? [];
+    this.#clear();
+    this.#drawBackground();
+    if (this.#pattern === 'squares') {
+      this.#drawSquarePattern(arrPattern);
+    } else if (this.#pattern === 'triangles') {
+      this.#drawTrianglePattern(arrPattern);
+    }
+  }
+
+  #initColorTuples(colors = this.#colors) {
+    const tuples: [string, string][] = [];
+    colors.forEach(colorCode => {
+      const { regexp, parser } = HEX_PARSERS[colorCode.length] ?? {};
+      if (regexp && parser) {
+        const [, ...pattern] = regexp.exec(colorCode) ?? [];
+        if (pattern.length) {
+          const color = pattern.map(parser) as RGBTriple;
+          tuples.push([
+            `rgb(${color.join(',')})`,
+            `rgb(${adjustColor(color).join(',')})`,
+          ]);
+        } else {
+          this.#logger.warn(`invalid color ${colorCode}`);
+        }
+      }
+    });
+    return tuples;
+  }
+
+  #clear() {
+    if (this.#ctx && this.#canvas) {
+      this.#ctx.clearRect(0, 0, this.#canvas.width, this.#canvas.height);
+    }
+  }
+
+  #drawBackground() {
+    if (this.#ctx && this.#canvas) {
+      this.#ctx.fillStyle = this.#color1;
+      this.#ctx.fillRect(0, 0, this.#canvas.width, this.#canvas.height);
+    }
+  }
+
+  #drawSquarePattern(pattern: Vector2D) {
+    if (this.#ctx) {
+      this.#ctx.fillStyle = this.#color2;
+      let i = pattern.length;
+      while (i--) {
+        if (pattern[i]) {
+          this.#drawMirroredSquare(i % 4, Math.floor(i / 4));
+        }
+      }
+    }
+  }
+
+  /**
+   * Draw a square at the given position, mirrored onto both the left and right half of the canvas.
+   */
+  #drawMirroredSquare(x: number, y: number) {
+    if (this.#ctx) {
+      this.#drawSquare(x, y);
+      this.#drawSquare(7 - x, y);
+    }
+  }
+
+  #drawSquare(x: number, y: number) {
+    if (this.#ctx) {
+      this.#ctx.fillRect(
+        this.#squareSize * x,
+        this.#squareSize * y,
+        this.#squareSize,
+        this.#squareSize
+      );
+    }
+  }
+
+  #drawTrianglePattern(pattern: number[]) {
+    if (this.#ctx) {
+      this.#ctx.fillStyle = this.#color2 ?? '';
+      let i = pattern.length;
+      while (i--) {
+        if (pattern[i]) {
+          const x = Math.floor(i / 2) % 2;
+          const y = Math.floor(i / 4);
+          const alt = i % 4;
+
+          const p1: Vector2D = [x, y];
+          const p2: Vector2D = [x, y];
+          const p3: Vector2D = [x, y];
+
+          switch (alt) {
+            case 0:
+              p2[1]++;
+              p3[0]++;
+              p3[1]++;
+              break;
+            case 1:
+              p2[0]++;
+              p3[0]++;
+              p3[1]++;
+              break;
+            case 2:
+              p2[0]++;
+              p3[1]++;
+              break;
+            case 3:
+              p1[0]++;
+              p2[0]++;
+              p2[1]++;
+              p3[1]++;
+              break;
+          }
+
+          this.#drawMirroredTriangle(p1, p2, p3);
+        }
+      }
+    }
+  }
+
+  /**
+   * Draw a square at the given position in the top-left quadrant of the
+   * canvas, and mirrored to the other three quadrants.
+   */
+  #drawMirroredTriangle(p1: Vector2D, p2: Vector2D, p3: Vector2D) {
+    if (this.#ctx) {
+      this.#drawTriangle(p1, p2, p3);
+      this.#drawTriangle([4 - p1[0], p1[1]], [4 - p2[0], p2[1]], [4 - p3[0], p3[1]]);
+    }
+  }
+
+  #drawTriangle(p1: Vector2D, p2: Vector2D, p3: Vector2D) {
+    const mult = (c: number) => c * this.#triangleSize;
+    if (this.#ctx) {
+      this.#ctx.beginPath();
+      this.#ctx.moveTo(...p1.map(mult) as Vector2D);
+      this.#ctx.lineTo(...p2.map(mult) as Vector2D);
+      this.#ctx.lineTo(...p3.map(mult) as Vector2D);
+      this.#ctx.closePath();
+      this.#ctx.fill();
+      this.#ctx.fill();
+    }
+  }
+}
--- elements/rh-avatar/rh-avatar.ts
+++ elements/rh-avatar/rh-avatar.ts
@@ -1,3 +1,5 @@
+import type { RandomPatternController } from './random-pattern-controller.js';
+
 import { customElement } from 'lit/decorators/custom-element.js';
 import { property } from 'lit/decorators/property.js';
 import { html, type PropertyValues } from 'lit';
@@ -6,27 +8,9 @@ import { Logger } from '@patternfly/pfe-core/controllers/logger.js';
 
 import { colorContextConsumer, type ColorTheme } from '../../lib/context/color/consumer.js';
 
-import { hash } from './lib/djb-hash.js';
-import { hsl2rgb, rgb2hsl, type RGBTriple } from './lib/hslrgb.js';
-
 import { BaseAvatar } from '@patternfly/elements/pf-avatar/BaseAvatar.js';
 
 import styles from './rh-avatar.css';
-
-type Vector2D = [x: number, y: number];
-
-const HEX_PARSERS = {
-  // ex: "#0fc"
-  [4 as number]: {
-    regexp: /^#([A-f0-9])([A-f0-9])([A-f0-9])$/,
-    parser: (c: string) => parseInt(`${c}${c}`, 16),
-  },
-  // ex: "#00ffcc"
-  [7 as number]: {
-    regexp: /^#([A-f0-9]{2})([A-f0-9]{2})([A-f0-9]{2})$/,
-    parser: (c: string) => parseInt(c, 16),
-  },
-};
 
 /**
  * An avatar is a visual used to represent a user. It may contain an image or
@@ -65,7 +49,7 @@ export class RhAvatar extends BaseAvatar {
   /**
    * The type of pattern to display.
    */
-  @property({ reflect: true }) pattern: 'squares'|'triangles' = 'squares';
+  @property({ reflect: true }) pattern?: 'squares'|'triangles';
 
   /**
   * The shape of the avatar itself.
@@ -76,226 +60,42 @@ export class RhAvatar extends BaseAvatar {
 
   #logger = new Logger(this);
 
-  #colors?: [string, string][];
-  #squareSize = 0;
-  #triangleSize = 0;
-  #colorIndex = -1;
-  #color1 = '';
-  #color2 = '';
-  #canvas?: HTMLCanvasElement;
-  #ctx?: CanvasRenderingContext2D;
+  #pattern?: RandomPatternController;
 
   render() {
     return html`
-      <canvas part="canvas" ?hidden=${!!this.src}></canvas>
-      <img .src="${this.src}" ?hidden=${!this.src} alt="" part="img">
+      <div id="container">${this.pattern ? html`
+        <canvas part="canvas"></canvas>` : html`
+        <img .src="${this.src ?? ''}" ?hidden=${!this.src} alt="" part="img">`}
+        <slot></slot>
+        <slot name="subtitle"></slot>
+      </div>
     `;
   }
 
-  firstUpdated() {
+  connectedCallback() {
+    super.connectedCallback();
     this.#style = getComputedStyle(this);
-    try {
-      this.#initCanvas();
-      this.dispatchEvent(new Event('ready'));
-    } catch (e: any) {
-      this.#logger.warn(e.message);
-    }
   }
 
-  updated(changed: PropertyValues<this>) {
-    if (changed.has('name') || changed.has('src') || changed.has('pattern')) {
-      this.#update();
-    }
-  }
-
-  #initCanvas() {
-    this.#canvas ??= this.shadowRoot?.querySelector('canvas') ?? undefined;
-
-    if (!this.#canvas) {
-      throw new Error('canvas unavailable');
-    }
-
-    const size = parseInt(this.#style?.getPropertyValue('width') ?? '0');
-
-    this.#canvas.width = size;
-    this.#canvas.height = size;
-
-    this.#squareSize = this.#canvas.width / 8;
-    this.#triangleSize = this.#canvas.width / 4;
-
-    this.#ctx = this.#canvas.getContext('2d') as CanvasRenderingContext2D;
-    this.#update();
-  }
-
-  #update() {
-    // if we have a src element, update the img, otherwise update the random pattern
-    if (!this.src) {
-      this.#colors ??= this.#initColors();
-      const bitPattern = hash(this.name).toString(2);
-      const arrPattern = bitPattern.split('').map(n => Number(n)) as Vector2D;
-      this.#colorIndex = Math.floor(
-        (this.#colors.length * parseInt(bitPattern, 2)) /
-        Math.pow(2, 32)
-      );
-      [this.#color1, this.#color2] = this.#colors[this.#colorIndex] ?? [];
-
-      this.#clear();
-      this.#drawBackground();
-      if (this.pattern === 'squares') {
-        this.#drawSquarePattern(arrPattern);
-      } else if (this.pattern === 'triangles') {
-        this.#drawTrianglePattern(arrPattern);
-      }
-    }
-  }
-
-  #initColors() {
-    const colors: [string, string][] = [];
-
-    this.#style?.getPropertyValue('--rh-avatar-colors')?.split(/\s+/).forEach(colorCode => {
-      const { regexp, parser } = HEX_PARSERS[colorCode.length] ?? {};
-      if (regexp && parser) {
-        const [, ...pattern] = regexp.exec(colorCode) ?? [];
-        if (pattern.length) {
-          const color = pattern.map(parser) as RGBTriple;
-          colors.push([
-            `rgb(${color.join(',')})`,
-            `rgb(${adjustColor(color).join(',')})`,
-          ]);
-        } else {
-          this.#logger.warn(`invalid color ${colorCode}`);
-        }
-      }
-    });
-
-    return colors;
-  }
-
-  #clear() {
-    if (this.#ctx && this.#canvas) {
-      this.#ctx.clearRect(0, 0, this.#canvas.width, this.#canvas.height);
-    }
-  }
-
-  #drawBackground() {
-    if (this.#ctx && this.#canvas) {
-      this.#ctx.fillStyle = this.#color1;
-      this.#ctx.fillRect(0, 0, this.#canvas.width, this.#canvas.height);
-    }
-  }
-
-  #drawSquarePattern(pattern: Vector2D) {
-    if (this.#ctx) {
-      this.#ctx.fillStyle = this.#color2;
-      let i = pattern.length;
-      while (i--) {
-        if (pattern[i]) {
-          this.#drawMirroredSquare(i % 4, Math.floor(i / 4));
-        }
-      }
-    }
-  }
-
-  /**
-   * Draw a square at the given position, mirrored onto both the left and right half of the canvas.
-   */
-  #drawMirroredSquare(x: number, y: number) {
-    if (this.#ctx) {
-      this.#drawSquare(x, y);
-      this.#drawSquare(7 - x, y);
-    }
-  }
-
-  #drawSquare(x: number, y: number) {
-    if (this.#ctx) {
-      this.#ctx.fillRect(
-        this.#squareSize * x,
-        this.#squareSize * y,
-        this.#squareSize,
-        this.#squareSize
-      );
-    }
-  }
-
-  #drawTrianglePattern(pattern: number[]) {
-    if (this.#ctx) {
-      this.#ctx.fillStyle = this.#color2 ?? '';
-      let i = pattern.length;
-      while (i--) {
-        if (pattern[i]) {
-          const x = Math.floor(i / 2) % 2;
-          const y = Math.floor(i / 4);
-          const alt = i % 4;
-
-          const p1: Vector2D = [x, y];
-          const p2: Vector2D = [x, y];
-          const p3: Vector2D = [x, y];
-
-          switch (alt) {
-            case 0:
-              p2[1]++;
-              p3[0]++;
-              p3[1]++;
-              break;
-            case 1:
-              p2[0]++;
-              p3[0]++;
-              p3[1]++;
-              break;
-            case 2:
-              p2[0]++;
-              p3[1]++;
-              break;
-            case 3:
-              p1[0]++;
-              p2[0]++;
-              p2[1]++;
-              p3[1]++;
-              break;
-          }
-
-          this.#drawMirroredTriangle(p1, p2, p3);
-        }
-      }
-    }
-  }
-
-  /**
-   * Draw a square at the given position in the top-left quadrant of the
-   * canvas, and mirrored to the other three quadrants.
-   */
-  #drawMirroredTriangle(p1: Vector2D, p2: Vector2D, p3: Vector2D) {
-    if (this.#ctx) {
-      this.#drawTriangle(p1, p2, p3);
-      this.#drawTriangle([4 - p1[0], p1[1]], [4 - p2[0], p2[1]], [4 - p3[0], p3[1]]);
-    }
-  }
-
-  #drawTriangle(p1: Vector2D, p2: Vector2D, p3: Vector2D) {
-    const mult = (c: number) => c * this.#triangleSize;
-    if (this.#ctx) {
-      this.#ctx.beginPath();
-      this.#ctx.moveTo(...p1.map(mult) as Vector2D);
-      this.#ctx.lineTo(...p2.map(mult) as Vector2D);
-      this.#ctx.lineTo(...p3.map(mult) as Vector2D);
-      this.#ctx.closePath();
-      this.#ctx.fill();
-      this.#ctx.fill();
+  async updated(changed: PropertyValues<this>) {
+    if (changed.has('pattern') && this.pattern) {
+      const { RandomPatternController } = await import('./random-pattern-controller.js');
+      const { name, pattern } = this;
+      const size = parseInt(this.#style?.getPropertyValue('width') ?? '0');
+      const canvas = this.shadowRoot?.querySelector('canvas') as HTMLCanvasElement;
+      const colors = this.#style?.getPropertyValue('--rh-avatar-colors')?.split(/\s+/) ?? [];
+      this.#pattern ??= new RandomPatternController(this, {
+        name,
+        pattern,
+        canvas,
+        size,
+        colors,
+      });
     }
   }
 }
 
-function adjustColor(color: RGBTriple): RGBTriple {
-  const dark = 0.1;
-  const lAdj = 0.1; // luminance adjustment
-  const hsl = rgb2hsl(...color);
-
-  // if luminance is too dark already, then lighten the alternate color
-  // instead of darkening it.
-  hsl[2] += hsl[2] > dark ? -lAdj : lAdj;
-
-  return hsl2rgb(...hsl);
-}
 
 declare global {
   interface HTMLElementTagNameMap {
