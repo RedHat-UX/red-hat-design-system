79dee15ddda2830f2bc702c495af7c2fed3e564a
Benny Powers <web@bennypowers.com>
2023-03-07 11:54:49 +0200

    test(avatar): basic tests

--- elements/rh-avatar/test/rh-avatar.spec.ts
+++ elements/rh-avatar/test/rh-avatar.spec.ts
@@ -1,11 +1,11 @@
 import { expect, html } from '@open-wc/testing';
 import { createFixture } from '@patternfly/pfe-tools/test/create-fixture.js';
-import { RhAvatar } from '@rhds/elements/rh-avatar/rh-avatar.js';
+import { RhAvatar } from '../rh-avatar.js';
 
 describe('<rh-avatar>', function() {
   describe('simply instantiating', function() {
     let element: RhAvatar;
-    before(async function() {
+    beforeEach(async function() {
       element = await createFixture<RhAvatar>(html`<rh-avatar></rh-avatar>`);
     });
     it('should upgrade', async function() {
@@ -13,9 +13,6 @@ describe('<rh-avatar>', function() {
         .to.be.an.instanceOf(customElements.get('rh-avatar'))
         .and
         .to.be.an.instanceOf(RhAvatar);
-    });
-    it(`should default to pattern: "squares"`, async function() {
-      expect(element.pattern).to.equal('squares');
     });
   });
 });
