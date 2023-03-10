9d8f50384240bc465f89ed7e530a9bed702c2f1f
Steven Spriggs <steven.spriggs@gmail.com>
2023-03-10 16:33:07 -0500

    fix(subnav): updateActiveItem for roving-tab-index controller

--- elements/rh-subnav/demo/rh-subnav.js
+++ elements/rh-subnav/demo/rh-subnav.js
@@ -2,3 +2,14 @@ import '@rhds/elements/rh-subnav/rh-subnav.js';
 import '@rhds/elements/rh-context-provider/rh-context-provider.js';
 
 import '@patternfly/elements/pf-icon/pf-icon.js';
+
+
+const demo = document.getElementById('demo');
+const links = demo.querySelectorAll('a');
+links.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    links.forEach(link => link.removeAttribute('active'));
+    link.setAttribute('active', '');
+  });
+});
