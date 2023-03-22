85d31d81fe74c5691d4b6ef22908abb17ab99e18
bennypowers <bennypowers@users.noreply.github.com>
2023-03-22 09:52:31 +0000

    deploy: 94b43fcc80fc57a808cdb08415673c8ea1b382e4

--- assets/packages/@rhds/elements/elements/rh-footer/rh-footer-universal.ts
+++ assets/packages/@rhds/elements/elements/rh-footer/rh-footer-universal.ts
@@ -66,13 +66,13 @@ export class RhFooterUniversal extends LitElement {
               <a class="global-logo-anchor"
                   part="logo-anchor"
                   href="https://redhat.com"
-                  alt="Visit Red Hat">
-                <svg title="Red Hat logo"
-                    class="global-logo-image"
+                  aria-label="Visit Red Hat">
+                <svg class="global-logo-image"
                     part="logo-image"
                     data-name="Layer 1"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 192 145">
+                    <title>Red Hat logo</title>
                   <defs>
                     <style>
                       .band {
