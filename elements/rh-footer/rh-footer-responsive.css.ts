4725692d8d9d16f4cd4c1a5f6baad4fb00b22201
Michael Potter <mgp140@gmail.com>
2023-03-26 04:56:01 -0400

    fix(footer): match sidegap to the design specs (#841)
    * feat: add three stages to sidegap with global custom property
    
    * chore: added changelog
    
    * docs: update cssprops
    
    * test: verify section side gap at breakpoints
    
    * chore: fix linting errors
    
    * refactor: remove global --rhds-section-side-gap variable
    
    * refactor: removed rhds global variable
    
    * refactor: removed rhds global variable
    
    * docs: update .changeset/mean-planes-heal.md
    
    ---------
    
    Co-authored-by: Benny Powers <bennypowers@users.noreply.github.com>

--- elements/rh-footer/rh-footer-responsive.css.ts
+++ elements/rh-footer/rh-footer-responsive.css.ts
@@ -129,19 +129,7 @@ export const responsiveStyles = css`
     }
   }
 
-  @media screen and (max-width: ${tabletLandscapeBreakpoint}) {
-    /* Equalize padding on mobile */
-    .section {
-      --_section-side-gap: var(--rh-footer-section-side-gap, var(--rh-space-2xl, 32px));
-    }
-  }
-
   @media screen and (min-width: ${tabletLandscapeBreakpoint}) and (max-width: ${desktopLargeBreakpoint}) {
-    /* Equalize padding on mobile */
-    .section {
-      --_section-side-gap: var(--rh-footer-section-side-gap, var(--rh-space-2xl, 32px));
-    }
-
     .header,
     .main {
       /* switch header to use grid instead */
@@ -151,10 +139,6 @@ export const responsiveStyles = css`
   }
 
   @media screen and (min-width: ${desktopLargeBreakpoint}) {
-    .section {
-      --_section-side-gap: var(--rh-footer-section-side-gap, var(--rh-space-2xl, 32px));
-    }
-
     .header,
     .main {
       /* switch header to use grid instead */
