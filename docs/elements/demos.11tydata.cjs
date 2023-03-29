cb1cc811ebf521f9ef09d619796b1bdfe59036fe
Benny Powers <web@bennypowers.com>
2023-03-29 17:05:29 +0300

    docs: demo permalinks

--- docs/elements/demos.11tydata.cjs
+++ docs/elements/demos.11tydata.cjs
@@ -3,5 +3,8 @@ module.exports = {
     data: 'demos',
     alias: 'demo',
     size: 1,
+    before: (paginationData, fullData) =>
+      paginationData
+        .map(demo => ({ ...demo, permalink: demo.permalink.replace('/components/', '/elements/') }))
   }
 };
