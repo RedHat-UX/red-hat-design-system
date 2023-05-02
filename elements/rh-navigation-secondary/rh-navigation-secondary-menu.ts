4677bf0fbf249fdb3ff66c3321d7ec5d6cbe35d8
Steven Spriggs <steven.spriggs@gmail.com>
2023-05-02 11:50:25 -0400

    docs(navigation-secondary): improve shadowparts jsdoc

--- elements/rh-navigation-secondary/rh-navigation-secondary-menu.ts
+++ elements/rh-navigation-secondary/rh-navigation-secondary-menu.ts
@@ -15,10 +15,10 @@ import styles from './rh-navigation-secondary-menu.css';
  * @summary 'A pop up menu for secondary nav, available in full-width and fixed-with sizes'
  *
  * @slot                  - Optional `<rh-navigation-secondary-menu-section>` elements or content following [design guidelines](../guidelines/#expandable-tray)
- * @csspart container     - container - <div> element, wrapper for menus
- * @csspart full-width    - container - <div> element, wrapper for full-width menus
- * @csspart fixed-width   - container - <div> element, wrapper for fixed-width menus
- * @csspart sections      - container - <div> element, wrapper for menu sections
+ * @csspart container     - container - `<div>` element, wrapper for menus
+ * @csspart full-width    - container - `<div>` element, wrapper for full-width menus
+ * @csspart fixed-width   - container - `<div>` element, wrapper for fixed-width menus
+ * @csspart sections      - container - `<div>` element, wrapper for menu sections
  *
  * @cssprop  --rh-navigation-secondary-menu-section-grid - grid-template-columns for menu sections {@default `repeat(auto-fit, minmax(15.5rem, 1fr))`}
  * @cssprop  {<length>} --rh-navigation-secondary-menu-section-grid-gap - grid-gap for menu sections {@default `32px`}
