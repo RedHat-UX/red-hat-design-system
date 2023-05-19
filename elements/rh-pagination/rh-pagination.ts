c58dd3034742be1d797e46f0905242778198bee4
Ivana Rodriguez <ivrodrig@redhat.com>
2023-05-19 15:24:15 -0400

    docs: add @ccsprops

--- elements/rh-pagination/rh-pagination.ts
+++ elements/rh-pagination/rh-pagination.ts
@@ -33,7 +33,36 @@ const L2 = html`
  *
  * @csspart
  * @fires
- * @cssprop
+ * @cssprop {<color>} --rh-pagination-accent-color
+ *          Sets the outline color when the page input has focus.
+ *          {@default `var(--rh-color-interactive-blue, #0066cc)`}
+ * @cssprop {<color>} --rh-pagination-background-focused
+ *          Sets the disabled stepper color.
+ *          {@default `var(--rh-color-gray-20, #c7c7c7)`}
+ * @cssprop {<color>} --rh-pagination-stepper-color
+ *           Sets the stepper color.
+ *          {@default `var(--rh-color-icon-subtle, #707070)`}
+ * @cssprop --rh-color-border-interactive-on-light
+ * @cssprop --rh-color-border-subtle-on-light
+ * @cssprop --rh-color-gray-20
+ * @cssprop --rh-color-gray-40
+ * @cssprop --rh-color-icon-subtle
+ * @cssprop --rh-color-interactive-blue
+ * @cssprop --rh-color-interactive-blue-darker
+ * @cssprop --rh-color-interactive-blue-darkest
+ * @cssprop --rh-color-interactive-purple-darker
+ * @cssprop --rh-color-interactive-purple-darkest
+ * @cssprop --rh-color-red-700
+ * @cssprop --rh-color-red-300
+ * @cssprop --rh-color-surface-light
+ * @cssprop --rh-color-surface-lighter
+ * @cssprop --rh-color-surface-lightest
+ * @cssprop --rh-font-size-body-text-md
+ * @cssprop --rh-length-2xl
+ * @cssprop --rh-length-4xl
+ * @cssprop --rh-space-xs
+ * @cssprop --rh-space-xl
+ * @cssprop --rh-space-2xl
  */
 @customElement('rh-pagination')
 export class RhPagination extends LitElement {
