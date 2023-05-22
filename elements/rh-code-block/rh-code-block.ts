64f21920e5a98bbfc7d10f4c879cb59ae2b5dafd
Benny Powers <web@bennypowers.com>
2023-05-22 10:57:22 +0300

    feat(code-block): color context

--- elements/rh-code-block/rh-code-block.ts
+++ elements/rh-code-block/rh-code-block.ts
@@ -1,13 +1,21 @@
+import { type ColorTheme, colorContextConsumer } from '../../lib/context/color/consumer.js';
 import { html, LitElement } from 'lit';
 import { customElement } from 'lit/decorators/custom-element.js';
+import { state } from 'lit/decorators/state.js';
 
 import style from './rh-code-block.css';
+import { classMap } from 'lit/directives/class-map.js';
 
 @customElement('rh-code-block')
 export class RhCodeBlock extends LitElement {
   static styles = [style];
+  @colorContextConsumer()
+
+  @state() private on?: ColorTheme;
+
   render() {
-    return html`<slot></slot>`;
+    const { on = '' } = this;
+    return html`<slot class="${classMap({ [on]: !!on })}"></slot>`;
   }
 }
 
