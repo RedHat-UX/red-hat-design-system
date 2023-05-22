b5f83aad1c3759342a83133400f770b796b9e133
Benny Powers <web@bennypowers.com>
2023-05-22 12:32:00 +0300

    feat(code-block): compact and basic resizable

--- elements/rh-code-block/rh-code-block.ts
+++ elements/rh-code-block/rh-code-block.ts
@@ -2,20 +2,27 @@ import { type ColorTheme, colorContextConsumer } from '../../lib/context/color/c
 import { html, LitElement } from 'lit';
 import { customElement } from 'lit/decorators/custom-element.js';
 import { state } from 'lit/decorators/state.js';
+import { classMap } from 'lit/directives/class-map.js';
+import { property } from 'lit/decorators/property.js';
 
 import style from './rh-code-block.css';
-import { classMap } from 'lit/directives/class-map.js';
 
 @customElement('rh-code-block')
 export class RhCodeBlock extends LitElement {
   static styles = [style];
-  @colorContextConsumer()
 
+  @property({ type: Boolean, reflect: true }) compact = false;
+
+  @property({ type: Boolean, reflect: true }) resizable = false;
+
+  @colorContextConsumer()
   @state() private on?: ColorTheme;
 
   render() {
     const { on = '' } = this;
-    return html`<slot class="${classMap({ [on]: !!on })}"></slot>`;
+    return html`
+      <slot id="content" class="${classMap({ [on]: !!on })}"></slot>
+    `;
   }
 }
 
