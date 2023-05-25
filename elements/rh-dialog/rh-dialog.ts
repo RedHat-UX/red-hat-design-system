54200dc8ee19e723e05fe1a99adcfedff70bb0b9
Ivana Rodriguez <ivrodrig@redhat.com>
2023-05-25 09:55:15 -0400

    docs: add JSDoc for CSS props

--- elements/rh-dialog/rh-dialog.ts
+++ elements/rh-dialog/rh-dialog.ts
@@ -27,6 +27,21 @@ function openChanged(this: RhDialog, oldValue: unknown) {
 /**
  * A dialog displays important information to users without requiring them to navigate away from the page.
  * @summary Displays important information to users without requiring them to navigate away from the page
+ *
+ * @cssprop {<color>} --rh-dialog-close-button-color
+ *           Sets the dialog close button color.
+ *          {@default `var(--rh-color-icon-secondary-on-dark, #ffffff)`}
+ * @cssprop --rh-border-radius-default
+ * @cssprop --rh-color-gray-90-rgb
+ * @cssprop --rh-color-icon-secondary-on-dark
+ * @cssprop --rh-color-icon-secondary-on-light
+ * @cssprop --rh-color-icon-subtle
+ * @cssprop --rh-color-surface-lightest
+ * @cssprop --rh-color-text-primary-on-light
+ * @cssprop --rh-modal-video-aspect-ratio
+ * @cssprop --rh-opacity-60
+ * @cssprop --rh-space-md
+ * @cssprop --rh-space-sm
  */
 @customElement('rh-dialog')
 export class RhDialog extends PfModal {
