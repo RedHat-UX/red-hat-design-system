e2f92d4d6587a5da37368905e1c69df49def4738
bennypowers <bennypowers@users.noreply.github.com>
2023-05-30 15:27:16 +0000

    deploy: 28ee0b9667c642a342ebe5674a7b32422c74cbe1

--- assets/packages/@rhds/elements/elements/rh-dialog/rh-dialog.js
+++ assets/packages/@rhds/elements/elements/rh-dialog/rh-dialog.js
@@ -23,8 +23,23 @@ function openChanged(oldValue) {
     }
 }
 /**
- * Dialog
- * @summary Displays content or helps a user focus on a specific task
+ * A dialog displays important information to users without requiring them to navigate away from the page.
+ * @summary Displays important information to users without requiring them to navigate away from the page
+ *
+ * @cssprop {<color>} --rh-dialog-close-button-color
+ *           Sets the dialog close button color.
+ *          {@default `var(--rh-color-icon-secondary-on-dark, #ffffff)`}
+ * @cssprop --rh-border-radius-default
+ * @cssprop --rh-color-gray-90-rgb
+ * @cssprop --rh-color-icon-secondary-on-dark
+ * @cssprop --rh-color-icon-secondary-on-light
+ * @cssprop --rh-color-icon-subtle
+ * @cssprop --rh-color-surface-lightest
+ * @cssprop --rh-color-text-primary-on-light
+ * @cssprop --rh-modal-video-aspect-ratio
+ * @cssprop --rh-opacity-60
+ * @cssprop --rh-space-md
+ * @cssprop --rh-space-sm
  */
 let RhDialog = class RhDialog extends PfModal {
     constructor() {
