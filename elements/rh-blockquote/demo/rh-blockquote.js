28ea40267f4524ce33bdeefd3404128c62ab9e38
Benny Powers <web@bennypowers.com>
2023-05-30 16:11:07 +0300

    docs: use internal references

--- elements/rh-blockquote/demo/rh-blockquote.js
+++ elements/rh-blockquote/demo/rh-blockquote.js
@@ -1,3 +1,3 @@
-import '@rhds/elements/rh-context-provider/rh-context-provider.js';
 import '@rhds/elements/rh-blockquote/rh-blockquote.js';
 
+import '@rhds/elements/lib/elements/rh-context-provider/rh-context-provider.js';
