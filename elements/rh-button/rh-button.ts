dadf41881fd8bb38cc6d1f4e5b49bf6985d03ed7
Nikki Massaro Kauffman <5090492+nikkimk@users.noreply.github.com>
2023-05-31 12:36:59 -0400

    docs(button): updated JSDoc description
    Co-authored-by: Benny Powers <bennypowers@users.noreply.github.com>

--- elements/rh-button/rh-button.ts
+++ elements/rh-button/rh-button.ts
@@ -10,8 +10,8 @@ import { BaseButton } from '@patternfly/elements/pf-button/BaseButton.js';
 import styles from './rh-button.css';
 
 /**
+ * A button communicates and triggers available actions when clicked or selected. Buttons allow users to perform an action when triggered. They feature a text label, a background or a border, and icons.
  * @summary Triggers actions on the page or in the background
- * @description A button communicates and triggers available actions when clicked or selected. Buttons allow users to perform an action when triggered. They feature a text label, a background or a border, and icons.
  *
  * @csspart icon - Container for the icon slot
  * @slot icon - Contains the button's icon or state indicator, e.g. a spinner.
