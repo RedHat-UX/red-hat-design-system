0d3081433fc6d683e13c7d1246962afba7071f06
Benny Powers <web@bennypowers.com>
2023-06-01 14:11:39 +0300

    fix(code-block): slotted links (light)

--- elements/rh-code-block/rh-code-block-toolbar.ts
+++ elements/rh-code-block/rh-code-block-toolbar.ts
@@ -1,6 +1,9 @@
 import { LitElement, html } from 'lit';
 import { customElement } from 'lit/decorators/custom-element.js';
 import { property } from 'lit/decorators/property.js';
+import { classMap } from 'lit/directives/class-map.js';
+
+import { SlotController } from '@patternfly/pfe-core/controllers/slot-controller.js';
 
 import styles from './rh-code-block-toolbar.css';
 
@@ -19,12 +22,15 @@ export class RhCodeBlockToolbar extends LitElement {
 
   @property() title = '';
 
+  #slots = new SlotController(this, 'title', null, 'actions');
+
   render() {
+    const hasTitle = this.title || this.#slots.hasSlotted('title');
     return html`
-      <div id="content">
-        <slot name="title">${this.title}</slot>
-        <slot></slot>
-        <slot name="actions"></slot>
+      <div id="container">
+        <slot id="title" name="title" class="${classMap({ hasTitle })}">${this.title}</slot>
+        <slot id="content"></slot>
+        <slot id="actions" name="actions"></slot>
       </div>
     `;
   }
