846b8387f2a259be78a310930f54ebc2a179a7b2
Ivana Rodriguez <55894965+eyevana@users.noreply.github.com>
2023-06-29 11:37:16 -0400

    docs: add permalink/copy to repo status table header & use SVG for check (#1097)
    * fix: add permalink/copy to repo status table header
    
    * feat: use SVG for checkmark instead
    
    * style: size SVG correctly
    
    * chore: demo issue
    
    * fix: need to add empty live above markdown heading

--- docs/_plugins/shortcodes/repoStatus.cjs
+++ docs/_plugins/shortcodes/repoStatus.cjs
@@ -1,8 +1,12 @@
+const fs = require('fs');
+
 /**
  * Reads component status data from global data (see above) and outputs a table for each component
  * @this {EleventyContext}
  */
-function repoStatus({ heading = 'Repo status', type = 'Pattern' } = {}) {
+function repoStatus({ heading = 'Repo status', type = 'Pattern', level = 2 } = {}) {
+  const headingLevel = Array.from({ length: level }, () => '#').join('');
+  const checkSVG = fs.readFileSync('node_modules/@patternfly/icons/fas/check.svg', 'utf8');
   /** @type {string[][]} */
   const docsPage = this.ctx._;
   const allStatuses = this.ctx.repoStatus ?? docsPage?.repoStatus ?? [];
@@ -18,8 +22,9 @@ function repoStatus({ heading = 'Repo status', type = 'Pattern' } = {}) {
     return /* html*/`
 
 <section class="section section--palette-default container">
-  <a id="Component status"></a>
-  <h2 id="component-status" class="section-title pfe-jump-links-panel__section">${heading}</h2>
+
+  ${`${headingLevel} ${heading} {.section-title .pfe-jump-links-panel__section}`}
+    
   <p>Learn more about our various code repos by visiting <a href="https://ux.redhat.com/about/how-we-build/" target="_blank">this page</a>.</p>
   <div class="component-status-table-container">
     <table class="component-status-table">
@@ -31,7 +36,7 @@ function repoStatus({ heading = 'Repo status', type = 'Pattern' } = {}) {
       <tbody>${bodyRows.map(([, rowHeader, ...columns]) => `
         <tr>
           <th>${rowHeader}</th>
-          ${columns.map(x => `<td>${x === 'x' ? '&check;' : ''}</td>`.trim()).join('\n').trim()}
+          ${columns.map(x => `<td>${x === 'x' ? `${checkSVG}` : ''}</td>`.trim()).join('\n').trim()}
         </tr>`.trim()).join('\n').trim()}
       </tbody>
     </table>
