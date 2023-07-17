451f9fc30e075c5948f82bbf77a7a030e94f6064
Michael Potter <mgp140@gmail.com>
2023-07-17 08:39:52 -0400

    docs: added jspm and redhatstatic cdn examples

--- docs/_plugins/shortcodes/renderInstallation.cjs
+++ docs/_plugins/shortcodes/renderInstallation.cjs
@@ -36,13 +36,48 @@ Replace \`/path/to\` in the \`href\` attribute with the installation path to the
   
   In the meantime, install this component using npm:
 
+npm
 ~~~shell
 npm install ${docsPage.manifest.packageJson.name}
-~~~`}
+~~~
+
+Red Hat CDN (recommend)
+~~~html
+<head>
+  <script type="importmap">
+    {
+      "imports": {
+        "@rhds/elements/": "https://www.redhatstatic.com/dx/v1-alpha/@rhds/elements@1.1.0/elements/",
+        "@patternfly/elements/": "https://www.redhatstatic.com/dx/v1-alpha/@patternfly/elements@2.2.2/"
+      }
+    }
+  </script>
+  <script type="module">
+    import '@rhds/elements/${docsPage.tagName}/${docsPage.tagName}.js';
+  </script>
+</head>
+~~~
+
+jspm.dev (for development purposes only)
+~~~html
+<script type="importmap"> 
+  { 
+    "imports": { 
+      "@rhds/elements/": "https://jspm.dev/@rhds/elements/", 
+      "@patternfly/elements/": "https://jspm.dev/@patternfly/elements/" 
+    } 
+  } 
+</script>
+<script type="module">
+  import '@rhds/elements/rh-cta/rh-cta.js';
+</script>
+~~~
+`}
 
   We recommend using an import map to manage your dependencies. For more information on import maps and how to use them, see the [import map reference on MDN Web Docs](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/script/type/importmap/).
 
   Then import this component into your project by using a [bare module specifier](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules):
+
 ~~~js
 import '@rhds/elements/${docsPage.tagName}/${docsPage.tagName}.js';
 ~~~
