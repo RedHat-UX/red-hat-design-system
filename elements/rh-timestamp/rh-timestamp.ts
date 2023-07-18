516d65977165910e9461c55732f6164166fed8ea
Ivana Rodriguez <ivrodrig@redhat.com>
2023-07-18 10:23:03 -0400

    feat: rh-timestamp init

---
+++ elements/rh-timestamp/rh-timestamp.ts
@@ -0,0 +1,86 @@
+import type { ComplexAttributeConverter, PropertyValues } from 'lit';
+
+import { LitElement, html } from 'lit';
+import { customElement } from 'lit/decorators/custom-element.js';
+import { property } from 'lit/decorators/property.js';
+
+import {
+  TimestampController,
+  type DateTimeFormat,
+} from '@patternfly/pfe-core/controllers/timestamp-controller.js';
+
+import styles from './rh-timestamp.css';
+
+const BooleanStringConverter: ComplexAttributeConverter = {
+  fromAttribute(value) {
+    return !value || value === 'true';
+  },
+};
+
+/**
+ * A **timestamp** provides consistent formats for displaying date and time values.
+ */
+@customElement('rh-timestamp')
+export class RhTimestamp extends LitElement {
+  static readonly styles = [styles];
+
+  @property({ reflect: true, attribute: 'date-format' }) dateFormat?: DateTimeFormat;
+
+  @property({ reflect: true, attribute: 'time-format' }) timeFormat?: DateTimeFormat;
+
+  @property({ attribute: false }) customFormat?: object;
+
+  @property({ reflect: true, attribute: 'display-suffix' }) displaySuffix?: string;
+
+  @property({ reflect: true }) locale?: string;
+
+  @property({ reflect: true, type: Boolean }) relative?: boolean;
+
+  @property({ reflect: true, type: Boolean }) utc?: boolean;
+
+  @property({ reflect: true, attribute: 'hour-12', converter: BooleanStringConverter }) hour12?: boolean;
+
+  @property({ reflect: true })
+  get date() {
+    return this.#timestamp.localeString;
+  }
+
+  set date(string) {
+    this.#timestamp.date = new Date(string);
+  }
+
+  get isoString() {
+    return this.#timestamp.isoString;
+  }
+
+  get time() {
+    return this.#timestamp.time;
+  }
+
+  #timestamp = new TimestampController(this);
+
+  connectedCallback() {
+    super.connectedCallback();
+    if (this.hasAttribute('date')) {
+      this.#timestamp.date = new Date(this.getAttribute('date')!);
+    }
+  }
+
+  willUpdate(changedProperties: PropertyValues<this>) {
+    for (const [prop] of changedProperties) {
+      this.#timestamp.set(prop, this[prop as keyof this]);
+    }
+  }
+
+  render() {
+    return html`
+      <time datetime="${this.#timestamp.isoString}">${this.#timestamp.time}</time>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'rh-timestamp': RhTimestamp;
+  }
+}
---
+++ elements/rh-timestamp/test/rh-timestamp.spec.ts
@@ -0,0 +1,21 @@
+import { expect, html } from '@open-wc/testing';
+import { createFixture } from '@patternfly/pfe-tools/test/create-fixture.js';
+import { RhTimestamp } from '@rhds/elements/rh-timestamp/rh-timestamp.js';
+
+describe('<rh-timestamp>', function() {
+  describe('simply instantiating', function() {
+    let element: RhTimestamp;
+    it('imperatively instantiates', function() {
+      expect(document.createElement('rh-timestamp')).to.be.an.instanceof(RhTimestamp);
+    });
+
+    it('should upgrade', async function() {
+      element = await createFixture<RhTimestamp>(html`<rh-timestamp></rh-timestamp>`);
+      const klass = customElements.get('rh-timestamp');
+      expect(element)
+        .to.be.an.instanceOf(klass)
+        .and
+        .to.be.an.instanceOf(RhTimestamp);
+    });
+  })
+});
