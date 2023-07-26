73e6918b245bf8e960f6c7e78b7757a0cf038b7f
bennypowers <bennypowers@users.noreply.github.com>
2023-07-26 11:48:02 +0000

    deploy: cf8ee6cb01ee60f114f2909fb219fd33b6c61f95

--- assets/packages/@rhds/elements/elements/rh-audio-player/rh-audio-player.d.ts
+++ assets/packages/@rhds/elements/elements/rh-audio-player/rh-audio-player.d.ts
@@ -7,7 +7,7 @@ import { type ColorPalette } from '../../lib/context/color/provider.js';
  * @slot title - optional, title of episode
  * @slot media - html `audio` element
  * @slot about - optional `rh-audio-player-about` panel with attribution
- * @slot about - optional `rh-audio-player-subscribe` panel with links to subscribe
+ * @slot subscribe - optional `rh-audio-player-subscribe` panel with links to subscribe
  * @slot transcript - optional `rh-transcript` panel with `rh-cue` elements
  * @cssprop --rh-audio-player-background-color - color of player background - {@default var(--rh-color-surface-lightest, #ffffff)}
  * @cssprop --rh-audio-player-icon-background-color {@default var(--rh-audio-player-background-color)}
