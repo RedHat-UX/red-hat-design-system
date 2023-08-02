72be2c81febd3620f41603fce1a69996022f7632
Ivana Rodriguez <ivrodrig@redhat.com>
2023-08-02 09:51:31 -0400

    docs: add screenshot and summary

--- elements/rh-timestamp/rh-timestamp.ts
+++ elements/rh-timestamp/rh-timestamp.ts
@@ -19,6 +19,8 @@ const BooleanStringConverter: ComplexAttributeConverter = {
 
 /**
  * A timestamp provides consistent formats for displaying date and time values.
+ *
+ * @summary Displays a line of text with date and time values
  */
 @customElement('rh-timestamp')
 export class RhTimestamp extends LitElement {
