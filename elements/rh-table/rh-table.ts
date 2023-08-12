10bdf50db97b28d4400c8fe3ff47b5c36d52e3cc
Ivana Rodriguez <ivrodrig@redhat.com>
2023-08-12 12:13:24 -0400

    chore: commit WIP

--- elements/rh-table/rh-table.ts
+++ elements/rh-table/rh-table.ts
@@ -4,6 +4,7 @@ import { LitElement, html, nothing } from 'lit';
 import { customElement } from 'lit/decorators/custom-element.js';
 import { property } from 'lit/decorators/property.js';
 import { queryAssignedElements } from 'lit/decorators/query-assigned-elements.js';
+import { Logger } from '@patternfly/pfe-core/controllers/logger.js';
 
 import styles from './rh-table.css';
 
@@ -15,51 +16,38 @@ import styles from './rh-table.css';
 export class RhTable extends LitElement {
   static readonly styles = [styles];
 
-  @property({ type: Boolean, reflect: true, attribute: 'no-hover' }) noHover = false;
+  @property({ type: Boolean, reflect: true, attribute: 'active-focus' }) activeFocus = false;
 
   @queryAssignedElements() private defaultSlot?: HTMLElement[];
 
-  private cells: HTMLTableCellElement[] = [];
+  private cols: HTMLTableColElement[] = [];
+  private logger = new Logger(this);
 
   render() {
     return html`
     <div id="container" 
-         @pointerleave=${this.noHover ? nothing : this._onPointerleave}
-         @pointerover=${this.noHover ? nothing : this._onPointerover}>
-      <slot @slotchange="${this._onSlotchange}"></slot>
+         @pointerleave=${this.activeFocus ? this.onPointerleave : nothing}
+         @pointerover=${this.activeFocus ? this.onPointerover2 : nothing}>
+      <slot @slotchange="${this.onSlotchange}"></slot>
     </div>
     `;
   }
 
-  _onPointerleave() {
-    this.cells.forEach(cell => {
-      const classes = [
-        'active-column',
-        'active-row--mouse',
-        'active-row--touch',
-        'active-cell--mouse',
-        'active-cell--touch'
-      ];
-      cell.classList.remove(...classes);
-    });
+  private onPointerleave() {
+    this.cols.forEach(col => col.classList.remove('active'));
   }
 
-  _onPointerover(event: PointerEvent) {
+  private onPointerover2(event: PointerEvent) {
     event.preventDefault();
 
-    // don't execute effect on heading row
-    if (event.composedPath().some(el => el instanceof HTMLTableSectionElement && el.tagName === 'THEAD')) {
-      return;
-    }
-
-    let { target, pointerType } = event;
+    let { target } = event;
 
     if (!(target instanceof Element)) {
       return;
     }
 
-    if (target.tagName !== 'td') {
-      const ancestorCell = target.closest('td');
+    if (!['td', 'th'].includes(target.tagName)) {
+      const ancestorCell = target.closest('td, th');
       if (ancestorCell) {
         target = ancestorCell;
       } else {
@@ -67,42 +55,22 @@ export class RhTable extends LitElement {
       }
     }
 
-    const currentCell = target as HTMLTableCellElement;
-    // pen should have the same experience as touch
-    const variant = pointerType === 'mouse' ? 'mouse' : 'touch';
-
-    // cell already has focus
-    if (currentCell.classList.contains(`active-cell--${variant}`)) {
-      return;
-    }
-
-    // highlight all siblings in the same column
-    const columnIds = currentCell.headers?.split(' ');
-    this.cells.forEach(cell => {
-      const sibling = columnIds.find(id => cell.headers?.includes(id) || cell.id === id);
-      if (sibling) {
-        cell.classList.add('active-column');
+    this.cols.forEach((col, index) => {
+      const { cellIndex } = target as HTMLTableCellElement;
+      if (index === cellIndex) {
+        col.classList.add('active');
       } else {
-        cell.classList.remove(`active-row--${variant}`);
-        cell.classList.remove('active-column');
+        col.classList.remove('active');
       }
-      // clear previously focused cell
-      cell.classList.remove(`active-cell--${variant}`);
     });
-
-    // highlight all siblings in the same row
-    const currentRow = currentCell.closest('tr') as HTMLTableRowElement;
-    [...currentRow.cells].forEach(cell => cell.classList.add(`active-row--${variant}`));
-
-    // add focus state
-    currentCell.classList.add(`active-cell--${variant}`);
   }
 
-  _onSlotchange() {
-    // todo: warning if no table
+  onSlotchange() {
     const table = this.defaultSlot?.[0];
     if (table) {
-      this.cells = [...(table.querySelectorAll('th, td') || [])] as HTMLTableCellElement[];
+      this.cols = [...(table.querySelectorAll('col') || [])] as HTMLTableColElement[];
+    } else {
+      this.logger.warn('[rh-table] expects a slotted <table> tag.');
     }
   }
 }
