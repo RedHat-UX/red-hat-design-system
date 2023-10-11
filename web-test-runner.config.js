c745bb530b0bdb9f6088b7a17a98f3fa961742c1
Benny Powers <web@bennypowers.com>
2023-10-11 09:55:24 +0300

    test: add firefox esr to test matrix
    Closes #1028

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,9 +1,21 @@
+import { playwrightLauncher } from '@web/test-runner-playwright';
 import { pfeTestRunnerConfig } from '@patternfly/pfe-tools/test/config.js';
 import { litcssOptions } from './web-dev-server.config.js';
 
-export default pfeTestRunnerConfig({
+const config = pfeTestRunnerConfig({
   litcssOptions,
   tsconfig: 'tsconfig.json',
   files: ['elements/**/*.spec.ts'],
   ...process.argv.includes('--default-reporter') && { reporter: 'default' },
 });
+
+export default {
+  ...config,
+  browsers: [
+    ...config.browsers,
+    ...!process.env.CI ? [] : [
+      playwrightLauncher({ product: 'firefox' }),
+      playwrightLauncher({ product: 'webkit' }),
+    ],
+  ],
+};
