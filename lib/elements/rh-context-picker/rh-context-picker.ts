b04c5049ff442dfec3753b6e7acf78bb55f4263b
Benny Powers <web@bennypowers.com>
2023-10-22 21:58:53 +0300

    fix(lib): context picker

--- lib/elements/rh-context-picker/rh-context-picker.ts
+++ lib/elements/rh-context-picker/rh-context-picker.ts
@@ -4,21 +4,44 @@ import { customElement } from 'lit/decorators/custom-element.js';
 import { property } from 'lit/decorators/property.js';
 import { query } from 'lit/decorators/query.js';
 
+import type { Color } from '@rhds/tokens/js/types.js';
+import {
+  ColorSurfaceDarkest as darkest,
+  ColorSurfaceDarker as darker,
+  ColorSurfaceDark as dark,
+  ColorSurfaceLight as light,
+  ColorSurfaceLighter as lighter,
+  ColorSurfaceLightest as lightest,
+} from '@rhds/tokens/color.js';
+
 import style from './rh-context-picker.css';
+import { styleMap } from 'lit/directives/style-map.js';
+
 @customElement('rh-context-picker')
 export class RhContextPicker extends LitElement {
   static formAssociated = true;
 
   static readonly styles = [style];
 
-  static readonly palettes: ColorPalette[] = [
-    'darkest',
-    'darker',
-    'dark',
-    'light',
-    'lighter',
-    'lightest',
-  ];
+  static readonly palettes = new Map(Object.entries({
+    darkest,
+    darker,
+    dark,
+    light,
+    lighter,
+    lightest,
+  } satisfies Record<Exclude<ColorPalette, 'base' | 'accent' | 'complement'>, Color>));
+
+  private static offsets = {
+    darkest: -4,
+    darker: -3,
+    dark: -3,
+    light: -1,
+    lighter: 1,
+    lightest: 2,
+  };
+
+  private static paletteNames = Array.from(RhContextPicker.palettes, ([name]) => name);
 
   declare shadowRoot: ShadowRoot;
 
@@ -29,27 +52,31 @@ export class RhContextPicker extends LitElement {
   /** ID of context element to toggle (same root) */
   @property() target?: string;
 
-  @property() value?: ColorPalette;
+  @property() value?: Exclude<ColorPalette, 'base' | 'complement' | 'accent'>;
+
+  @property() label = 'Color Palette';
 
   @query('#context-range') range?: HTMLInputElement;
 
   render() {
+    const offset = RhContextPicker.offsets[this.value ?? 'darkest'];
     return html`
-      <label for="context-range">Color Palette</label>
-      <input id="context-range"
-             name="range"
-             type="range"
-             list="palettes"
-             max="5"
-             @input="${this.#onInput}">
-      <datalist id="palettes">
-        <option value="0" label="darkest"></option>
-        <option value="1" label="darker"></option>
-        <option value="2" label="dark"></option>
-        <option value="3" label="light"></option>
-        <option value="4" label="lighter"></option>
-        <option value="5" label="lightest"></option>
-      </datalist>
+      <div id="container">
+        <input id="context-range"
+               name="range"
+               type="range"
+               list="palettes"
+               max="5"
+               style="${styleMap({ '--offset': `${offset}px` })}"
+               @input="${this.#onInput}">
+        <datalist id="palettes">${Array.from(RhContextPicker.palettes, ([palette, { hex }]) => html`
+          <option value="${palette}"
+                  title="palette" style="--c:var(--rh-color-surface-${palette},#${hex})">
+            <span class="visually-hidden">${palette}</span>
+          </option> `)}
+        </datalist>
+        <label for="context-range">${this.label}</label>
+      </div>
     `;
   }
 
@@ -69,17 +96,18 @@ export class RhContextPicker extends LitElement {
 
   updated(changedProperties: PropertyValues<this>) {
     if (changedProperties.has('value') && this.value && this.range) {
-      this.range.value = RhContextPicker.palettes.indexOf(this.value).toString();
+      this.range.value = RhContextPicker.paletteNames.indexOf(this.value).toString();
     }
   }
 
   #onInput(e: Event & { target: HTMLInputElement }) {
-    this.#internals.setFormValue(e.target.value);
-    this.#setValue(e.target.value);
+    const value = RhContextPicker.paletteNames[+e.target.value];
+    this.#setValue(value);
   }
 
   #setValue(value: string) {
-    this.value = RhContextPicker.palettes[+value];
+    this.#internals.setFormValue(value);
+    this.value = value as this['value'];
     this.sync();
   }
 
