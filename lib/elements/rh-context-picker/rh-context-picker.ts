7e537674b7432459c29530893285772ec13d8f94
Benny Powers <web@bennypowers.com>
2023-10-23 10:09:51 +0300

    feat(lib): context-demo element

--- lib/elements/rh-context-picker/rh-context-picker.ts
+++ lib/elements/rh-context-picker/rh-context-picker.ts
@@ -1,8 +1,10 @@
-import { type ColorPalette } from '../../context/color/provider.js';
 import { html, LitElement, type PropertyValues } from 'lit';
 import { customElement } from 'lit/decorators/custom-element.js';
 import { property } from 'lit/decorators/property.js';
 import { query } from 'lit/decorators/query.js';
+
+import { type ColorPalette } from '../../context/color/provider.js';
+import { colorContextConsumer, type ColorTheme } from '../../context/color/consumer.js';
 
 import type { Color } from '@rhds/tokens/js/types.js';
 import {
@@ -14,8 +16,18 @@ import {
   ColorSurfaceLightest as lightest,
 } from '@rhds/tokens/color.js';
 
-import style from './rh-context-picker.css';
 import { styleMap } from 'lit/directives/style-map.js';
+import { classMap } from 'lit/directives/class-map.js';
+
+import '@rhds/elements/rh-tooltip/rh-tooltip.js';
+
+import style from './rh-context-picker.css';
+
+export class ContextChangeEvent extends Event {
+  constructor(public colorPalette: ColorPalette) {
+    super('change', { bubbles: true });
+  }
+}
 
 @customElement('rh-context-picker')
 export class RhContextPicker extends LitElement {
@@ -23,16 +35,16 @@ export class RhContextPicker extends LitElement {
 
   static readonly styles = [style];
 
-  static readonly palettes = new Map(Object.entries({
+  static readonly palettes = new Map<ColorPalette, Color>(Object.entries({
     darkest,
     darker,
     dark,
     light,
     lighter,
     lightest,
-  } satisfies Record<Exclude<ColorPalette, 'base' | 'accent' | 'complement'>, Color>));
+  }) as [ColorPalette, Color][]);
 
-  private static offsets = {
+  private static offsets: Partial<Record<ColorPalette, number>> = {
     darkest: -4,
     darker: -3,
     dark: -3,
@@ -45,39 +57,55 @@ export class RhContextPicker extends LitElement {
 
   declare shadowRoot: ShadowRoot;
 
+  /** ID of context element to toggle (same root) */
+  @property() target?: string;
+
+  @property() value: ColorPalette = 'darkest';
+
+  @query('#context-range') private range?: HTMLInputElement;
+
+  @colorContextConsumer() private on?: ColorTheme;
+
+  #offset = RhContextPicker.offsets[this.value];
+
   #internals = this.attachInternals();
 
   #target: HTMLElement | null = null;
 
-  /** ID of context element to toggle (same root) */
-  @property() target?: string;
-
-  @property() value?: Exclude<ColorPalette, 'base' | 'complement' | 'accent'>;
-
-  @property() label = 'Color Palette';
-
-  @query('#context-range') range?: HTMLInputElement;
+  willUpdate() {
+    this.#offset = RhContextPicker.offsets[this.value];
+  }
 
   render() {
-    const offset = RhContextPicker.offsets[this.value ?? 'darkest'];
+    const { on = 'dark', value = 'darkest' } = this;
+    const derivedLabel = this.#internals.ariaLabel ?? Array.from(this.#internals.labels, x => x.textContent).join();
     return html`
-      <div id="container">
-        <input id="context-range"
-               name="range"
-               type="range"
-               list="palettes"
-               max="5"
-               style="${styleMap({ '--offset': `${offset}px` })}"
-               @input="${this.#onInput}">
-        <datalist id="palettes">${Array.from(RhContextPicker.palettes, ([palette, { hex }]) => html`
-          <option value="${palette}"
-                  title="palette" style="--c:var(--rh-color-surface-${palette},#${hex})">
-            <span class="visually-hidden">${palette}</span>
-          </option> `)}
-        </datalist>
-        <label for="context-range">${this.label}</label>
+      <div id="container" class="${classMap({ [on]: true })}">
+          <input id="context-range"
+                 class="${classMap({ [value]: true })}"
+                 name="range"
+                 type="range"
+                 list="palettes"
+                 max="5"
+                 aria-label="${derivedLabel}"
+                 style="${styleMap({ '--offset': `${this.#offset}px` })}"
+                 @input="${this.#onInput}">
+          <datalist id="palettes">${Array.from(RhContextPicker.palettes, ([palette]) => html`
+            <option id="option-${palette}"
+                    value="${palette}"
+                    title="${palette}"
+                    @click="${() => this.#setValue(palette)}">
+              <span class="visually-hidden">${palette}</span>
+            </option>`)}
+          </datalist>
       </div>
     `;
+  }
+
+  updated(changedProperties: PropertyValues<this>) {
+    if (changedProperties.has('value') && this.range) {
+      this.range.value = RhContextPicker.paletteNames.indexOf(this.value).toString();
+    }
   }
 
   formStateRestoreCallback(state: string) {
@@ -94,21 +122,20 @@ export class RhContextPicker extends LitElement {
     }
   }
 
-  updated(changedProperties: PropertyValues<this>) {
-    if (changedProperties.has('value') && this.value && this.range) {
-      this.range.value = RhContextPicker.paletteNames.indexOf(this.value).toString();
+  #onInput(event: Event) {
+    if (event.target instanceof HTMLInputElement) {
+      event.stopPropagation();
+      const value = RhContextPicker.paletteNames[+event.target.value];
+      this.#setValue(value);
     }
-  }
-
-  #onInput(e: Event & { target: HTMLInputElement }) {
-    const value = RhContextPicker.paletteNames[+e.target.value];
-    this.#setValue(value);
   }
 
   #setValue(value: string) {
     this.#internals.setFormValue(value);
     this.value = value as this['value'];
-    this.sync();
+    if (this.dispatchEvent(new ContextChangeEvent(this.value))) {
+      this.sync();
+    }
   }
 
   sync() {
