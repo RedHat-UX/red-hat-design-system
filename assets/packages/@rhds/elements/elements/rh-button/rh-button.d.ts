74c806b3533dc39d73df2d3a49bf06891fbf3f99
bennypowers <bennypowers@users.noreply.github.com>
2023-10-23 13:52:19 +0000

    deploy: fcf22f028fcf59e7f0552c33a0408401446fcd38

--- assets/packages/@rhds/elements/elements/rh-button/rh-button.d.ts
+++ assets/packages/@rhds/elements/elements/rh-button/rh-button.d.ts
@@ -1,16 +1,31 @@
-import { BaseButton } from '@patternfly/elements/pf-button/BaseButton.js';
+import { LitElement, type TemplateResult } from 'lit';
 /**
  * A button is clickable text or an icon that triggers an action on the page or in the background. Depending on the action, content, and hierarchy, a button can be used on its own or grouped with other buttons.
  *
  * @summary Triggers actions on the page or in the background
- * @summary Triggers actions on the page or in the background
+ *
+ * @csspart button - Internal button element
  * @csspart icon - Container for the icon slot
+ *
  * @slot icon - Contains the button's icon or state indicator, e.g. a spinner.
  * @slot - Contains button text
  */
-export declare class RhButton extends BaseButton {
+export declare class RhButton extends LitElement {
     #private;
     static readonly styles: import("lit").CSSResult[];
+    static readonly formAssociated = true;
+    static readonly shadowRootOptions: ShadowRootInit;
+    /** Disables the button */
+    disabled: boolean;
+    type?: 'button' | 'submit' | 'reset';
+    /** Accessible name for the button, use when the button does not have slotted text */
+    label?: string;
+    /** Form value for the button */
+    value?: string;
+    /** Form name for the button */
+    name?: string;
+    /** Shorthand for the `icon` slot, the value is icon name */
+    icon?: string;
     /**
      * Changes the style of the button.
      * - Primary: Used for the most important call to action on a page. Try to
@@ -22,16 +37,16 @@ export declare class RhButton extends BaseButton {
      * - Tertiary: Tertiary buttons are flexible and can be used as needed.
      */
     variant: 'primary' | 'secondary' | 'tertiary' | 'close' | 'play';
-    /** @deprecated The size property is not currently used */
-    size: string;
     /**
-     * When set, indicates that the button performs a destructive action
+     * Use danger buttons for actions a user can take that are potentially
+     * destructive or difficult/impossible to undo, like deleting or removing
+     * user data.
      */
     danger: boolean;
     private on?;
     willUpdate(): void;
-    render(): import("lit-html").TemplateResult<1>;
-    protected renderDefaultIcon(): import("lit-html").TemplateResult<1>;
+    render(): TemplateResult<1>;
+    protected formDisabledCallback(): Promise<void>;
 }
 declare global {
     interface HTMLElementTagNameMap {
