15dc5573d033cce047ec745aca542b0f3a8b55f1
Benny Powers <web@bennypowers.com>
2023-10-24 19:17:34 +0300

    docs: playground tabs

--- docs/_plugins/shortcodes/rh-playground.js
+++ docs/_plugins/shortcodes/rh-playground.js
@@ -77,7 +77,7 @@ class RhPlayground extends LitElement {
     const { config } = await import(`/assets/playgrounds/${this.tagName}-playground.js`);
     this.config = config;
     this.demos = Object.entries(config.files ?? {})
-      .filter(([, { contentType }]) => contentType.startsWith('text/html'))
+      .filter(([, { contentType }]) => contentType?.startsWith('text/html'))
       .map(([filename, { label }]) => ({ filename, label, active: filename === 'demo/index.html' }));
     await import('playground-elements');
     this.requestUpdate();
