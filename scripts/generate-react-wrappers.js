73fdae6bb505c922414e6e8c4cc3247fb3374d18
Benny Powers <web@bennypowers.com>
2023-11-29 12:49:57 +0200

    fix: doh

--- scripts/generate-react-wrappers.js
+++
@@ -1,7 +0,0 @@
-import { generateReactWrappers } from '@patternfly/pfe-tools/react/generate-wrappers.js';
-
-const inURL = new URL('../custom-elements.json', import.meta.url);
-const outURL = new URL('../react/', import.meta.url);
-
-await generateReactWrappers(inURL, outURL, '@rhds/elements', 'rh');
-
--- scripts/generate-react-wrappers.ts
+++ scripts/generate-react-wrappers.ts
@@ -1,6 +1,7 @@
 import { generateReactWrappers } from '@patternfly/pfe-tools/react/generate-wrappers.js';
 
 const inURL = new URL('../custom-elements.json', import.meta.url);
-const outURL = new URL('../kkkct/', import.meta.url);
+const outURL = new URL('../react/', import.meta.url);
 
-await generateReactWrappers(inURL, outURL);
+await generateReactWrappers(inURL, outURL, '@rhds/elements', 'rh');
+
