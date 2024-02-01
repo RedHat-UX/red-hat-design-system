041af8770923507c4c45e732ab1503a6af976522
Steven Spriggs <steven.spriggs@gmail.com>
2024-02-01 15:46:17 -0500

    docs: remove font styling from copy-permalink allow page to set those styles

--- docs/assets/elements/uxdot-copy-permalink.js
+++ docs/assets/elements/uxdot-copy-permalink.js
@@ -32,10 +32,6 @@ class UxdotCopyPermalink extends LitElement {
       background: var(--rh-color-surface-light, #e0e0e0);
     }
 
-    ::slotted(:is(h1, h2, h3, h4, h5, h6)) {
-      font-size: var(--rh-font-size-heading-md, 1.75rem) !important;
-      font-weight: var(--rh-font-weight-heading-medium, 500);
-    }
   `;
 
   static properties = {
