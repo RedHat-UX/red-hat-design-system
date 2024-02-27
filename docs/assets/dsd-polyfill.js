a6b6d6eb33d600ffeebfe96511020d737d1f5b8b
Steven Spriggs <steven.spriggs@gmail.com>
2024-02-27 10:37:24 -0500

    docs: remove unused files

--- docs/assets/dsd-polyfill.js
+++
@@ -1,9 +0,0 @@
-(function attachShadowRoots(root) {
-  root.querySelectorAll('template[shadowrootmode]').forEach(template => {
-    const mode = template.getAttribute('shadowrootmode');
-    const shadowRoot = template.parentNode.attachShadow({ mode });
-    shadowRoot.appendChild(template.content);
-    template.remove();
-    attachShadowRoots(shadowRoot);
-  });
-})(document);
