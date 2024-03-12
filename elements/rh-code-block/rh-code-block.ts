d5f3edb70fea83d34024c3be873d85438e951837
Benny Powers <web@bennypowers.com>
2024-03-12 10:56:05 +0200

    feat(code-block): line numbers

--- elements/rh-code-block/rh-code-block.ts
+++ elements/rh-code-block/rh-code-block.ts
@@ -1,31 +1,25 @@
 import { LitElement, html, type PropertyValues } from 'lit';
 import { customElement } from 'lit/decorators/custom-element.js';
 import { classMap } from 'lit/directives/class-map.js';
+import { styleMap } from 'lit/directives/style-map.js';
 import { property } from 'lit/decorators/property.js';
+// import { provide } from '@lit/context';
+
 import { SlotController } from '@patternfly/pfe-core/controllers/slot-controller.js';
 
 import { type ColorTheme, colorContextConsumer } from '../../lib/context/color/consumer.js';
+
+// import { wrapContext } from './context.js';
 
 import { RhCodeActionEvent } from './rh-code-action.js';
 
 import style from './rh-code-block.css';
 
-const lineNumberStyleMap = new WeakMap<HTMLElement, CSSStyleDeclaration>();
-
-/**
- * Tell me how many lines-of-text an element is in height.
- */
-function computeLineNumber(el?: HTMLElement | null): number {
-  if (!el) {
-    return 0;
-  } else {
-    if (!lineNumberStyleMap.get(el)) {
-      lineNumberStyleMap.set(el, getComputedStyle(el));
-    }
-    const divHeight = el.offsetHeight;
-    const lineHeight = parseInt(lineNumberStyleMap.get(el)!.getPropertyValue('line-height'));
-    return Math.floor(divHeight / lineHeight) || 0;
-  }
+interface CodeLineHeightsInfo {
+  lines: string[];
+  lineHeights: (number | undefined)[];
+  sizer: HTMLElement;
+  oneLinerHeight: number;
 }
 
 /**
@@ -51,28 +45,52 @@ export class RhCodeBlock extends LitElement {
 
   @colorContextConsumer() private on?: ColorTheme;
 
-  @property({ type: Boolean }) wrap = false;
+  /* @provide({ context: wrapContext })*/ @property({ type: Boolean }) wrap = false;
 
   #slots = new SlotController(this, null, 'actions');
 
+  #ro = new ResizeObserver(() => this.#computeLineNumbers());
+
   #linesMax = 0;
+
+  #lineHeights: `${string}px`[] = [];
+
+  override connectedCallback() {
+    super.connectedCallback();
+    this.#ro.observe(this);
+  }
+
+  override disconnectedCallback() {
+    super.disconnectedCallback();
+    this.#ro.disconnect();
+  }
 
   render() {
     const { on = '', fullHeight, wrap } = this;
-    const expandable = this.#linesMax > 5;
+    const expandable = this.#lineHeights.length > 5;
     const truncated = expandable && !fullHeight;
     return html`
       <div id="container"
            class="${classMap({ [on]: !!on, wrap, truncated, expandable, fullHeight })}"
            @code-action="${this.#onCodeAction}">
-        <slot id="content" @slotchange="${this.#computeSnippetLines}"></slot>
+        <div id="content-lines">
+          <div id="sizers" aria-hidden="true"></div>
+          <ol id="line-numbers" aria-hidden="true">${this.#lineHeights.map((height, i) => html`
+            <li style="${styleMap({ height })}">${i + 1}</li>`)}
+          </ol>
+          <slot id="content" @slotchange="${this.#computeLineNumbers}"></slot>
+        </div>
         <slot id="actions"
               name="actions"
               ?hidden="${!this.#slots.hasSlotted('actions')}"></slot>
         <button id="expand"
                 ?hidden="${!expandable}"
                 @click="${this.#onClickExpand}">
-          <span>^</span>
+          <svg xmlns="http://www.w3.org/2000/svg"
+               fill="currentColor"
+               viewBox="0 0 11 7">
+            <path d="M4.919.239.242 4.847a.801.801 0 0 0 0 1.148l.778.766a.83.83 0 0 0 1.165 0L5.5 3.495 8.815 6.76a.83.83 0 0 0 1.165 0l.778-.766a.802.802 0 0 0 0-1.148L6.08.239a.826.826 0 0 0-1.162 0Z"/>
+          </svg>
           <slot name="show-more" ?hidden="${this.fullHeight}">Show more</slot>
           <slot name="show-less" ?hidden="${!this.fullHeight}">Show less</slot>
         </button>
@@ -81,22 +99,70 @@ export class RhCodeBlock extends LitElement {
   }
 
   protected override firstUpdated(): void {
-    this.#computeSnippetLines();
+    this.#computeLineNumbers();
   }
 
   protected override updated(changed: PropertyValues<this>): void {
     if (changed.has('wrap')) {
-      this.#computeSnippetLines();
+      // this.#computeSnippetLines();
+      this.#computeLineNumbers();
     }
   }
 
   /**
    * Clone the text content and connect it to the document, in order to calculate the number of lines
+   * @license portions copyright prism.js authors (MIT license)
    */
-  #computeSnippetLines() {
+  #computeLineNumbers() {
     const slot = this.shadowRoot?.getElementById('content') as HTMLSlotElement;
-    this.#linesMax = computeLineNumber(slot);
-    this.requestUpdate('#linesMax', 0);
+
+    const codes: HTMLElement[] = slot.assignedElements().flatMap(x =>
+        x instanceof HTMLScriptElement ||
+        x instanceof HTMLPreElement ? [x]
+      : []);
+
+    const infos: CodeLineHeightsInfo[] = codes.map(element => {
+      const sizer = document.createElement('span');
+      sizer.className = 'sizer';
+      sizer.innerText = '0';
+      sizer.style.display = 'block';
+      this.shadowRoot?.querySelector('#sizers')?.appendChild(sizer);
+      return {
+        lines: element.textContent?.split(/\n(?!$)/g) ?? [],
+        lineHeights: [],
+        sizer,
+        oneLinerHeight: sizer.getBoundingClientRect().height,
+      };
+    });
+
+    for (const { lines, lineHeights, sizer, oneLinerHeight } of infos) {
+      lineHeights[lines.length - 1] = undefined; // why?
+      lines.forEach((line, i) => {
+        if (line.length > 1) {
+          const e = sizer.appendChild(document.createElement('span'));
+          e.style.display = 'block';
+          e.textContent = line;
+        } else {
+          lineHeights[i] = oneLinerHeight;
+        }
+      });
+    }
+
+    for (const { sizer, lineHeights } of infos) {
+      let childIndex = 0;
+      for (let i = 0; i < lineHeights.length; i++) {
+        if (lineHeights[i] === undefined) {
+          lineHeights[i] = sizer.children[childIndex++].getBoundingClientRect()?.height ?? 0;
+        }
+      }
+      sizer.remove();
+    }
+
+    this.#lineHeights = infos.flatMap(x =>
+      x.lineHeights?.map(y =>
+        `${y ?? x.oneLinerHeight}px` as const));
+
+    this.requestUpdate('#linesNumbers', 0);
   }
 
   #onCodeAction(event: RhCodeActionEvent) {
