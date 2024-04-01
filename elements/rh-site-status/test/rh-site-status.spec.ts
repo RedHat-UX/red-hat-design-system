dc580dd220605bbbf99ca55c36480963b47636ed
Steven Spriggs <steven.spriggs@gmail.com>
2024-04-01 14:18:18 -0400

    test(site-status): improve tests

--- elements/rh-site-status/test/rh-site-status.spec.ts
+++ elements/rh-site-status/test/rh-site-status.spec.ts
@@ -3,6 +3,23 @@ import { createFixture } from '@patternfly/pfe-tools/test/create-fixture.js';
 import { RhSiteStatus } from '@rhds/elements/rh-site-status/rh-site-status.js';
 
 
+function overrideFetch(ok: boolean, status: number, statusText: string, json?: () => Promise<unknown>) {
+  window.fetch = new Proxy(window.fetch, {
+    apply: (target, thisArg, args) => {
+      if (args[0] === 'https://status.redhat.com/index.json') {
+        return Promise.resolve({
+          ok,
+          status,
+          statusText,
+          json
+        });
+      }
+      return target.apply(thisArg, args);
+    }
+  });
+}
+
+
 describe('<rh-site-status>', function() {
   describe('simply instantiating', function() {
     let element: RhSiteStatus;
@@ -16,43 +33,57 @@ describe('<rh-site-status>', function() {
     });
   });
 
-  // TODO: We can't rely on default endpoint to always return a status 200
-  // Figure out how to mock the fetch call, should we store a json file locally that
-  // contains the different state status responses?
-  // describe('when api call is successful', function() {
-  //   let element: RhSiteStatus;
-
-  //   beforeEach(async function() {
-  //     element = await createFixture<RhSiteStatus>(html`<rh-site-status></rh-site-status>`);
-  //     await element.updateComplete;
-  //   });
-
-  //   it('should show an success message', async function() {
-  //     // delaying 1 second to allow the fetch to succeed
-  //     setTimeout(() => {
-  //       expect(element.shadowRoot!.textContent).to.include('All systems operational');
-  //     }, 1000);
-  //   });
-  // });
-
-  describe('when endpoint is incorrect', function() {
+  describe('when endpoint returns an All systems operational', function() {
     let element: RhSiteStatus;
 
     beforeEach(async function() {
+      overrideFetch(true, 200, 'All systems operational', () => Promise.resolve({ status: { indicator: 'none', description: 'All systems operational' } }));
+
       element = await createFixture<RhSiteStatus>(html`<rh-site-status></rh-site-status>`);
-      element.endpoint = 'index.json';
       await element.updateComplete;
     });
 
-    it('should show a loading spinner', async function() {
-      expect(element.shadowRoot!.querySelector('rh-spinner')).to.exist;
+    it('should show all systems operational message', async function() {
+      // delaying 100ms to allow the fetch to complete
+      setTimeout(() => {
+        expect(element.shadowRoot!.textContent).to.include('All systems operational');
+      }, 1000);
+    });
+  });
+
+  describe('when endpoint returns a 404', function() {
+    let element: RhSiteStatus;
+
+    beforeEach(async function() {
+      overrideFetch(false, 404, 'Not Found');
+
+      element = await createFixture<RhSiteStatus>(html`<rh-site-status></rh-site-status>`);
+      await element.updateComplete;
     });
 
     it('should show an error message', async function() {
-      // delaying 1 second to allow the fetch to fail
+      // delaying 100ms to allow the fetch to complete
       setTimeout(() => {
-        expect(element.shadowRoot!.textContent).to.include('Error loading status');
-      }, 1000);
+        expect(element.shadowRoot!.textContent).to.include('Error: 404 Not Found');
+      }, 100);
     });
+  });
+});
+
+describe('when endpoint returns a partial minor outage ', function() {
+  let element: RhSiteStatus;
+
+  beforeEach(async function() {
+    overrideFetch(true, 200, 'Partially Degraded Service', () => Promise.resolve({ status: { indicator: 'minor', description: 'Partially Degraded Service' } }));
+
+    element = await createFixture<RhSiteStatus>(html`<rh-site-status></rh-site-status>`);
+    await element.updateComplete;
+  });
+
+  it('should show partial outage message', async function() {
+    // delaying 100ms to allow the fetch to complete
+    setTimeout(() => {
+      expect(element.shadowRoot!.textContent).to.include('Partial service');
+    }, 100);
   });
 });
