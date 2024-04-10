cbb266acfd979ce1dcc1a9fca546ccf674258189
Adam Johnson <adam.b.johnson1@gmail.com>
2024-04-10 10:28:54 -0400

    docs(skip-link): added JSDoc

--- elements/rh-skip-link/rh-skip-link.ts
+++ elements/rh-skip-link/rh-skip-link.ts
@@ -4,8 +4,13 @@ import { customElement } from 'lit/decorators/custom-element.js';
 import styles from './rh-skip-link.css';
 
 /**
- * Skip Link
- * @slot - Place element content here
+ * A skip link is used to skip repetitive content on a page.
+ * It is hidden by default and can be activated by hitting
+ * the "Tab" key after loading/refreshing a page.
+ *
+ * @summary Skip to the main content of a page
+ *
+ * @slot - Place an anchor tag with an `href` that targets an ID of the main content on the page.
  */
 @customElement('rh-skip-link')
 export class RhSkipLink extends LitElement {
