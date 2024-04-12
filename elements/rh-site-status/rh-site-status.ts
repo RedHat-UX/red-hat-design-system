53d3db98cd532222af4a0aea4dc5f6874af6b9b1
Steven Spriggs <steven.spriggs@gmail.com>
2024-04-12 08:45:16 -0400

    fix(site-status): remove unused part

--- elements/rh-site-status/rh-site-status.ts
+++ elements/rh-site-status/rh-site-status.ts
@@ -96,7 +96,7 @@ export class RhSiteStatus extends LitElement {
   render() {
     const { on = '' } = this;
     return html`
-      <a href="https://status.redhat.com/" part="container" class="${classMap({ [on]: !!on })}">
+      <a href="https://status.redhat.com/" class="${classMap({ [on]: !!on })}">
         ${this.#isLoading ? html`<rh-spinner size="sm"></rh-spinner>` : html`
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="17" viewBox="0 0 16 17" fill="none">
             ${this.#icon}
