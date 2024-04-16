c6631eefe22d14726b14efebc21dedc22fdce9ad
Steven Spriggs <steven.spriggs@gmail.com>
2024-04-16 15:58:06 -0400

    docs(site-status): improve demos with loading state demo

--- elements/rh-site-status/demo/fetch-override.js
+++ elements/rh-site-status/demo/fetch-override.js
@@ -1,15 +1,13 @@
+
+const { fetch: originalFetch } = window;
+
 export function overrideFetch(ok, status, statusText, json) {
-  window.fetch = new Proxy(window.fetch, {
-    apply: (target, thisArg, args) => {
-      if (args[0] === 'https://status.redhat.com/index.json') {
-        return Promise.resolve({
-          ok,
-          status,
-          statusText,
-          json
-        });
-      }
-      return target.apply(thisArg, args);
-    }
-  });
+  window.fetch = async () => {
+    return Promise.resolve({
+      ok,
+      status,
+      statusText,
+      json
+    });
+  };
 }
