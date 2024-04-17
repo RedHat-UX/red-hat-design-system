581e712662b0c69cad129434a652d20f5e0fa58d
Steven Spriggs <steven.spriggs@gmail.com>
2024-04-17 10:06:02 -0400

    docs(site-status): readding arg check to fetch override

--- elements/rh-site-status/demo/fetch-override.js
+++ elements/rh-site-status/demo/fetch-override.js
@@ -1,13 +1,15 @@
-
-const { fetch: originalFetch } = window;
-
 export function overrideFetch(ok, status, statusText, json) {
-  window.fetch = async () => {
-    return Promise.resolve({
-      ok,
-      status,
-      statusText,
-      json
-    });
-  };
+  window.fetch = new Proxy(window.fetch, {
+    apply: (target, thisArg, args) => {
+      if (args[0] === 'https://status.redhat.com/index.json') {
+        return Promise.resolve({
+          ok,
+          status,
+          statusText,
+          json
+        });
+      }
+      return target.apply(thisArg, args);
+    }
+  });
 }
