b28c8819811acc9b9bc8417b60563df589f68a51
Steven Spriggs <steven.spriggs@gmail.com>
2024-04-18 02:28:02 -0400

    feat(site-status): add `<rh-site-status>` (#1507)
    * feat(site-status): add site-status
    
    * fix(site-status): add missing export
    
    * chore(site-stutus): fix comment
    
    * fix(site-status): lint
    
    * fix(site-status): add color-context
    
    * test(site-staus): fix missing semi colon
    
    * fix(site-status): remove errant import
    
    * fix(site-status): allow endpoint to be overridden
    
    * docs(site-status): add incorrect endpoint demo
    
    * docs(site-status): correct auto generated template for demo
    
    * test(site-status): improve tests
    
    * fix(site-status): lint
    
    * fix(site-status): lint
    
    * fix(site-status): ugh lint...
    
    * fix(site-status): revert customizable endpoint, add href to status page link
    
    * test(site-status): improve tests
    
    * docs(site-status): add status demos
    
    * docs(site-status): stub docs
    
    * docs(site-status): add jsdoc
    
    * docs: add site-status to related items
    
    * fix(site-status): add type guard for api response
    
    * docs(site-status): update readme
    
    * docs(site-status): imporove jsdoc
    
    * docs(site-status): add docs pages and images
    
    * chore(site-status): add changeset
    
    * chore(site-status): update changeset to include example
    
    * chore(site-status): remove unused demo files
    
    * fix(site-status): remove unused part
    
    * style(site-status): reorder type
    
    * fix(site-status): make #isApiStatus static
    
    * fix(site-status): class decorators cant be use with static private identifier
    
    * fix(site-status): move getStatus to firstUpdated, remove extraneous throw
    
    * fix(site-status): handle capitalization in css
    
    * fix(site-status): correct first letter uppercase style
    
    * fix(site-status): revert response error removal
    
    * fix(site-status): remove status as statusText includes the 404 text.
    
    * fix(site-status): correct viewbox attr
    
    * docs(site-status): add repoStatus
    
    * test(site-status): use sinon for fetch stub
    
    * test(site-status): remove unused aTimeout
    
    * fix(site-status): switch api call to a try catch
    
    * fix(site-status): remove contextProvider not used in design spec
    
    * fix(site-status): use block instead of display contents on host
    
    * fix(site-status): move restore of fetch to top describes
    
    * fix(site-status): implement loading-text slot for translations and accessibility with aria-polite and aria-busy
    
    * docs(site-status): improve demos with loading state demo
    
    * fix(site-status): add focus and hover states
    
    * docs(site-status): add if status is still loading to accessibility
    
    * fix(site-status): readding color context provider
    
    * chore(site-status): lint
    
    * fix(site-status): remove context provider add dark styles
    
    * docs(site-status): readding arg check to fetch override
    
    * chore(site-status): lint

---
+++ elements/rh-site-status/test/rh-site-status.spec.ts
@@ -0,0 +1,110 @@
+import { expect, html } from '@open-wc/testing';
+import { createFixture } from '@patternfly/pfe-tools/test/create-fixture.js';
+import { RhSiteStatus } from '@rhds/elements/rh-site-status/rh-site-status.js';
+
+import { stub, type SinonStub } from 'sinon';
+
+describe('<rh-site-status>', function() {
+  let element: RhSiteStatus;
+  let fetchStub: SinonStub;
+
+  afterEach(function() {
+    fetchStub?.restore?.();
+  });
+
+  describe('simply instantiating', function() {
+    beforeEach(async function() {
+      fetchStub = stub(window, 'fetch');
+      fetchStub.resolves({
+        ok: true,
+        status: 200,
+        statusText: 'OK',
+        json: () => Promise.resolve({
+          status: {
+            indicator: 'none',
+            description: 'All systems operational'
+          }
+        })
+      });
+      element = await createFixture<RhSiteStatus>(html`<rh-site-status></rh-site-status>`);
+      await element.updateComplete;
+    });
+
+    it('should upgrade', async function() {
+      element = await createFixture<RhSiteStatus>(html`<rh-site-status></rh-site-status>`);
+      const klass = customElements.get('rh-site-status');
+      expect(element)
+        .to.be.an.instanceOf(klass)
+        .and
+        .to.be.an.instanceOf(RhSiteStatus);
+    });
+  });
+
+  describe('when endpoint returns an All systems operational', function() {
+    beforeEach(async function() {
+      fetchStub = stub(window, 'fetch');
+      fetchStub.resolves({
+        ok: true,
+        status: 200,
+        statusText: 'OK',
+        json: () => Promise.resolve({
+          status: {
+            indicator: 'none',
+            description: 'All Systems Operational'
+          }
+        })
+      });
+      element = await createFixture<RhSiteStatus>(html`<rh-site-status></rh-site-status>`);
+      await element.updateComplete;
+    });
+
+    it('should show all systems operational message', async function() {
+      // we moved the text capitalization styling to a CSS concern, so can't test for the text capitalization here.
+      expect(element.shadowRoot!.textContent).to.include('All Systems Operational');
+    });
+  });
+
+  describe('when endpoint returns a 404', function() {
+    beforeEach(async function() {
+      fetchStub = stub(window, 'fetch');
+      fetchStub.resolves({
+        ok: false,
+        status: 404,
+        statusText: '404 Not Found',
+      });
+      element = await createFixture<RhSiteStatus>(html`<rh-site-status></rh-site-status>`);
+      await element.updateComplete;
+    });
+
+    afterEach(function() {
+      fetchStub.restore();
+    });
+
+    it('should show an error message', async function() {
+      expect(element.shadowRoot!.textContent).to.include('Error loading status');
+    });
+  });
+
+  describe('when endpoint returns a partial minor outage ', function() {
+    beforeEach(async function() {
+      fetchStub = stub(window, 'fetch');
+      fetchStub.resolves({
+        ok: true,
+        status: 200,
+        statusText: 'OK',
+        json: () => Promise.resolve({
+          status: {
+            indicator: 'minor',
+            description: 'Partially Degraded Service'
+          }
+        })
+      });
+      element = await createFixture<RhSiteStatus>(html`<rh-site-status></rh-site-status>`);
+      await element.updateComplete;
+    });
+
+    it('should show partial outage message', async function() {
+      expect(element.shadowRoot!.textContent).to.include('Partial service');
+    });
+  });
+});
