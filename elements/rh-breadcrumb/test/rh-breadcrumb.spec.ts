bafdc14ee4c3ecdd1a53b8d15527635a45f25f16
Adam Johnson <adam.b.johnson1@gmail.com>
2024-05-20 14:11:52 -0400

    fix(breadcrumb): update breadcrumb demo html

--- elements/rh-breadcrumb/test/rh-breadcrumb.spec.ts
+++ elements/rh-breadcrumb/test/rh-breadcrumb.spec.ts
@@ -24,10 +24,12 @@ describe('<rh-breadcrumb>', function() {
       element = await createFixture<RhBreadcrumb>(html`
         <rh-breadcrumb>
           <ol>
-            <li><a href="../../../..">Home</a></li>
-            <li><a href="../../../">About</a></li>
-            <li><a href="../../">Our Team</a></li>
-            <li><a href="#" aria-current="page">Jamie Simeon Walinsky-Choeffer Houseman Stalinsky</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="../../../">Products</a></li>
+            <li><a href="../../">Red Hat OpenShift on AWS</a></li>
+            <li><a href="../../">4</a></li>
+            <li><a href="../../">Introduction to ROSA</a></li>
+            <li><a href="#" aria-current="page">Chapter 1. Understanding ROSA</a></li>
           </ol>
         </rh-breadcrumb>
       `);
