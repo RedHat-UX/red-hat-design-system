26fc20114d6cd8cacced28964f5dda5ced93a304
Steven Spriggs <steven.spriggs@gmail.com>
2024-06-04 11:44:57 -0400

    docs: reduce jank in hero load

--- docs/assets/javascript/elements/uxdot-hero.js
+++ docs/assets/javascript/elements/uxdot-hero.js
@@ -14,6 +14,7 @@ class UxdotHero extends LitElement {
       display: flex;
       flex-direction: column;
       align-items: center;
+      width: 100%;
     }
 
     slot[name="header"]::slotted(*) {
