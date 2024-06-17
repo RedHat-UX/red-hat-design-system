2c93610d3b26536d42d57df561e6a3e7fe7d22d5
Adam Johnson <adam.b.johnson1@gmail.com>
2024-06-17 15:47:03 -0400

    chore(breadcrumb): resolve typescript warning

--- elements/rh-breadcrumb/rh-breadcrumb.ts
+++ elements/rh-breadcrumb/rh-breadcrumb.ts
@@ -12,11 +12,8 @@ import styles from './rh-breadcrumb.css';
  * of links to the parent pages of the current page in hierarchical order. It
  * helps users find their place within a website or web application.
  * @summary Links displaying a hierarchical location
- *
  * @slot - Place an ordered list (`<ol>`) of your breadcrumbs into the slot
- *
  * @csspart container - container element for slotted breadcrumb
- *
  * @cssprop --rh-breadcrumb-link-color
  *         The link color for each anchor in the list
  *         {@default `var(--rh-color-interactive-blue-darker, #0066cc)`}
@@ -57,7 +54,7 @@ import styles from './rh-breadcrumb.css';
  *          The `subtle` variant background image separating each breadcrumb item
  *          {@default `var(--rh-breadcrumb-caret-image-subtle, url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='10' height='15' viewBox='0 0 10 15' fill='none'%3E%3Cg clip-path='url(%23clip0_232_19576)'%3E%3Cpath d='M2.5 14.5L0.5 12.5L5.5 7.5L0.5 2.5L2.5 0.5L9.5 7.5L2.5 14.5Z' fill='%23a3a3a3'/%3E%3C/g%3E%3Cdefs%3E%3CclipPath id='clip0_232_19576'%3E%3Crect width='10' height='14' fill='white' transform='matrix(-1 0 0 1 10 0.5)'/%3E%3C/clipPath%3E%3C/defs%3E%3C/svg%3E"))`}
  *
-*/
+ */
 
 @customElement('rh-breadcrumb')
 export class RhBreadcrumb extends LitElement {
