622bfe4ecb40022a84b94aae55eb2f8e252ce873
Benny Powers <web@bennypowers.com>
2024-06-28 09:56:37 +0300

    refactor: use slots for messages, prefer attrs

--- elements/rh-switch/rh-switch.ts
+++ elements/rh-switch/rh-switch.ts
@@ -1,4 +1,4 @@
-import { LitElement, html } from 'lit';
+import { LitElement, html, isServer } from 'lit';
 import { customElement } from 'lit/decorators/custom-element.js';
 import { property } from 'lit/decorators/property.js';
 import { classMap } from 'lit/directives/class-map.js';
@@ -6,13 +6,12 @@ import { classMap } from 'lit/directives/class-map.js';
 import { InternalsController } from '@patternfly/pfe-core/controllers/internals-controller.js';
 import { SlotController } from '@patternfly/pfe-core/controllers/slot-controller.js';
 
-import { observed } from '@patternfly/pfe-core/decorators/observed.js';
-
 import { DirController } from '../../lib/DirController.js';
 import { colorContextConsumer, type ColorTheme } from '../../lib/context/color/consumer.js';
 
 
 import styles from './rh-switch.css';
+import { getRandomId } from '@patternfly/pfe-core/functions/random.js';
 
 /**
  * A switch toggles the state of a setting (between on and off). Switches and checkboxes can often be used interchangeably, but the switch provides a more explicit, visible representation on a setting.
@@ -33,12 +32,15 @@ export class RhSwitch extends LitElement {
 
   @property({ reflect: true, type: Boolean, attribute: 'show-check-icon' }) showCheckIcon = false;
 
-  @observed
   @property({ reflect: true, type: Boolean }) checked = false;
 
   @property({ reflect: true, type: Boolean }) disabled = false;
 
   @property({ reflect: true, attribute: 'accessible-label' }) accessibleLabel?: string;
+
+  @property({ reflect: true, attribute: 'message-on' }) messageOn?: string;
+
+  @property({ reflect: true, attribute: 'message-off' }) messageOff?: string;
 
   @property({ reflect: true, type: Boolean }) reversed = false;
 
@@ -49,11 +51,13 @@ export class RhSwitch extends LitElement {
 
   #internals = InternalsController.of(this, { role: 'switch' });
 
-  #slots = new SlotController(this);
+  #slots = new SlotController(this, null, 'message-on', 'message-off');
 
   #dir = new DirController(this);
 
-  #internalColorPalette?: string | null;
+  get #message() {
+    return this.checked ? this.messageOn : this.messageOff;
+  }
 
   get labels(): NodeListOf<HTMLLabelElement> {
     return this.#internals.labels as NodeListOf<HTMLLabelElement>;
@@ -65,41 +69,57 @@ export class RhSwitch extends LitElement {
 
   override connectedCallback(): void {
     super.connectedCallback();
-
     this.tabIndex = 0;
     this.addEventListener('click', this.#onClick);
     this.addEventListener('keyup', this.#onKeyup);
     this.addEventListener('keydown', this.#onKeyDown);
-    this.#updateLabels();
   }
 
   willUpdate() {
-    /*
-     * TEMPORARY: this fixes the need to access the parents color-palette in order to get the `lightest`
-     * value.  This fix will only update the component when switching between light and dark themes as
-     * thats when the consumer requests an update.  Switching between lighter -> light for example will
-     * not trigger the component to update at this time.
-     */
-    this.#internalColorPalette = this.closest('[color-palette]')?.getAttribute('color-palette');
-
     this.#internals.ariaChecked = String(!!this.checked);
     this.#internals.ariaDisabled = String(!!this.disabled);
     this.#internals.ariaLabel = this.accessibleLabel ?? '';
+    const noMessageOn = this.#slots.isEmpty('message-on');
+    const noMessageOff = this.#slots.isEmpty('message-off');
+    if (noMessageOn || noMessageOff) {
+      this.#internals.ariaDescription = this.#message ?? '';
+    } else {
+      const stateSlotName = this.checked ? 'message-on' : 'message-off';
+      const stateEls = this.#slots.getSlotted(stateSlotName);
+      for (const el of stateEls) {
+        el.id ||= getRandomId('rh-switch-message');
+      }
+      if ('ariaDescribedByElements' in ElementInternals) {
+        // see https://w3c.github.io/aria/#dom-ariamixin
+        // @ts-expect-error: InternalsController has the wrong type here, this is a FrozenArray
+        this.#internals.ariaDescribedByElements = stateEls;
+      } else {
+        !isServer && this.setAttribute('aria-describedby', stateEls.map(x => x.id).join(' '));
+      }
+    }
   }
 
   render() {
     const rtl = this.#dir.dir === 'rtl';
     const { on = '' } = this;
     return html`
-    <div id="container" part="container" class="${classMap({ [on]: !!on, rtl, [`color-palette-${this.#internalColorPalette}`]: !!this.#internalColorPalette })}">
-      ${this.reversed ? html`
-        <slot></slot>
-        ${this.#switchTemplate()}
-      ` : html`
-        ${this.#switchTemplate()}
-        <slot></slot>
-      `}
-    </div>
+      <div id="container"
+           part="container"
+           class="${classMap({ [on]: !!on, rtl })}">
+        ${this.reversed ? html`
+          <slot></slot>
+          ${this.#switchTemplate()}
+        ` : html`
+          ${this.#switchTemplate()}
+          <slot></slot>
+        `}
+        <slot name="message-on" ?hidden="${!this.checked}">
+          <span aria-hidden="true">${this.messageOn}</span>
+        </slot>
+        <slot name="message-off" ?hidden="${this.checked}">
+          <span aria-hidden="true">${this.messageOff}</span>
+        </slot>
+      </div>
     `;
   }
 
@@ -108,11 +128,11 @@ export class RhSwitch extends LitElement {
       <div id="switch" part="switch">
         ${this.showCheckIcon ? html`
           <svg id="toggle"
-              role="presentation"
-              fill="currentColor"
-              height="1em"
-              width="1em"
-              viewBox="0 0 512 512">
+               role="presentation"
+               fill="currentColor"
+               height="1em"
+               width="1em"
+               viewBox="0 0 512 512">
             <path d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z" />
           </svg>
         ` : ``}
@@ -156,13 +176,6 @@ export class RhSwitch extends LitElement {
     }
   }
 
-  private _checkedChanged(newValue: boolean, oldValue: boolean) {
-    if ((newValue === oldValue) || (oldValue === undefined)) {
-      return;
-    }
-    this.#updateLabels();
-  }
-
   #toggle() {
     if (this.disabled) {
       return;
@@ -170,20 +183,6 @@ export class RhSwitch extends LitElement {
 
     this.checked = !this.checked;
     this.dispatchEvent(new Event('change', { bubbles: true }));
-  }
-
-  #updateLabels() {
-    const labelState = this.checked ? 'on' : 'off';
-    const [messages] = this.#slots.getSlotted<HTMLElement>();
-    if (!messages) {
-      return;
-    }
-    const states = messages.querySelectorAll<HTMLElement>('[data-state]');
-    states.forEach(state => {
-      if (state) {
-        state.hidden = state.dataset.state !== labelState;
-      }
-    });
   }
 }
 
