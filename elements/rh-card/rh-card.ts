4de19917fe28d5d6e37da864bdb02664d287bfd3
Benny Powers <web@bennypowers.com>
2024-07-04 13:50:26 +0300

    feat(card): heading typography props

--- elements/rh-card/rh-card.ts
+++ elements/rh-card/rh-card.ts
@@ -27,6 +27,15 @@ import styles from './rh-card.css';
  * @cssprop     {<length>} --rh-card-header-font-size
  *              Font size for header on card
  *              {@default `1.5rem`}
+ * @cssprop     --rh-card-heading-font-family
+ *              The font family for headings in the header and body
+ *              {@default --rh-font-family-heading}
+ * @cssprop     --rh-card-heading-font-size
+ *              The font size for headings in the header and body
+ *              {@default --rh-font-size-heading-sm}
+ * @cssprop     --rh-card-heading-font-weight
+ *              The font weight for headings in the header and body
+ *              {@default --rh-font-weight-body-text-medium}
  */
 @customElement('rh-card')
 export class RhCard extends LitElement {
