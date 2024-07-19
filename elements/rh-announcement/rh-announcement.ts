f9ff57182f9e39e9c6f9cd608ce3997f56b6a429
alypilkons-RH <apilkons@redhat.com>
2024-07-19 10:59:31 -0400

    updated CSS

--- elements/rh-announcement/rh-announcement.ts
+++ elements/rh-announcement/rh-announcement.ts
@@ -37,11 +37,11 @@ const CLOSE_ICON_SVG = svg`
     <path d="M 8.274 6 L 11.686 2.588 C 12.104 2.17 12.104 1.491 11.686 1.072 L 10.928 0.314 C 10.509 -0.105 9.83 -0.105 9.411 0.314 L 6 3.725 L 2.588 0.314 C 2.17 -0.105 1.491 -0.105 1.072 0.314 L 0.314 1.072 C -0.105 1.491 -0.105 2.169 0.314 2.588 L 3.725 6 L 0.314 9.411 C -0.105 9.83 -0.105 10.509 0.314 10.928 L 1.072 11.686 C 1.491 12.104 2.17 12.104 2.588 11.686 L 6 8.274 L 9.411 11.686 C 9.83 12.104 10.509 12.104 10.928 11.686 L 11.686 10.928 C 12.104 10.509 12.104 9.83 11.686 9.411 L 8.274 6 Z"></path>
   </svg>`;
 
-  export class AnnouncementCloseEvent extends Event {
-    constructor() {
-      super('close', { bubbles: true, cancelable: true });
-    }
-  } 
+export class AnnouncementCloseEvent extends Event {
+  constructor() {
+    super('close', { bubbles: true, cancelable: true });
+  }
+}
 
 @customElement('rh-announcement')
 export class RhAnnouncement extends LitElement {
@@ -62,67 +62,69 @@ export class RhAnnouncement extends LitElement {
    *
    * Card always resets its context to `base`, unless explicitly provided with a `color-palette`.
    */
-    @colorContextProvider()
-    @property({ reflect: true, attribute: 'color-palette' })
-      colorPalette?: 'dark' | 'light';
+  @colorContextProvider()
+  @property({ reflect: true, attribute: 'color-palette' })
+    colorPalette?: 'dark' | 'light';
 
-    @property({ reflect: true, type: Boolean }) dismissable = false;
-   
-    @property({ type: Number }) imgHeight = 0;
-    @property({ type: Number }) imgWidth = 0;
-    @property({ type: Boolean }) hasImage = false;
+  @property({ reflect: true, type: Boolean }) dismissable = false;
+  @property({ reflect: true, type: Boolean }) smallimg = false;
 
-    private imageSlotRef: Ref<HTMLSlotElement> = createRef();
+  @property({ type: Number }) imgHeight = 0;
+  @property({ type: Number }) imgWidth = 0;
+  @property({ type: Boolean }) hasImage = false;
 
-    #slots = new SlotController(this, 'image', null, 'cta');
+  private imageSlotRef: Ref<HTMLSlotElement> = createRef();
 
-    #closeHandler() {
-      const event = new AnnouncementCloseEvent();
-      if (this.dispatchEvent(event)) {
-        this.remove();
-      }
+  #slots = new SlotController(this, 'image', null, 'cta');
+
+  #closeHandler() {
+    const event = new AnnouncementCloseEvent();
+    if (this.dispatchEvent(event)) {
+      this.remove();
     }
-      render() {
-      const { on = '', colorPalette = '' } = this;
-      return html`
-      <div id="container"
-            part="container"
-            class="${classMap({ [on]: !!on, [colorPalette]: !!colorPalette, smallImg: this.hasSmallImg() })}">
-          <div id="row">
-            <div id="image"
-              part="image"
-              class="${classMap({ empty: !this.#slots.hasSlotted('image') })}">
-            <slot name="image" ${ref(this.imageSlotRef)}></slot>
-          </div>
-          <div id="content">
-            <div id="body"
-                part="body"
-                class="${classMap({ empty: !this.querySelector(':not([slot])') })}">
-              <slot></slot>
-            </div>
-            <div id="cta"
-                part="cta"
-                class="${classMap({ empty: !this.#slots.hasSlotted('cta') })}">
-              <slot name="cta"></slot>
-            </div>
-            </div>
-          </div>
-          ${!this.dismissable ? '' : html`
-          <div id="header-actions">
-            <button id="close-button"
-                aria-label="Close"
-                confirm
-                @click=${this.#closeHandler}>${CLOSE_ICON_SVG}</button>
-          </div>`}
+  }
+
+  render() {
+    const { on = '', colorPalette = '' } = this;
+    return html`
+  <div id="container"
+        part="container"
+        class="${classMap({ [on]: !!on, [colorPalette]: !!colorPalette, smallImg: this.hasSmallImg() })}">
+      <div id="row">
+        <div id="image"
+          part="image"
+          class="${classMap({ empty: !this.#slots.hasSlotted('image') })}">
+        <slot name="image" ${ref(this.imageSlotRef)}></slot>
+      </div>
+      <div id="content">
+        <div id="body"
+            part="body"
+            class="${classMap({ empty: !this.querySelector(':not([slot])') })}">
+          <slot></slot>
         </div>
-      `;
-    }
+        <div id="cta"
+            part="cta"
+            class="${classMap({ empty: !this.#slots.hasSlotted('cta') })}">
+          <slot name="cta"></slot>
+        </div>
+        </div>
+      </div>
+      ${!this.dismissable ? '' : html`
+      <div id="header-actions">
+        <button id="close-button"
+            aria-label="Close"
+            confirm
+            @click=${this.#closeHandler}>${CLOSE_ICON_SVG}</button>
+      </div>`}
+    </div>
+  `;
+  }
 
-  firstUpdated () {
+  firstUpdated() {
     const slot = this.imageSlotRef.value;
     if (slot) {
       const nodes = slot.assignedNodes({ flatten: true }) as HTMLElement[];
-      const img = nodes.find(node => node instanceof HTMLImageElement) as HTMLImageElement | undefined;
+      const img = nodes.find(node => node instanceof HTMLImageElement) as HTMLImageElement | undefined
 
       if (img) {
         this.imgWidth = img.width;
@@ -136,11 +138,13 @@ export class RhAnnouncement extends LitElement {
   }
 
   private hasSmallImg() {
-    return this.imgWidth <= this.imgHeight
+    if (this.smallimg) {
+      return true;
+    }
+    return this.imgWidth <= this.imgHeight;
   }
-
 }
-  
+
 declare global {
   interface HTMLElementTagNameMap {
     'rh-announcement': RhAnnouncement;
