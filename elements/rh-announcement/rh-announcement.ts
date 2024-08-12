210597fff237272404b663e3e7804c0c8c9d1146
alypilkons-RH <apilkons@redhat.com>
2024-08-12 16:38:02 -0400

    changed button to rh-button and added screenshot png file

--- elements/rh-announcement/rh-announcement.ts
+++ elements/rh-announcement/rh-announcement.ts
@@ -7,6 +7,8 @@ import { colorContextConsumer, type ColorTheme } from '../../lib/context/color/c
 import { colorContextProvider } from '../../lib/context/color/provider.js';
 
 import styles from './rh-announcement.css';
+
+import '@rhds/elements/rh-button/rh-button.js';
 /**
  * Announcements are flexible surfaces used to group information in a full width banner layout, traditionally across the top of a page.
  * They are used to announce new features, promos, or news.
@@ -26,15 +28,6 @@ import styles from './rh-announcement.css';
  * @csspart     footer
  *              The cta for the banner. Contains the cta slot.
  */
-
-const CLOSE_ICON_SVG = svg`
-  <svg 
-    aria-hidden="true" 
-    fill="currentColor" 
-    height="12px" width="12px" 
-    viewBox="0 0 12 12">
-    <path d="M 8.274 6 L 11.686 2.588 C 12.104 2.17 12.104 1.491 11.686 1.072 L 10.928 0.314 C 10.509 -0.105 9.83 -0.105 9.411 0.314 L 6 3.725 L 2.588 0.314 C 2.17 -0.105 1.491 -0.105 1.072 0.314 L 0.314 1.072 C -0.105 1.491 -0.105 2.169 0.314 2.588 L 3.725 6 L 0.314 9.411 C -0.105 9.83 -0.105 10.509 0.314 10.928 L 1.072 11.686 C 1.491 12.104 2.17 12.104 2.588 11.686 L 6 8.274 L 9.411 11.686 C 9.83 12.104 10.509 12.104 10.928 11.686 L 11.686 10.928 C 12.104 10.509 12.104 9.83 11.686 9.411 L 8.274 6 Z"></path>
-  </svg>`;
 
 export class AnnouncementCloseEvent extends Event {
   constructor() {
@@ -104,10 +97,11 @@ export class RhAnnouncement extends LitElement {
       </div>
       ${!this.dismissible ? '' : html`
       <div id="header-actions">
-        <button id="close-button"
+        <rh-button id="close-button"
             aria-label="Close"
+            variant="close"
             confirm
-            @click=${this.#closeHandler}>${CLOSE_ICON_SVG}</button>
+            @click=${this.#closeHandler}></rh-button>
       </div>`}
     </div>
   `;
@@ -119,5 +113,3 @@ declare global {
     'rh-announcement': RhAnnouncement;
   }
 }
-
-
