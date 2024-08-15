afc185f27350b52542891be0991a6b0f06c62a41
Adam Johnson <adam.b.johnson1@gmail.com>
2024-08-15 11:54:11 -0400

    test(announcement): add a11y test

--- elements/rh-announcement/test/rh-announcement.spec.ts
+++ elements/rh-announcement/test/rh-announcement.spec.ts
@@ -1,5 +1,6 @@
 import { expect, html } from '@open-wc/testing';
 import { createFixture } from '@patternfly/pfe-tools/test/create-fixture.js';
+
 import { RhAnnouncement } from '@rhds/elements/rh-announcement/rh-announcement.js';
 
 describe('<rh-announcement>', function() {
@@ -17,5 +18,26 @@ describe('<rh-announcement>', function() {
           .and
           .to.be.an.instanceOf(RhAnnouncement);
     });
+  });
+});
+
+describe('when the element loads', function() {
+  let element: RhAnnouncement;
+  beforeEach(async function() {
+    element = await createFixture<RhAnnouncement>(html`
+      <rh-announcement>
+        <img src="https://placehold.co/120x40?text=logo" slot="image" alt="LOGO" width="120" height="40">
+        <p>Main content</p>
+        <rh-cta slot="cta">
+          <a href="#">CTA Text</a>
+        </rh-cta>
+      </rh-announcement>
+    `);
+    await element.updateComplete;
+  });
+
+  it('should be accessible', async function() {
+    await expect(element)
+        .to.be.accessible();
   });
 });
