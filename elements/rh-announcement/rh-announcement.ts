a26dd8e4d3f72f5ed35c5b02b5d70c7f7adbd3b4
Adam Johnson <adam.b.johnson1@gmail.com>
2024-08-16 14:08:10 -0400

    fix(announcement): use regular button element
    See #1773 and this comment:
    
    https://github.com/RedHat-UX/red-hat-design-system/pull/1722#discussion_r1714173015

--- elements/rh-announcement/rh-announcement.ts
+++ elements/rh-announcement/rh-announcement.ts
@@ -1,4 +1,4 @@
-import { LitElement, html } from 'lit';
+import { LitElement, html, svg } from 'lit';
 import { customElement } from 'lit/decorators/custom-element.js';
 import { property } from 'lit/decorators/property.js';
 import { classMap } from 'lit/directives/class-map.js';
@@ -8,9 +8,16 @@ import { SlotController } from '@patternfly/pfe-core/controllers/slot-controller
 import { colorContextConsumer, type ColorTheme } from '../../lib/context/color/consumer.js';
 import { colorContextProvider } from '../../lib/context/color/provider.js';
 
-import '@rhds/elements/rh-button/rh-button.js';
-
 import styles from './rh-announcement.css';
+
+const CLOSE_ICON_SVG = svg`
+  <svg
+    aria-hidden="true"
+    fill="currentColor"
+    height="12px" width="12px"
+    viewBox="0 0 12 12">
+    <path d="M 8.274 6 L 11.686 2.588 C 12.104 2.17 12.104 1.491 11.686 1.072 L 10.928 0.314 C 10.509 -0.105 9.83 -0.105 9.411 0.314 L 6 3.725 L 2.588 0.314 C 2.17 -0.105 1.491 -0.105 1.072 0.314 L 0.314 1.072 C -0.105 1.491 -0.105 2.169 0.314 2.588 L 3.725 6 L 0.314 9.411 C -0.105 9.83 -0.105 10.509 0.314 10.928 L 1.072 11.686 C 1.491 12.104 2.17 12.104 2.588 11.686 L 6 8.274 L 9.411 11.686 C 9.83 12.104 10.509 12.104 10.928 11.686 L 11.686 10.928 C 12.104 10.509 12.104 9.83 11.686 9.411 L 8.274 6 Z"></path>
+  </svg>`;
 
 export class AnnouncementCloseEvent extends Event {
   constructor() {
@@ -104,12 +111,10 @@ export class RhAnnouncement extends LitElement {
       </div>
       ${!this.dismissible ? '' : html`
       <div id="header-actions">
-        <rh-button id="close-button"
-            variant="close"
-            confirm
-            @click=${this.#closeHandler}>
-          Close
-        </rh-button>
+        <button id="close-button"
+                aria-label="Close"
+                confirm
+                @click=${this.#closeHandler}>${CLOSE_ICON_SVG}</button>
       </div>`}
     </div>
   `;
