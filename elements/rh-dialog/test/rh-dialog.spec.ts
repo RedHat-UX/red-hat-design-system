4a2d5bf525608bd7a35a2b840e593483ac6f1a41
Steven Spriggs <steven.spriggs@gmail.com>
2024-08-19 15:46:48 -0400

    test(dialog): improve readabilty

--- elements/rh-dialog/test/rh-dialog.spec.ts
+++ elements/rh-dialog/test/rh-dialog.spec.ts
@@ -22,10 +22,12 @@ describe('<rh-dialog>', function() {
         .and
         .to.be.an.instanceOf(RhDialog);
   });
+
   describe('with a trigger', function() {
     let element: RhDialog;
     let trigger: RhButton;
     const updateComplete = () => element.updateComplete;
+
     beforeEach(async function() {
       element = await createFixture(html`
         <rh-dialog trigger="trigger">
@@ -35,58 +37,72 @@ describe('<rh-dialog>', function() {
         </rh-dialog>
         <rh-button id="trigger">Open</rh-button>
       `);
-      trigger = document.getElementById('trigger')!;
+      trigger = document.getElementById('trigger')! as RhButton;
     });
+
     describe('clicking the trigger', function() {
       let openEventPromise: Promise<Event>;
       let closeEventPromise: Promise<Event>;
       let cancelEventPromise: Promise<Event>;
+
       beforeEach(function() {
+        element.id = 'trigger';
         openEventPromise = oneEvent(element, 'open');
         closeEventPromise = oneEvent(element, 'close');
         cancelEventPromise = oneEvent(element, 'cancel');
       });
       beforeEach(() => trigger.click());
       beforeEach(updateComplete);
+
       it('opens the dialog', function() {
         expect(element.open).to.be.true;
       });
+
       it('fires "open" event', async function() {
         const openEvent = await openEventPromise;
         expect(openEvent.type).to.equal('open');
       });
+
       describe('pressing Escape', function() {
         beforeEach(press('Escape'));
         beforeEach(updateComplete);
+
         it('closes the dialog', function() {
           expect(element.open).to.be.false;
         });
+
         it('fires the cancel event', async function() {
           const cancelEvent = await cancelEventPromise;
           expect(cancelEvent.type).to.equal('cancel');
         });
       });
+
       describe('clicking outside the dialog', function() {
         beforeEach(() => clickElementAtOffset(document.body, [10, 10]));
         beforeEach(updateComplete);
+
         it('closes the dialog', function() {
           expect(element.open).to.be.false;
         });
+
         it('fires the cancel event', async function() {
           const cancelEvent = await cancelEventPromise;
           expect(cancelEvent.type).to.equal('cancel');
         });
       });
+
       describe('clicking the close button', function() {
         // ordinarily we try our best to avoid querying the shadow root in test files
         // in this case, we feel justified in making an exception, because the "close-button"
         // css part is already included in the element's public API.
         // NOTE: we query specifically for the element with that part, not by shadow class or id
-        beforeEach(() => element.shadowRoot.querySelector('[part="close-button"]')?.click());
+        beforeEach(() => element.shadowRoot?.querySelector<HTMLElement>('[part="close-button"]')?.click());
         beforeEach(updateComplete);
+
         it('closes the dialog', function() {
           expect(element.open).to.be.false;
         });
+
         it('fires the close event', async function() {
           const closeEvent = await closeEventPromise;
           expect(closeEvent.type).to.equal('close');
