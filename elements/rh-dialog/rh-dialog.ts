24d190d8f04c6ab7199f4fc9b2eb82f4b254077c
Adam Johnson <adam.b.johnson1@gmail.com>
2024-09-03 14:03:20 -0400

    feat(dialog): use `rh-button` for dialog close button

--- elements/rh-dialog/rh-dialog.ts
+++ elements/rh-dialog/rh-dialog.ts
@@ -14,6 +14,7 @@ import { query } from 'lit/decorators/query.js';
 import { ifDefined } from 'lit/directives/if-defined.js';
 
 import '@rhds/elements/rh-surface/rh-surface.js';
+import '@rhds/elements/rh-button/rh-button.js';
 
 export class DialogCancelEvent extends Event {
   constructor() {
@@ -145,15 +146,13 @@ export class RhDialog extends LitElement {
                   <slot name="footer"></slot>
                 </footer>
               </div>
-              <button id="close-button"
-                      part="close-button"
-                      aria-label="Close dialog"
-                      @keydown=${this.onKeydown}
-                      @click=${this.close}>
-                <svg fill="currentColor" viewBox="0 0 352 512">
-                  <path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path>
-                </svg>
-              </button>
+              <rh-button variant="close"
+                         id="close-button"
+                         part="close-button"
+                         @keydown=${this.onKeydown}
+                         @click=${this.close}>
+                <span class="visually-hidden">Close Dialog</span>
+              </rh-button>
             </div>
           </div>
         </section>
