f4af71293964f5daa4b320ba3e61981c7aa6b143
Benny Powers <web@bennypowers.com>
2024-09-17 17:38:34 +0300

    docs: copy assets

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -50,7 +50,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('docs/styles/**/*');
   eleventyConfig.addPassthroughCopy('docs/patterns/**/*.css');
   eleventyConfig.addPassthroughCopy('docs/theming/**/*.css');
-  eleventyConfig.addPassthroughCopy('docs/icons/**/*.{css,js}');
+  eleventyConfig.addPassthroughCopy('docs/foundations/icons/**/*.{css,js}');
 
 
   if (isLocal) {
