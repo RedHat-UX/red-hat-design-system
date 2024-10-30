5db8f54e266c8e156e53c3a991d724d9a99ddb4e
Adam Johnson <adam.b.johnson1@gmail.com>
2024-10-30 17:41:24 -0400

    feat(navigation-universal): add dropdown sub-component
    Much cleaner.

---
+++ elements/rh-navigation-universal/rh-navigation-universal-dropdown.ts
@@ -0,0 +1,39 @@
+import { LitElement, html } from 'lit';
+import { customElement } from 'lit/decorators/custom-element.js';
+
+import styles from './rh-navigation-universal-dropdown.css';
+
+/**
+ * A universal navigation dropdown is an element that is slotted into
+ * the universal navigation. It uses the `<details>` and `<summary>`
+ * elements to create a dropdown menu inside universal navigation.
+ * @summary A dropdown sub component for universal navigation.
+ * @slot - Place content to display in the dropdown.
+ * @slot summary-label - The dropdown `<summary>` text. Defaults to "All Red Hat".
+ */
+
+@customElement('rh-navigation-universal-dropdown')
+export class RhNavigationUniversalDropdown extends LitElement {
+  static readonly styles = [styles];
+
+  render() {
+    return html`
+      <details>
+        <summary>
+          <span id="summary-shift">
+            <slot name="summary-label">All Red Hat</slot>
+          </span>
+        </summary>
+        <div id="details-content">
+          <slot></slot>
+        </div>
+      </details>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'rh-navigation-universal-dropdown': RhNavigationUniversalDropdown;
+  }
+}
--- elements/rh-navigation-universal/rh-navigation-universal.ts
+++ elements/rh-navigation-universal/rh-navigation-universal.ts
@@ -1,10 +1,13 @@
-import { SlotController } from '@patternfly/pfe-core/controllers/slot-controller.js';
-
 import { LitElement, html } from 'lit';
 import { customElement } from 'lit/decorators/custom-element.js';
 import { property } from 'lit/decorators/property.js';
 
+import { SlotController } from '@patternfly/pfe-core/controllers/slot-controller.js';
+
+import './rh-navigation-universal-dropdown.js';
+
 import styles from './rh-navigation-universal.css';
+
 
 /**
  * A universal navigation is a secondary navigation element consisting of a list
@@ -26,17 +29,11 @@ export class RhNavigationUniversal extends LitElement {
   @property({ attribute: 'accessible-label' }) accessibleLabel?: string;
 
   /**
-   * Customize the default "All Red Hat" text in the `<summary>` element.
-   * Only relevant for the bordered variant.
-   */
-  @property({ attribute: 'summary-label' }) summaryLabel?: string;
-
-  /**
    * Sets the variant to bordered, pushes the navigation to the right, adds borders on hover
    */
   @property({ reflect: true }) variant?: 'bordered';
 
-  #slots = new SlotController(this, 'personalization-link', null, 'details-content');
+  #slots = new SlotController(this, 'personalization-link', null);
 
   render() {
     const label = this.accessibleLabel ? this.accessibleLabel : 'Universal Navigation';
@@ -53,15 +50,8 @@ export class RhNavigationUniversal extends LitElement {
       <nav aria-label="${label}" id="container" part="container">
         ${this.variant === 'bordered' ? personalizationHtml : ''}
         <slot id="nav-universal-default-slot"></slot>
-        <slot hidden name="details-content"></slot>
       </nav>
     `;
-  }
-
-  firstUpdated() {
-    if (this.variant === 'bordered') {
-      this.#appendListItem();
-    }
   }
 
   protected override async getUpdateComplete(): Promise<boolean> {
@@ -69,56 +59,6 @@ export class RhNavigationUniversal extends LitElement {
       await import('@rhds/elements/rh-icon/rh-icon.js');
     }
     return super.getUpdateComplete();
-  }
-
-  #findUnorderedList(): HTMLUListElement | null {
-    const slot = this.shadowRoot?.querySelector<HTMLSlotElement>('slot:not(slot[name])');
-
-    const content = slot?.assignedElements().find(node => {
-      return node.nodeName.toLowerCase() === 'ul';
-    });
-
-    if (!(content instanceof HTMLUListElement)) {
-      return null;
-    }
-
-    return content;
-  }
-
-  #getDetailsContentHTML(): any { // TODO: Fix `any`
-    const slot = this.shadowRoot?.querySelector<HTMLSlotElement>('slot[name="details-content"]');
-
-    const fragment = document.createDocumentFragment();
-    const nodes = slot?.assignedNodes();
-    if (!nodes) {
-      return;
-    }
-
-    for (const node of nodes) {
-      if (node.nodeType === Node.ELEMENT_NODE) {
-        fragment.appendChild(node.cloneNode(true));
-      }
-    }
-
-    return fragment;
-  }
-
-  #appendListItem(): void {
-    const summaryLabel = this.summaryLabel ? this.summaryLabel : 'All Red Hat';
-    const ul = this.#findUnorderedList();
-    const li = document.createElement('li');
-    const allRedHatContent = `
-      <details id="nav-universal-details">
-        <summary id="nav-universal-summary">
-          <span id="nav-universal-summary-shift">${summaryLabel}</span>
-        </summary>
-        <div id="nav-universal-details-content"></div>
-      </details>
-    `;
-    li.innerHTML = allRedHatContent;
-    const getDetailsContentEl = li.querySelector('#nav-universal-details-content');
-    getDetailsContentEl?.appendChild(this.#getDetailsContentHTML());
-    ul?.appendChild(li);
   }
 }
 
