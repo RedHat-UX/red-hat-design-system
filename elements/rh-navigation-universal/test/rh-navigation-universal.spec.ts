67cef0271934a046c1af9d31f3d737dc92955524
Adam Johnson <adam.b.johnson1@gmail.com>
2024-10-31 15:22:26 -0400

    test(navigation-universal): add a11y related tests

--- elements/rh-navigation-universal/test/rh-navigation-universal.spec.ts
+++ elements/rh-navigation-universal/test/rh-navigation-universal.spec.ts
@@ -1,8 +1,17 @@
 import { expect, html } from '@open-wc/testing';
 import { createFixture } from '@patternfly/pfe-tools/test/create-fixture.js';
+import { a11ySnapshot } from '@patternfly/pfe-tools/test/a11y-snapshot.js';
+
 import { RhNavigationUniversal } from '@rhds/elements/rh-navigation-universal/rh-navigation-universal.js';
 
 describe('<rh-navigation-universal>', function() {
+  before(function() {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = '/elements/rh-navigation-universal/rh-navigation-universal-lightdom.css';
+    document.head.append(link);
+  });
+
   describe('simply instantiating', function() {
     let element: RhNavigationUniversal;
     it('imperatively instantiates', function() {
@@ -16,6 +25,35 @@ describe('<rh-navigation-universal>', function() {
           .to.be.an.instanceOf(klass)
           .and
           .to.be.an.instanceOf(RhNavigationUniversal);
+    });
+  });
+
+  describe('when the element loads', function() {
+    let element: RhNavigationUniversal;
+    beforeEach(async function() {
+      element = await createFixture<RhNavigationUniversal>(html`
+        <rh-navigation-universal variant="bordered" accessible-label="Global Nav">
+          <ul>
+            <li><a href="https://access.redhat.com/">Support</a></li>
+            <li><a href="https://docs.redhat.com/en">Documentation</a></li>
+            <li>
+              <rh-navigation-universal-dropdown>
+                <p>Everything you want is on the other side of fear. <a href="#">Jack Canfield</a></p>
+              </rh-navigation-universal-dropdown>
+            </li>
+          </ul>
+        </rh-navigation-universal>
+      `);
+      await element.updateComplete;
+    });
+
+    it('should be accessible', async function() {
+      await expect(element).to.be.accessible();
+    });
+
+    it('should have a custom aria-label on the nav tag in the shadowroot', async function() {
+      const shadowRootNavTag = element.shadowRoot?.querySelector('nav');
+      expect(shadowRootNavTag?.getAttribute('aria-label')).to.equal('Global Nav');
     });
   });
 });
