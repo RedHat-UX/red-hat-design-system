6824f89981c843dda830fcc3a82fd32239334dfa
bennypowers <bennypowers@users.noreply.github.com>
2024-11-03 16:53:51 +0000

    deploy: d2950e39d30de0a42b3d07becaee4cc91bdf1f93

--- assets/packages/@rhds/elements/elements/rh-icon/rh-icon.d.ts
+++ assets/packages/@rhds/elements/elements/rh-icon/rh-icon.d.ts
@@ -12,9 +12,8 @@ export declare class IconResolveErrorEvent extends ErrorEvent {
 }
 /**
  * Icons represents general concepts and can support text as a decorative
- * element. The icon element, `<rh-icon>`, is a container that allows users to
- * add icons of varying dimensions in the same area without shifting surrounding
- * content.
+ * element. The icon element is a container that allows users to add icons of
+ * varying dimensions in the same area without shifting surrounding content.
  * @summary Decorative element which supports related content
  * @slot - Slotted content is used as a fallback in case the icon doesn't load
  * @fires load - Fired when an icon is loaded and rendered
