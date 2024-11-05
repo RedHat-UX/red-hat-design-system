04b7fe8991124d15c3a6318036af35f151bbd17e
Mark Caron <mark.caron@gmail.com>
2024-11-05 12:00:16 -0500

    chore: deleting files that should have been ignored

--- docs/assets/javascript/elements/uxdot/uxdot-pattern-ssr-controller.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable @stylistic/max-len */
-import { isServer } from 'lit';
-export const UxdotPatternSSRController = isServer ? await import('./uxdot-pattern-ssr-controller-server.js').then(m => m.UxdotPatternSSRControllerServer)
-    : await import('./uxdot-pattern-ssr-controller-client.js').then(m => m.UxdotPatternSSRControllerClient);
-//# sourceMappingURL=uxdot-pattern-ssr-controller.js.map
