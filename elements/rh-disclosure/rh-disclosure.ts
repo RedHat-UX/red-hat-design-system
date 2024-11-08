6e021d23acd7b748553c824e7b6307faed11a0db
Adam Johnson <adam.b.johnson1@gmail.com>
2024-11-08 14:27:28 -0500

    feat(disclosure): enable ESC to close

--- elements/rh-disclosure/rh-disclosure.ts
+++ elements/rh-disclosure/rh-disclosure.ts
@@ -5,16 +5,40 @@ import styles from './rh-disclosure.css';
 
 /**
  * @summary A disclosure is a widget that enables content to be either collapsed (hidden) or expanded (visible).
- * @slot - Place the details, summary, and `.details-content` div here.
+ * @slot - Place the `details`, `summary`, and `.details-content` elements in the default slot.
  */
 @customElement('rh-disclosure')
 export class RhDisclosure extends LitElement {
   static readonly styles = [styles];
 
+  #details!: HTMLDetailsElement;
+  #summary!: HTMLElement;
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.#details = this.querySelector<HTMLDetailsElement>('details')!;
+    this.#summary = this.querySelector<HTMLElement>('details summary')!;
+    this.#details.addEventListener('keydown', this.#closeDetails.bind(this));
+  }
+
+  disconnectedCallback() {
+    this.#details.removeEventListener('keydown', this.#closeDetails.bind(this));
+  }
+
   render() {
     return html`
       <slot></slot>
     `;
+  }
+
+  #closeDetails(event: KeyboardEvent): void {
+    if (this.#details.open === false) {
+      return;
+    }
+    if (event.code === 'Escape') {
+      this.#details.open = false;
+      this.#summary.focus();
+    }
   }
 }
 
