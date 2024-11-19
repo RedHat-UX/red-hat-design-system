f196568c0ec2a7350f75af44ac9aef8e68d8d648
Adam Johnson <adam.b.johnson1@gmail.com>
2024-11-19 17:18:00 -0500

    fix(disclosure): let `render` handle elements/state

--- elements/rh-disclosure/rh-disclosure.ts
+++ elements/rh-disclosure/rh-disclosure.ts
@@ -1,5 +1,6 @@
-import { LitElement, html, isServer } from 'lit';
+import { LitElement, html } from 'lit';
 import { customElement } from 'lit/decorators/custom-element.js';
+import { property } from 'lit/decorators/property.js';
 import { query } from 'lit/decorators/query.js';
 
 import '@rhds/elements/rh-icon/rh-icon.js';
@@ -12,17 +13,24 @@ import styles from './rh-disclosure.css';
  * @slot summary - The title of the disclosure
  * @csspart caret - The caret icon in the shadow DOM
  */
-
 @customElement('rh-disclosure')
 export class RhDisclosure extends LitElement {
   static readonly styles = [styles];
+
+  /**
+   * Sets the disclosure to be in its open state
+   */
+  @property({ type: Boolean, reflect: true }) open = false;
 
   @query('details') private detailsEl!: HTMLDetailsElement;
   @query('summary') private summaryEl!: HTMLElement;
 
   render() {
     return html`
-      <details @keydown=${!isServer && this.#closeDetails}>
+      <details
+        ?open="${this.open}"
+        @keydown="${this.#onKeydown}"
+        @toggle="${this.#onToggle}">
         <summary>
           <rh-icon part="caret" id="caret" set="ui" icon="caret-down"></rh-icon>
           <slot name="summary"></slot>
@@ -34,15 +42,21 @@ export class RhDisclosure extends LitElement {
     `;
   }
 
-  #closeDetails(event: KeyboardEvent): void {
-    if (!this.detailsEl.hasAttribute('open')) {
-      return;
-    }
+  #onToggle(): void {
+    this.open = this.detailsEl.open;
+  }
+
+  #onKeydown(event: KeyboardEvent): void {
     if (event.code === 'Escape') {
       event.stopPropagation();
-      this.detailsEl.removeAttribute('open');
-      this.summaryEl.focus();
+      this.#closeDetails();
     }
+  }
+
+  #closeDetails(): void {
+    this.detailsEl.open = false;
+    this.open = false;
+    this.summaryEl.focus();
   }
 }
 
