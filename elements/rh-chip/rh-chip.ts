bf821be2793a89917c3fbdb7ceb83101d1233a08
Adam Johnson <adam.b.johnson1@gmail.com>
2025-03-03 14:24:52 -0500

    feat(chip): support dark color palettes

--- elements/rh-chip/rh-chip.ts
+++ elements/rh-chip/rh-chip.ts
@@ -3,6 +3,7 @@ import { customElement } from 'lit/decorators/custom-element.js';
 import { classMap } from 'lit/directives/class-map.js';
 import { property } from 'lit/decorators/property.js';
 import { query } from 'lit/decorators/query.js';
+import { colorContextConsumer, type ColorTheme } from '../../lib/context/color/consumer.js';
 import { consume } from '@lit/context';
 
 import { context, type RhChipGroupContext } from './context.js';
@@ -34,6 +35,11 @@ export class RhChip extends LitElement {
   static readonly styles = [styles];
 
   /**
+   * Sets color theme based on parent context
+   */
+  @colorContextConsumer() private on?: ColorTheme;
+
+  /**
    * Whether the chip is checked.
    */
   @property({ type: Boolean, reflect: true }) checked = false;
@@ -50,10 +56,11 @@ export class RhChip extends LitElement {
   private ctx?: RhChipGroupContext;
 
   render() {
+    const { on = 'light' } = this;
     const attrName = this.chipName ? this.chipName : 'chip-checkbox';
     const { size, color } = this.ctx ?? {};
     return html`
-      <label part="chip" class=${classMap({ [`size-${size}`]: !!size, [`color-${color}`]: !!color })}>
+      <label part="chip" class=${classMap({ on: true, [on]: true, [`size-${size}`]: !!size, [`color-${color}`]: !!color })}>
         <slot></slot>
         <input type="checkbox" name=${attrName}
                @input=${this.#onChecked} ?checked=${this.checked}>
