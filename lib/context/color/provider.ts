fd7603f0e6f4337a12dc2c741ebca3ce38d3eae8
Benny Powers <web@bennypowers.com>
2025-03-09 10:58:08 +0200

    feat: reject disallowed color palettes

--- lib/context/color/provider.ts
+++ lib/context/color/provider.ts
@@ -1,6 +1,7 @@
-import { type ReactiveElement } from 'lit';
+import { type ReactiveController, type ReactiveElement } from 'lit';
 
 import styles from '@rhds/tokens/css/color-context-provider.css.js';
+import { Logger } from '@patternfly/pfe-core/controllers/logger.js';
 
 const Palettes = Object.freeze([
   'light',
@@ -27,8 +28,33 @@ interface ColorPaletteElement extends ReactiveElement {
 
 type Constructor<T> = new(...args: any[]) => T;
 
+class PaletteController implements ReactiveController {
+  #host: ReactiveElement & { colorPalette?: ColorPalette };
+  #palettes: Set<ColorPalette>;
+  #last?: ColorPalette;
+  #logger: Logger;
+  constructor(host: ReactiveElement, palettes: ColorPalette[]) {
+    this.#host = host;
+    this.#palettes = new Set(palettes);
+    this.#last = this.#host.colorPalette;
+    this.#host.addController(this);
+    this.#logger = new Logger(host);
+  }
+
+  hostUpdate(): void {
+    const { colorPalette } = this.#host;
+    if (colorPalette && !this.#palettes.has(colorPalette)) {
+      this.#logger.warn(`color-palette="${colorPalette}" is not allowed`);
+      this.#host.colorPalette = this.#last;
+    } else {
+      this.#last = colorPalette;
+    }
+  }
+}
+
 /**
  * Makes this element a [color scheme provider](https://ux.redhat.com/themeing/color-palettes)
+ * Limits the element to the specified color palettes, if provided.
  *
  * @param supportedPalettes list of supported color palettes
  * @see https://ux.redhat.com/themeing/color-palettes
@@ -44,6 +70,7 @@ export function colorSchemeProvider(...supportedPalettes: ColorPalette[]) {
     if (attribute !== 'color-palette' || !reflect) {
       throw new Error('@colorSchemeProvider requires the `color-palette` attribute.');
     }
+    klass.addInitializer(instance => new PaletteController(instance, supportedPalettes));
     klass.styles = [
       ...Array.isArray(klass.styles) ? klass.styles : klass.styles ? [klass.styles] : [],
       styles,
