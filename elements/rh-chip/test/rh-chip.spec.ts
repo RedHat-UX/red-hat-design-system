4a63dd5ab12f86ffa4bc19aeb861c9fa26051cd6
Adam Johnson <adam.b.johnson1@gmail.com>
2025-03-12 16:29:26 -0400

    test(chip): separate test files and add more tests

--- elements/rh-chip/test/rh-chip.spec.ts
+++ elements/rh-chip/test/rh-chip.spec.ts
@@ -1,21 +1,23 @@
-import { expect, html } from '@open-wc/testing';
+import { expect, html, oneEvent } from '@open-wc/testing';
 import { createFixture } from '@patternfly/pfe-tools/test/create-fixture.js';
-import { RhChipGroup } from '@rhds/elements/rh-chip/rh-chip-group.js';
+import { RhChip, ChipChangeEvent } from '@rhds/elements/rh-chip/rh-chip.js';
+import { sendKeys } from '@web/test-runner-commands';
 
-describe('<rh-chip-group>', function() {
+describe('<rh-chip>', function() {
+  let element: RhChip;
+
   describe('simply instantiating', function() {
-    let element: RhChipGroup;
     it('imperatively instantiates', function() {
-      expect(document.createElement('rh-chip-group')).to.be.an.instanceof(RhChipGroup);
+      expect(document.createElement('rh-chip')).to.be.an.instanceof(RhChip);
     });
 
     it('should upgrade', async function() {
-      element = await createFixture<RhChipGroup>(html`<rh-chip-group></rh-chip-group>`);
-      const klass = customElements.get('rh-chip-group');
+      element = await createFixture<RhChip>(html`<rh-chip>Option</rh-chip>`);
+      const klass = customElements.get('rh-chip');
       expect(element)
           .to.be.an.instanceOf(klass)
           .and
-          .to.be.an.instanceOf(RhChipGroup);
+          .to.be.an.instanceOf(RhChip);
     });
   });
 
@@ -23,11 +25,7 @@ describe('<rh-chip-group>', function() {
     let element: RhChipGroup;
     beforeEach(async function() {
       element = await createFixture<RhChipGroup>(html`
-         <rh-chip-group>
-           <rh-chip>Option 1</rh-chip>
-           <rh-chip>Option 2</rh-chip>
-           <rh-chip>Option 3</rh-chip>
-         </rh-chip-group>
+        <rh-chip>Option 1</rh-chip>
       `);
       await element.updateComplete;
     });
@@ -35,6 +33,66 @@ describe('<rh-chip-group>', function() {
     it('should be accessible', async function() {
       await expect(element)
           .to.be.accessible();
+    });
+  });
+
+  function getCheckbox(el: RhChip): HTMLInputElement {
+    return el.shadowRoot!.querySelector('input') as HTMLInputElement;
+  }
+
+  describe('a basic chip', function() {
+    let checkbox: HTMLInputElement;
+
+    beforeEach(async function() {
+      element = await createFixture<RhChip>(html`<rh-chip>Test Chip</rh-chip>`);
+      await element.updateComplete;
+      checkbox = getCheckbox(element);
+    });
+
+    it('fires a change event when clicked', async function() {
+      const changeEventPromise = oneEvent(element, 'change');
+      checkbox.click();
+      const event = await changeEventPromise;
+
+      expect(event).to.be.an.instanceof(ChipChangeEvent);
+      expect(event.type).to.equal('change');
+      expect((event as ChipChangeEvent).checked).to.be.false; // Initial value before change
+
+      // Chip now checked
+      expect(element.checked).to.be.true;
+    });
+  });
+
+  describe('a disabled chip', function() {
+    let checkbox: HTMLInputElement;
+
+    beforeEach(async function() {
+      element = await createFixture<RhChip>(html`<rh-chip disabled>Disabled Chip</rh-chip>`);
+      await element.updateComplete;
+      checkbox = getCheckbox(element);
+    });
+
+    it('does not have the checked attribute on the host', function() {
+      expect(element.getAttribute('checked')).to.be.null;
+    });
+
+    it('has aria-disabled attribute set to true', function() {
+      expect(checkbox.getAttribute('aria-disabled')).to.equal('true');
+    });
+
+    it('prevents checking via click when disabled', async function() {
+      checkbox.click();
+      await element.updateComplete;
+      expect(element.checked).to.be.false;
+    });
+
+    it('prevents checking via keyboard when disabled', async function() {
+      checkbox.focus();
+
+      await sendKeys({ press: ' ' });
+      await element.updateComplete;
+
+      expect(element.checked).to.be.false;
     });
   });
 });
