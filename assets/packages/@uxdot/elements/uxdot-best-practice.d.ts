85b624366a2b4ce5fcf8bf05aed7545e7e652f54
marionnegp <marionnegp@users.noreply.github.com>
2025-03-20 15:10:54 +0000

    deploy: b8c4e0c2671d65c4ff38e968dc903cfe8c59ae53

--- assets/packages/@uxdot/elements/uxdot-best-practice.d.ts
+++ assets/packages/@uxdot/elements/uxdot-best-practice.d.ts
@@ -1,6 +1,6 @@
 import { LitElement } from 'lit';
 export declare class UxdotBestPractice extends LitElement {
     static styles: CSSStyleSheet[];
-    variant: 'do' | 'dont';
+    variant: 'do' | 'dont' | 'caution';
     render(): import("lit-html").TemplateResult<1>;
 }
--- assets/packages/@uxdot/elements/uxdot-best-practice.js
+++ assets/packages/@uxdot/elements/uxdot-best-practice.js
@@ -1,6 +1,6 @@
 import { LitElement, html } from 'lit';
 import { css } from "lit";
-const styles = css `:host{display:block;margin-block:var(--rh-space-2xl)}#container{display:flex;flex-direction:column;gap:var(--rh-space-2xl);margin-block:var(--rh-space-2xl)}span{font-family:var(--rh-font-family-heading);font-size:var(--rh-font-size-heading-xs);font-weight:var(--rh-font-weight-heading-medium);display:flex;flex-direction:row;align-items:center;gap:var(--rh-space-md)}#do span{color:var(--rh-color-status-success)}#dont span{color:var(--rh-color-status-danger)}::slotted(uxdot-example){margin:0!important}figure{margin:0!important}`;
+const styles = css `:host{display:block;margin-block:var(--rh-space-2xl)}#container{display:flex;flex-direction:column;gap:var(--rh-space-2xl);margin-block:var(--rh-space-2xl)}span{font-family:var(--rh-font-family-heading);font-size:var(--rh-font-size-heading-xs);font-weight:var(--rh-font-weight-heading-medium);display:flex;flex-direction:row;align-items:center;gap:var(--rh-space-md)}#do span{color:var(--rh-color-status-success)}#dont span{color:var(--rh-color-status-danger)}#caution span{color:var(--rh-color-yellow-60)}::slotted(uxdot-example){margin:0!important}figure{margin:0!important}`;
 export class UxdotBestPractice extends LitElement {
     constructor() {
         super(...arguments);
@@ -8,13 +8,21 @@ export class UxdotBestPractice extends LitElement {
     }
     render() {
         const { variant } = this;
-        const icon = variant === 'do' ? 'check-circle-fill' : 'close-circle-fill';
-        const title = variant === 'do' ? 'Do' : 'Don\'t';
+        const iconMap = {
+            do: 'check-circle-fill',
+            dont: 'close-circle-fill',
+            caution: 'warning-fill',
+        };
+        const titleMap = {
+            do: 'Do',
+            dont: 'Don\'t',
+            caution: 'Caution',
+        };
         return html `
       <figure id="container">
         <slot name="image"></slot>
         <figcaption id="${variant}">
-          <span><rh-icon set="ui" icon="${icon}" size="md"></rh-icon>${title}</span>
+          <span><rh-icon set="ui" icon="${iconMap[variant]}" size="md"></rh-icon>${titleMap[variant]}</span>
           <slot></slot>
         </figcaption>
       </figure>
