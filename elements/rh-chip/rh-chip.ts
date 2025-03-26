8bc479c1b30b3b28057422502fe494c7dae01e8f
Adam Johnson <adam.b.johnson1@gmail.com>
2025-03-26 14:01:21 -0400

    fix(chip): make chip a FACE

--- elements/rh-chip/rh-chip.ts
+++ elements/rh-chip/rh-chip.ts
@@ -3,15 +3,17 @@ import { customElement } from 'lit/decorators/custom-element.js';
 import { classMap } from 'lit/directives/class-map.js';
 import { ifDefined } from 'lit/directives/if-defined.js';
 import { property } from 'lit/decorators/property.js';
-import { query } from 'lit/decorators/query.js';
-import { themable } from '@rhds/elements/lib/themable.js';
 import { consume } from '@lit/context';
+
+import { InternalsController } from '@patternfly/pfe-core/controllers/internals-controller.js';
 
 import { rhChipGroupSizeCtx } from './context.js';
 
-import '@rhds/elements/rh-icon/rh-icon.js';
+import { themable } from '@rhds/elements/lib/themable.js';
 
 import styles from './rh-chip.css';
+
+import '@rhds/elements/rh-icon/rh-icon.js';
 
 export class ChipChangeEvent extends Event {
   constructor(public checked: boolean) {
@@ -34,28 +36,12 @@ export class ChipChangeEvent extends Event {
 export class RhChip extends LitElement {
   static readonly styles = [styles];
 
+  static formAssociated = true;
+
   /**
    * Whether the chip is checked.
    */
-  @property({
-    type: Boolean, reflect: true,
-    hasChanged(newVal: boolean, oldVal: boolean) {
-      return newVal !== oldVal;
-    },
-  })
-
-  set checked(value: boolean) {
-    const oldValue = this.#checked;
-    this.#checked = value;
-    if (this.chipInput) {
-      this.chipInput.checked = value;
-    }
-    this.requestUpdate('checked', oldValue);
-  }
-
-  get checked(): boolean {
-    return this.#checked;
-  }
+  @property({ type: Boolean, reflect: true }) checked = false;
 
   /**
    * Whether the chip is disabled.
@@ -65,24 +51,28 @@ export class RhChip extends LitElement {
   /**
    * Set a custom string for the input's `value` attribute. Defaults to `on`.
    */
-  @property({ reflect: true, attribute: 'value' }) chipValue?: string;
+  @property({ reflect: true }) value?: string;
 
-  @query('input') private chipInput!: HTMLInputElement;
+  #internals = InternalsController.of(this);
+  #formDisabled = false;
 
   @consume({ context: rhChipGroupSizeCtx, subscribe: true })
   private size?: 'sm';
 
-  #checked = false;
+  formDisabledCallback(disabled: boolean) {
+    this.#formDisabled = disabled;
+    this.requestUpdate();
+  }
 
   render() {
     return html`
       <label part="chip" class=${classMap({ [`size-${this.size}`]: !!this.size })}>
         <slot></slot>
         <input type="checkbox"
-               value="${ifDefined(this.chipValue)}"
+               value="${ifDefined(this.value)}"
                @change="${this.#onChecked}"
-               ?checked="${this.checked}"
-               aria-disabled="${String(this.disabled) as 'true' | 'false'}">
+               .checked="${this.checked}"
+               aria-disabled="${String(this.disabled || this.#formDisabled) as 'true' | 'false'}">
         <rh-icon id="close-icon" set="ui" icon="close-circle"></rh-icon>
       </label>
     `;
@@ -97,17 +87,12 @@ export class RhChip extends LitElement {
     }
 
     if (this.dispatchEvent(new ChipChangeEvent(this.checked))) {
-      this.checked = this.chipInput.checked;
+      this.checked = (event.target as HTMLInputElement).checked;
     }
-  }
 
-  updated(changedProperties: Map<string | number | symbol, unknown>): void {
-    super.updated(changedProperties);
-
-    // Match input's checked state with component's checked property
-    if (changedProperties.has('checked') && this.chipInput) {
-      this.chipInput.checked = this.checked;
-    }
+    const value = this.value ?? this.textContent;
+    const state = this.checked ? 'checked' : 'unchecked';
+    this.#internals.setFormValue(value, state);
   }
 }
 
