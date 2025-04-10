ff6d0dbffbc1f7c0a3d0ade64dc16c4570a24c9e
Benny Powers <web@bennypowers.com>
2025-04-10 11:38:45 +0300

    perf: put .empty on the slot
    no need for a wrapper, we should try to limit the number of dom nodes

--- elements/rh-tile/rh-tile.ts
+++ elements/rh-tile/rh-tile.ts
@@ -149,7 +149,7 @@ export class RhTile extends LitElement {
 
   #logger = new Logger(this);
 
-  #slots = new SlotController(this, { slots: ['icon', 'footer', null] });
+  #slots = new SlotController(this, 'image', 'icon', 'title', 'headline', null, 'footer');
 
   get #isCheckable() {
     return !!this.radioGroup || this.checkable;
@@ -188,7 +188,7 @@ export class RhTile extends LitElement {
   }
 
   render() {
-    const { bleed, compact, checkable, checked, colorPalette, desaturated } = this;
+    const { bleed, compact, checkable, checked, desaturated } = this;
     const disabled = this.disabledGroup || this.disabled || this.#internals.formDisabled;
     const hasSlottedIcon = this.#slots.hasSlotted('icon');
     const hasSlottedBody = this.#slots.hasSlotted(null);
@@ -209,10 +209,8 @@ export class RhTile extends LitElement {
           <slot id="icon"
                 class="${classMap({ compact, checkable })}"
                 name="icon"
-                ?hidden="${this.icon === undefined && !hasSlottedIcon}">
-            ${this.icon !== undefined ?
-              html`<rh-icon icon="${ifDefined(this.icon)}" set="${this.iconSet}"></rh-icon>`
-              : html``}
+                ?hidden="${this.icon === undefined && !hasSlottedIcon}">${this.icon === undefined ? ''
+      : html`<rh-icon icon="${ifDefined(this.icon)}" set="${this.iconSet}"></rh-icon>`}
           </slot>
           <div id="content">
             <div id="header">
@@ -228,9 +226,7 @@ export class RhTile extends LitElement {
                        ?disabled="${disabled}"></input>
               </div>
             </div>
-            <div id="body" class="${classMap({ empty: !hasSlottedBody })}">
-              <slot id="body-text"></slot>
-            </div>
+            <slot id="body" class="${classMap({ empty: this.#slots.isEmpty() })}"></slot>
             <div id="footer" class="${classMap({ empty: !hasSlottedFooter })}">
               <slot id="footer-text" name="footer"></slot><rh-icon set="ui" icon="${linkIcon}"></rh-icon>
             </div>
