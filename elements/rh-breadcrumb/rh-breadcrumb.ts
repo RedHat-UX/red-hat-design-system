cfceeb85c7523220de681fe3611a725c3ca4778e
Adam Johnson <adam.b.johnson1@gmail.com>
2025-05-06 11:53:02 -0400

    feat(breadcrumb): add ability to truncate middle breadcrumb items
    This code was developed with possible assistance by generative AI.

--- elements/rh-breadcrumb/rh-breadcrumb.ts
+++ elements/rh-breadcrumb/rh-breadcrumb.ts
@@ -1,10 +1,20 @@
-import { LitElement, html } from 'lit';
+import { LitElement, html, isServer } from 'lit';
 import { customElement } from 'lit/decorators/custom-element.js';
 import { property } from 'lit/decorators/property.js';
 
 import { themable } from '@rhds/elements/lib/themable.js';
 
 import styles from './rh-breadcrumb.css';
+
+const truncationBtn = html`
+  <li class="truncate-btn-container">
+    <button type="button" class="truncate-btn" aria-expanded="false" title="Show middle breadcrumb items">
+      <span aria-hidden="true">&#8230;</span>
+      <span class="truncate-visually-hidden">
+        Show middle breadcrumb items
+      </span>
+    </button>
+  </li>`;
 
 /**
  * A breadcrumb navigation is a secondary navigation element consisting of a list
@@ -38,6 +48,7 @@ import styles from './rh-breadcrumb.css';
 @customElement('rh-breadcrumb')
 @themable
 export class RhBreadcrumb extends LitElement {
+  static readonly styles = [styles];
   /**
    * Customize the default `aria-label` on the `<nav>` container.
    * Defaults to "Breadcrumb" if no attribute/property is set.
@@ -49,15 +60,74 @@ export class RhBreadcrumb extends LitElement {
    */
   @property({ reflect: true }) variant?: 'subtle';
 
-  static readonly styles = [styles];
+  /**
+ * Breadcrumbs over four items will be truncated and include a button to expand the middle breadcrumb items
+ */
+  @property({ reflect: true, type: Boolean }) truncate? = false;
+
+  #list: HTMLOListElement | null = null;
+  #listItems: NodeListOf<HTMLLIElement> | null | undefined = null;
 
   render() {
     const label = this.accessibleLabel ? this.accessibleLabel : 'Breadcrumb';
     return html`
-      <nav aria-label="${label}" part="container" id="container">
+      <nav 
+        aria-label="${label}"
+        @click="${this.truncate && this.#handleTruncationClick}"
+        @keyup="${this.truncate && this.#onKeyUp}"
+        id="container"
+        part="container">
         <slot></slot>
       </nav>
     `;
+  }
+
+  firstUpdated(): void {
+    if (isServer) {
+      return;
+    }
+    this.#updateLightDOM();
+  }
+
+  #updateLightDOM(): void {
+    this.#list = this.querySelector('ol');
+    if (!this.#list) {
+      return;
+    }
+
+    if (this.#list.children.length < 5) {
+      return;
+    }
+
+    const middleItems = this.#list.querySelectorAll('li:nth-child(n+2):nth-last-child(n+3)');
+    for (const item of middleItems) {
+      item.setAttribute('hidden', 'true');
+    }
+
+    const [truncationBtnString] = truncationBtn.strings;
+    middleItems[0].insertAdjacentHTML('beforebegin', truncationBtnString);
+  }
+
+  #handleTruncationClick(event: Event): void {
+    const target = event.target as HTMLButtonElement;
+    if (!target.closest('.truncate-btn')) {
+      return;
+    }
+
+    this.#listItems = this.#list?.querySelectorAll('li');
+    if (!this.#listItems) {
+      return;
+    }
+    for (const item of this.#listItems) {
+      item.removeAttribute('hidden');
+    }
+
+    target.closest('.truncate-btn-container')?.remove();
+  }
+
+  // appease linter
+  #onKeyUp() {
+    return;
   }
 }
 
