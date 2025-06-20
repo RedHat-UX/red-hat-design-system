04a26f27521588284641e7576b63aa1c5e64dae2
Steven Spriggs <steven.spriggs@gmail.com>
2025-06-20 16:08:29 -0400

    test(navigation-vertical): update tests

--- elements/rh-navigation-vertical/test/rh-navigation-vertical.spec.ts
+++ elements/rh-navigation-vertical/test/rh-navigation-vertical.spec.ts
@@ -1,21 +1,21 @@
 import { expect, html } from '@open-wc/testing';
 import { createFixture } from '@patternfly/pfe-tools/test/create-fixture.js';
-import { RhNavigationTreeView } from '@rhds/elements/rh-navigation-vertical/rh-navigation-vertical.js';
+import { RhNavigationVertical } from '@rhds/elements/rh-navigation-vertical/rh-navigation-vertical.js';
 
 describe('<rh-navigation-vertical>', function() {
   describe('simply instantiating', function() {
-    let element: RhNavigationTreeView;
+    let element: RhNavigationVertical;
     it('imperatively instantiates', function() {
-      expect(document.createElement('rh-navigation-vertical')).to.be.an.instanceof(RhNavigationTreeView);
+      expect(document.createElement('rh-navigation-vertical')).to.be.an.instanceof(RhNavigationVertical);
     });
 
     it('should upgrade', async function() {
-      element = await createFixture<RhNavigationTreeView>(html`<rh-navigation-vertical></rh-navigation-vertical>`);
+      element = await createFixture<RhNavigationVertical>(html`<rh-navigation-vertical></rh-navigation-vertical>`);
       const klass = customElements.get('rh-navigation-vertical');
       expect(element)
           .to.be.an.instanceOf(klass)
           .and
-          .to.be.an.instanceOf(RhNavigationTreeView);
+          .to.be.an.instanceOf(RhNavigationVertical);
     });
   });
 });
