9c107abdb17051df53b160b7b9957372d96d0cd3
Benny Powers <web@bennypowers.com>
2025-08-04 17:44:18 +0300

    test(progress-stepper): add some initial tests

--- elements/rh-progress-stepper/test/rh-progress-stepper.spec.ts
+++ elements/rh-progress-stepper/test/rh-progress-stepper.spec.ts
@@ -22,4 +22,50 @@ describe('<rh-progress-stepper>', function() {
           .to.be.an.instanceOf(RhProgressStepper);
     });
   });
+  describe('with first step active', function() {
+    let element: RhProgressStepper;
+    beforeEach(async function() {
+      element = await createFixture(html`
+        <rh-progress-stepper>
+          <rh-progress-step state="active">1</rh-progress-step>
+          <rh-progress-step>2</rh-progress-step>
+          <rh-progress-step>3</rh-progress-step>
+          <rh-progress-step>4</rh-progress-step>
+          <rh-progress-step>5</rh-progress-step>
+        </rh-progress-stepper>
+      `);
+    });
+  });
+  describe('with more than one step marked as active', function() {
+    let element: RhProgressStepper;
+    beforeEach(async function() {
+      element = await createFixture(html`
+        <rh-progress-stepper>
+          <rh-progress-step href="#" state="active">1</rh-progress-step>
+          <rh-progress-step href="#" state="active">2</rh-progress-step>
+          <rh-progress-step href="#">3</rh-progress-step>
+          <rh-progress-step href="#">4</rh-progress-step>
+          <rh-progress-step href="#">5</rh-progress-step>
+        </rh-progress-stepper>
+      `);
+    });
+    it('sets the last active step as aria-current', async function() {
+      // note: this test may break if we ever move to element internals for aria-current
+      // playwright a11ySnapshot does not yet support aria-current
+      // see https://github.com/microsoft/playwright/issues/36913
+      const [hasAriaCurrent] = Array.from(element.querySelectorAll('rh-progress-step'), x => x.shadowRoot?.querySelector('[aria-current]'))
+          .filter(x => !!x);
+      // super brittle: much better if we could assert on the AT tree
+      const [assignedNode] = hasAriaCurrent?.querySelector('slot')?.assignedNodes() ?? [];
+      expect((assignedNode as Text).data).to.equal('2');
+    });
+    it('does not expose the first step to AT as current', function() {
+      // note: this test may break if we ever move to element internals for aria-current
+      // playwright a11ySnapshot does not yet support aria-current
+      // see https://github.com/microsoft/playwright/issues/36913
+      const hasAriaCurrent = Array.from(element.querySelectorAll('rh-progress-step'), x => x.shadowRoot?.querySelector('[aria-current]'))
+          .filter(x => !!x);
+      expect(hasAriaCurrent.length).to.be.lessThan(2);
+    });
+  });
 });
