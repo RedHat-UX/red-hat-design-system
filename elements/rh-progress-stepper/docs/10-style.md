24a899fe57e3afee69707f5281a5e554b69e45eb
Luke <ldary@redhat.com>
2025-08-06 09:59:13 -0400

    feat(progress-stepper): add <rh-progress-stepper> element (#2449)
    * Progress Stepper Redux
    
    * Fix Lint Errors
    
    * Addressing feedback
    
    * feat(progress-step): adjusting active to inactive step-line-color
    
    * Update demos, slots, and styles
    
    * Address latest feedback
    
    * Pushing latest changes
    
    * Fix lint errors and status
    
    * docs(progress-stepper): inline docs
    
    * docs(progress-stepper): readme
    
    * feat(progress-stepper): CSS cleanup and vertical fixes
    
    * feat(progress-stepper): fixing thumbnail and adding themable to element
    
    * feat(progress-stepper): fixing demos to be simpler and adding color-context and label-attribute demos
    
    * docs(progress-stepper): tag aliases
    
    * feat(progress-stepper): adding new demos, improving API on compact, fixing linked labels, and more
    
    * docs(progress-stepper): update content on Overview page
    
    * docs(progress-stepper): remove outdated code.md page
    
    * docs(progress-stepper): update content on Style page
    
    * docs(progress-stepper): remove outdated images on Guidelines page
    
    * docs(progress-stepper): update content on Guidelines page. WIP.
    
    * docs(progress-stepper): add rwd and best practices to guidelines page
    
    * docs(progress-stepper): add a11y docs page and content
    
    * docs(progress-stepper): update content for all elements page
    
    * feat(progress-stepper): adding mobile support, cleaning up host styles
    
    * chore(progress-stepper): match changeset to RHDS new element formatting
    
    * test(progress-stepper): add some initial tests
    
    * style: whitespace
    
    * refactor: make current step a context
    
    * feat(progress-stepper): minor css grid adjustments to match designs more closely
    
    * fix(progress-stepper): output current step to shadowdom for non-compact variants
    
    * feat(progress-stepper): updates to JSDocs
    
    * docs(progress-stepper): Adding coming soon to interaction states section
    
    * Update elements/rh-progress-stepper/README.md
    
    Co-authored-by: Adam Johnson <adjohnso@redhat.com>
    
    * Update elements/rh-progress-stepper/rh-progress-stepper.css
    
    Co-authored-by: Adam Johnson <adjohnso@redhat.com>
    
    * Update elements/rh-progress-stepper/rh-progress-step.css
    
    Co-authored-by: Adam Johnson <adjohnso@redhat.com>
    
    * Update elements/rh-progress-stepper/rh-progress-step.css
    
    Co-authored-by: Adam Johnson <adjohnso@redhat.com>
    
    * Update elements/rh-progress-stepper/rh-progress-step.css
    
    Co-authored-by: Adam Johnson <adjohnso@redhat.com>
    
    * feat(progress-stepper): addressing 5 of 7 of Marionne' design feedback
    
    * style(progress-stepper): fix lint issue on --rh-color-status-disabled
    
    * fix(progress-stepper): spacing/grid  issues per Marionne
    
    * test(progress-stepper): removing unused a11ySnapshot
    
    * docs: ssr progress stepper
    
    * docs(progress-stepper): update demos, jsdoc
    
    Prefer attributes to slots
    
    * perf(progress-stepper): recompute state as needed
    
    Also improves inline documentation
    
    * fix(progress-stepper): always treat icon as an active step
    
    Also, don't reflect default "inactive" state
    
    * docs: disable ssr for progress stepper
    
    * fix(progress-stepper): make custom icon font weight medium, like active state
    
    ---------
    
    Co-authored-by: Mark Caron <mark.caron@gmail.com>
    Co-authored-by: Benny Powers <web@bennypowers.com>
    Co-authored-by: Benny Powers - עם ישראל חי! <bennypowers@users.noreply.github.com>
    Co-authored-by: Adam Johnson <adam.b.johnson1@gmail.com>
    Co-authored-by: Mark Caron <markcaron@users.noreply.github.com>
    Co-authored-by: Adam Johnson <adjohnso@redhat.com>

---
+++ elements/rh-progress-stepper/docs/10-style.md
@@ -0,0 +1,103 @@
+## Style
+
+A progress stepper is a group of at least 3 sequential steps placed in the 
+middle of a horizontal or vertical line.
+
+<figure>
+  <uxdot-example color-palette="lightest">
+    <img src="../progress-stepper-style-anatomy.svg"
+          alt="A progress stepper with annotations around the items listed below."
+          width="663"
+          height="80">
+  </uxdot-example>
+  <figcaption>
+    <ol>
+      <li>Status icon</li>
+      <li>Title</li>
+      <li>Optional description</li>
+    </ol>
+  </figcaption>
+</figure>
+
+### Status icons
+
+A progress stepper uses icons to indicate the status of a process or task. These 
+icons change as users move through each step.
+
+<uxdot-example color-palette="lightest">
+  <img src="../progress-stepper-style-status-icons.svg"
+        alt="Three compact progress steppers showing current step, warning, and error states."
+        width="560"
+        height="64">  
+</uxdot-example>
+
+
+## Color Scheme
+
+A progress stepper is available in both light and dark color schemes.
+
+### Light scheme
+
+<uxdot-example color-palette="lightest">
+  <img src="../progress-stepper-style-scheme-light.svg"
+        alt="Light scheme progress stepper showing two completed steps, a current step, and a not yet completed last step."
+        width="643"
+        height="78">  
+</uxdot-example>
+
+### Dark scheme
+
+<uxdot-example color-palette="darkest">
+  <img src="../progress-stepper-style-scheme-dark.svg"
+        alt="Dark scheme progress stepper showing two completed steps, a current step, and a not yet completed last step."
+        width="643"
+        height="78">  
+</uxdot-example>
+
+## Variants
+
+### Orientation
+
+A progress stepper is available in both horizontal and vertical orientations.
+
+<uxdot-example color-palette="lightest">
+  <img src="../progress-stepper-style-orientation.svg"
+        alt="Two progress steppers. One horizontal, one vertical. Each contain four steps."
+        width="643"
+        height="422">  
+</uxdot-example>
+
+### Size
+
+A progress stepper is available in both default and compact sizes.
+
+<uxdot-example color-palette="lightest">
+  <img src="../progress-stepper-style-size.svg"
+        alt="Three progress steppers. One horizontal with descriptions. Two other compact progress steppers, one horizontal one vertical."
+        width="643"
+        height="326">  
+</uxdot-example>
+
+### Descriptions
+
+If more description is needed, a progress stepper with descriptions can be used which allows for more context for each step.
+
+<uxdot-example color-palette="lightest">
+  <img src="../progress-stepper-style-descriptions.svg"
+        alt="Two horizontal progress steppers. One with and one without a description below each step's label."
+        width="643"
+        height="198">  
+</uxdot-example>
+
+### Space
+
+<uxdot-example color-palette="lightest">
+  <img src="../progress-stepper-style-space.svg"
+        alt="Four progress steppers showing 32px space between each step's text. There's 16px space between the icon and the label for all variants except the default horizontal stepper. For the default, there's 8px of margin bottom between the icon and the label."
+        width="643"
+        height="422">  
+</uxdot-example>
+
+## Interaction states
+
+Coming soon
---
+++ elements/rh-progress-stepper/rh-progress-step.ts
@@ -0,0 +1,180 @@
+import type { IconNameFor, IconSetName } from '@rhds/icons';
+
+import { html, LitElement } from 'lit';
+
+import { customElement } from 'lit/decorators/custom-element.js';
+import { property } from 'lit/decorators/property.js';
+import { classMap } from 'lit/directives/class-map.js';
+import { ifDefined } from 'lit/directives/if-defined.js';
+import { consume } from '@lit/context';
+
+import { compactContext, currentStepContext } from './context.js';
+
+import { observes } from '@patternfly/pfe-core/decorators.js';
+
+import { themable } from '@rhds/elements/lib/themable.js';
+
+import styles from './rh-progress-step.css';
+
+/**
+ * Available states for a progress step:
+ * - `inactive` - The step is not active
+ * - `active` - The step is currently active
+ * - `complete` - The step has been completed
+ * - `warn` - The step is in a warning state
+ * - `fail` - The step has failed
+ * - `custom` - The step uses a custom icon
+ */
+export type ProgressStepState = 'inactive' | 'active' | 'complete' | 'warn' | 'fail' | 'custom';
+
+/**
+ * Map of state names to their corresponding icon names
+ */
+const ICONS = new Map(Object.entries({
+  active: 'resources-full',
+  complete: 'check-circle-fill',
+  warn: 'error-fill',
+  fail: 'ban-fill',
+} as const));
+
+/**
+ * Fired when a step becomes active
+ */
+export class RhProgressStepChangeEvent extends Event {
+  constructor() {
+    super('change', { bubbles: true });
+  }
+}
+
+/**
+ * A progress step represents a single step in a progress stepper.
+ * Each step can have different states and may include an icon, label,
+ * and description. Steps can also be linked to URLs.
+ *
+ * @summary Single step in a progress stepper
+ *
+ * @fires { RhProgressStepChangeEvent } fired when this step becomes active
+ */
+@customElement('rh-progress-step')
+@themable
+export class RhProgressStep extends LitElement {
+  static styles = [styles];
+
+  /**
+   * Sets the state of the progress step
+   * - `inactive` - The step is not active
+   * - `active` - The step is currently active
+   * - `complete` - The step has been completed
+   * - `warn` - The step is in a warning state
+   * - `fail` - The step has failed
+   * - `custom` - The step uses a custom icon
+   */
+  @property({ reflect: true }) state?: ProgressStepState;
+
+  /**
+   * Sets the description text for the progress step
+   * Overridden by the `description` slot.
+   */
+  @property({ reflect: true }) description?: string;
+
+  /**
+   * Custom icon for the step. Overridden by the `icon` slot.
+   * When the step is in the `warn` or `fail` state, it should not have a custom
+   * icon.
+   */
+  @property() icon?: IconNameFor<IconSetName>;
+
+  /** Icon set for the `icon` property - 'ui' by default */
+  @property({ attribute: 'icon-set' }) iconSet: IconSetName = 'ui';
+
+  /**
+   * Sets a URL to make the step clickable
+   */
+  @property({ reflect: true }) href?: string;
+
+  @consume({ context: compactContext, subscribe: true })
+  private compact?: boolean;
+
+  @consume({ context: currentStepContext, subscribe: true })
+  private currentStep: RhProgressStep | null = null;
+
+  #icon: IconNameFor<IconSetName> | undefined = this.icon;
+  #iconSet: IconSetName = this.iconSet;
+
+  override connectedCallback(): void {
+    super.connectedCallback();
+    this.role = 'listitem';
+  }
+
+  render() {
+    const compact = this.compact ?? false;
+    const ariaCurrent = this.currentStep === this ? 'step' : undefined;
+    const labelSlot = html`
+      <!-- A short title for the step, which also serves as the step's accessible name -->
+      <slot></slot>
+    `;
+    return html`
+      <div id="container" class="${classMap({ compact })}">
+        <!-- summary: custom icon for the step
+             description: |
+               Overrides the \`icon\` and \`icon-set\` attributes.
+               Prefer using the \`icon\` attribute and the (default) UI Icon set.
+               Avoid slotting content here if the step is in the \`warn\` or \`fail\` state,
+               Since those states should always show their prescribed icons. -->
+        <slot name="icon">
+          <rh-icon icon="${ifDefined(this.#icon)}" set="${ifDefined(this.#iconSet)}"></rh-icon>
+        </slot>${this.href ? html`
+          <a id="label"
+             href="${this.href}"
+             aria-current="${ifDefined(ariaCurrent)}">${labelSlot}</a>` : html`
+          <strong id="label"
+                  aria-current="${ifDefined(ariaCurrent)}">${labelSlot}</strong>`}
+        <!-- summary: Elaborative description for the step
+             description: |
+               Rich HTML content can be slotted here , to override the (plain text) \`description\` attribute.
+               Avoid slotting links, images, block-level content, etc.: descriptions should be prose only. -->
+        <slot name="description" id="description">${this.description}</slot>
+      </div>
+    `;
+  }
+
+  /**
+   * Computes the icon for the step:
+   * always use the prescribed warn or fail icons
+   * otherwise, use the custom user icon,
+   * or fall back to the default active/inactive icon
+   */
+  @observes('icon')
+  @observes('iconSet')
+  @observes('state')
+  protected computeIcon() {
+    const state = this.state?.toLowerCase() as ProgressStepState;
+    if (state === 'warn') {
+      this.#icon = 'error-fill';
+    } else if (state === 'fail') {
+      this.#icon = 'ban-fill';
+    } else if (this.icon) {
+      this.#icon = this.icon;
+      if (this.iconSet) {
+        this.#iconSet = this.iconSet;
+      } else {
+        this.#iconSet = 'ui';
+      }
+    } else if (ICONS.has(state)) {
+      this.#icon = ICONS.get(state);
+    } else {
+      this.#icon = undefined;
+    }
+  }
+
+  @observes('state')
+  protected stateChanged() {
+    this.dispatchEvent(new RhProgressStepChangeEvent());
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'rh-progress-step': RhProgressStep;
+  }
+}
