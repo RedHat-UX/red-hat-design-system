e5ce174c85a1816b8300abd1f97998d49da7e682
ArathyKumar <ask6295@gmail.com>
2025-08-19 14:51:26 +0530

    chore: created rh-menu-dropdown component

---
+++ elements/rh-menu-dropdown/docs/rh-menu-dropdown.md
@@ -0,0 +1,17 @@
+{% renderOverview %}
+  <rh-menu-dropdown></rh-menu-dropdown>
+{% endrenderOverview %}
+
+{% band header="Usage" %}{% endband %}
+
+{% renderSlots %}{% endrenderSlots %}
+
+{% renderAttributes %}{% endrenderAttributes %}
+
+{% renderMethods %}{% endrenderMethods %}
+
+{% renderEvents %}{% endrenderEvents %}
+
+{% renderCssCustomProperties %}{% endrenderCssCustomProperties %}
+
+{% renderCssParts %}{% endrenderCssParts %}
---
+++ elements/rh-menu-dropdown/test/rh-menu-dropdown.spec.ts
@@ -0,0 +1,21 @@
+import { expect, html } from '@open-wc/testing';
+import { createFixture } from '@patternfly/pfe-tools/test/create-fixture.js';
+import { RhMenuDropdown } from '@rhds/elements/rh-menu-dropdown/rh-menu-dropdown.js';
+
+describe('<rh-menu-dropdown>', function() {
+  describe('simply instantiating', function() {
+    let element: RhMenuDropdown;
+    it('imperatively instantiates', function() {
+      expect(document.createElement('rh-menu-dropdown')).to.be.an.instanceof(RhMenuDropdown);
+    });
+
+    it('should upgrade', async function() {
+      element = await createFixture<RhMenuDropdown>(html`<rh-menu-dropdown></rh-menu-dropdown>`);
+      const klass = customElements.get('rh-menu-dropdown');
+      expect(element)
+        .to.be.an.instanceOf(klass)
+        .and
+        .to.be.an.instanceOf(RhMenuDropdown);
+    });
+  })
+});
