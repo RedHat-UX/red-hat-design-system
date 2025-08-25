9cd04e4fccc2e8c9d54585541dca5f43fc20e6b2
adamjohnson <adamjohnson@users.noreply.github.com>
2025-08-25 17:32:19 +0000

    deploy: ab7618354c87bcea8df15b9e1688d9a990f2267f

--- assets/packages/@uxdot/elements/uxdot-pattern.js
+++ assets/packages/@uxdot/elements/uxdot-pattern.js
@@ -1,10 +1,12 @@
-var _UxdotPattern_instances, _UxdotPattern_picked, _UxdotPattern_onExpand, _UxdotPattern_onChange;
+var _UxdotPattern_instances, _UxdotPattern_picked, _UxdotPattern_slots, _UxdotPattern_onExpand, _UxdotPattern_onChange;
 import { __classPrivateFieldGet, __classPrivateFieldSet, __decorate } from "tslib";
 import { html, isServer } from 'lit';
 import { customElement } from 'lit/decorators/custom-element.js';
 import { property } from 'lit/decorators/property.js';
 import { ifDefined } from 'lit/directives/if-defined.js';
+import { classMap } from 'lit/directives/class-map.js';
 import { SSRFailureRecoverableElement } from './ssr-failure-recoverable.js';
+import { SlotController } from '@patternfly/pfe-core/controllers/slot-controller.js';
 import { colorPalettes } from '@rhds/elements/lib/color-palettes.js';
 import { themable } from '@rhds/elements/lib/themable.js';
 import { ColorPaletteListConverter, ContextChangeEvent, paletteNames, } from '@rhds/elements/lib/elements/rh-context-picker/rh-context-picker.js';
@@ -15,7 +17,7 @@ import '@rhds/elements/rh-tabs/rh-tabs.js';
 import { TabExpandEvent } from '@rhds/elements/rh-tabs/rh-tab.js';
 import { UxdotPatternSSRController } from './uxdot-pattern-ssr-controller.js';
 import { css } from "lit";
-const styles = css `@import url("/assets/packages/@rhds/elements/elements/rh-code-block/rh-code-block-lightdom.css");:host{container:host/inline-size;display:block;margin-block-start:var(--rh-space-2xl);max-width:56rem}[hidden]{display:none!important}#container{display:grid;grid-template-columns:auto;grid-template-areas:"heading" "controls" "content" "code";gap:var(--rh-space-2xl) var(--rh-space-4xl)}@container host (width >= 530px){#container{grid-template-columns:auto auto;grid-template-areas:"heading controls" "content content" "code code"}}@container host (width >= 992px){#container{grid-template-areas:"heading heading" "controls controls" "content content" "code code"}}#heading{grid-area:heading;display:flex;align-items:center}#description{grid-area:content}::slotted(h2),::slotted(h3),::slotted(h4),::slotted(h5),::slotted(h6),h3{margin-block:0!important;font-size:var(--rh-font-size-body-text-lg)!important}#content{display:block;resize:vertical;overflow:auto;grid-area:content;padding:var(--rh-space-lg);border:var(--rh-border-width-sm) solid var(--rh-color-border-subtle);border-radius:var(--rh-border-radius-default);background-color:var(--rh-color-surface);color:var(--rh-color-text-primary);border-color:var(--rh-color-border-subtle)}@container host (width > 300px){#content{padding:var(--rh-space-xl)}}@container host (width > 540px){#content{padding:var(--rh-space-4xl)}}#content>*{margin-inline:auto}#color-picker{grid-area:controls;justify-self:flex-end;display:flex;align-items:center;gap:var(--rh-space-lg,16px)}#code-tabs{border:var(--rh-border-width-sm) solid var(--rh-color-border-subtle);border-radius:var(--rh-border-radius-default);grid-area:code;background-color:var(--rh-color-surface)}#code-tabs rh-tab-panel{padding:0;border-radius:0}#code-tabs rh-code-block{--rh-border-radius-default:0;--rh-border-width-sm:0px;border-width:0}`;
+const styles = css `@import url("/assets/packages/@rhds/elements/elements/rh-code-block/rh-code-block-lightdom.css");:host{container:host/inline-size;display:block;margin-block-start:var(--rh-space-2xl);max-width:56rem}[hidden]{display:none!important}#container{display:grid;grid-template-columns:auto;grid-template-areas:"heading" "controls" "description" "content" "code";gap:var(--rh-space-2xl) var(--rh-space-4xl)}@container host (width >= 530px){#container{grid-template-columns:auto auto;grid-template-areas:"heading controls" "description description" "content content" "code code"}}@container host (width >= 992px){#container{grid-template-areas:"heading heading" "controls controls" "description description" "content content" "code code"}}#heading{grid-area:heading;display:flex;align-items:center}#description{grid-area:description}#description ::slotted(p){margin-block:0!important}::slotted(h2),::slotted(h3),::slotted(h4),::slotted(h5),::slotted(h6),h3{margin-block:0!important;font-size:var(--rh-font-size-body-text-lg)!important}#content{display:block;resize:vertical;overflow:auto;grid-area:content;padding:var(--rh-space-lg);border:var(--rh-border-width-sm) solid var(--rh-color-border-subtle);border-radius:var(--rh-border-radius-default);background-color:var(--rh-color-surface);color:var(--rh-color-text-primary);border-color:var(--rh-color-border-subtle)}@container host (width > 300px){#content{padding:var(--rh-space-xl)}}@container host (width > 540px){#content{padding:var(--rh-space-4xl)}}#content>*{margin-inline:auto}#color-picker{grid-area:controls;justify-self:flex-end;display:flex;align-items:center;gap:var(--rh-space-lg,16px)}#code-tabs{border:var(--rh-border-width-sm) solid var(--rh-color-border-subtle);border-radius:var(--rh-border-radius-default);grid-area:code;background-color:var(--rh-color-surface)}#code-tabs rh-tab-panel{padding:0;border-radius:0}#code-tabs rh-code-block{--rh-border-radius-default:0;--rh-border-width-sm:0px;border-width:0}#description.empty~#content{grid-area:description}#description.empty~#code-tabs{grid-area:content}`;
 let UxdotPattern = class UxdotPattern extends SSRFailureRecoverableElement {
     constructor() {
         super(...arguments);
@@ -34,6 +36,7 @@ let UxdotPattern = class UxdotPattern extends SSRFailureRecoverableElement {
         this.allow = paletteNames;
         _UxdotPattern_picked.set(this, false);
         this.ssr = new UxdotPatternSSRController(this);
+        _UxdotPattern_slots.set(this, new SlotController(this, null, 'heading'));
     }
     render() {
         const { activeTab = 'html' } = this;
@@ -58,7 +61,9 @@ let UxdotPattern = class UxdotPattern extends SSRFailureRecoverableElement {
                              allow="${this.allow}"></rh-context-picker>
         </form>
 
-        <div id="description"><slot></slot></div>
+        <div id="description" class="${classMap({ empty: __classPrivateFieldGet(this, _UxdotPattern_slots, "f").isEmpty(null) })}">
+          <slot></slot>
+        </div>
 
         <rh-surface id="content">${allContent}</rh-surface>
 
@@ -100,6 +105,7 @@ let UxdotPattern = class UxdotPattern extends SSRFailureRecoverableElement {
     }
 };
 _UxdotPattern_picked = new WeakMap();
+_UxdotPattern_slots = new WeakMap();
 _UxdotPattern_instances = new WeakSet();
 _UxdotPattern_onExpand = function _UxdotPattern_onExpand(event) {
     if (!isServer && event instanceof TabExpandEvent) {
