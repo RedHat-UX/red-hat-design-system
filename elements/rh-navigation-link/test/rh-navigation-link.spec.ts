6973ed1d7984d992fc68c0d2851444c205f11b6f
Steven Spriggs <steven.spriggs@gmail.com>
2025-09-29 12:00:20 -0400

    test(navigation-link): clean up

--- elements/rh-navigation-link/test/rh-navigation-link.spec.ts
+++ elements/rh-navigation-link/test/rh-navigation-link.spec.ts
@@ -1,21 +1,21 @@
-import { expect, html } from '@open-wc/testing';
-import { createFixture } from '@patternfly/pfe-tools/test/create-fixture.js';
+import { expect, fixture, html } from '@open-wc/testing';
 import { RhNavigationLink } from '@rhds/elements/rh-navigation-link/rh-navigation-link.js';
+
+let element: RhNavigationLink;
 
 describe('<rh-navigation-link>', function() {
   describe('simply instantiating', function() {
-    let element: RhNavigationLink;
     it('imperatively instantiates', function() {
       expect(document.createElement('rh-navigation-link')).to.be.an.instanceof(RhNavigationLink);
     });
 
     it('should upgrade', async function() {
-      element = await createFixture<RhNavigationLink>(html`<rh-navigation-link></rh-navigation-link>`);
+      element = await fixture<RhNavigationLink>(html`<rh-navigation-link href="#">Link</rh-navigation-link>`);
       const klass = customElements.get('rh-navigation-link');
       expect(element)
-        .to.be.an.instanceOf(klass)
-        .and
-        .to.be.an.instanceOf(RhNavigationLink);
+          .to.be.an.instanceOf(klass)
+          .and
+          .to.be.an.instanceOf(RhNavigationLink);
     });
-  })
+  });
 });
