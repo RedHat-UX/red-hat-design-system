e5ae6bc5e8b18fba5989294bd1d64eb51345ac9e
Steven Spriggs <steven.spriggs@gmail.com>
2025-10-16 14:46:29 -0400

    chore(navigation-primary): remove orphaned code

--- elements/rh-navigation-primary/rh-navigation-primary.ts
+++ elements/rh-navigation-primary/rh-navigation-primary.ts
@@ -94,16 +94,6 @@ export class RhNavigationPrimary extends LitElement {
    */
   @property({ attribute: 'accessible-label' }) accessibleLabel = 'main';
 
-  static focusableChildElements(parent: HTMLElement): NodeListOf<HTMLElement> {
-    return parent.querySelectorAll(`a,
-                                    button:not([disabled]),
-                                    input:not([disabled]),
-                                    select:not([disabled]),
-                                    textarea:not([disabled]),
-                                    [tabindex]:not([tabindex="-1"]):not([disabled]),
-                                    details:not([disabled]),
-                                    summary:not(:disabled)`);
-  }
 
   constructor() {
     super();
