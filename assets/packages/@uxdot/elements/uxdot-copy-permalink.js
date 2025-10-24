893ef6f448a2dc01337c959386fed7c3e55897dd
bennypowers <bennypowers@users.noreply.github.com>
2025-10-24 07:11:18 +0000

    deploy: 9ccfed5b84fa857fcb18e60c5e298c933c9bf852

--- assets/packages/@uxdot/elements/uxdot-copy-permalink.js
+++ assets/packages/@uxdot/elements/uxdot-copy-permalink.js
@@ -6,7 +6,8 @@ import { property } from 'lit/decorators/property.js';
 import { RhAlert } from '@rhds/elements/rh-alert/rh-alert.js';
 import '@rhds/elements/rh-icon/rh-icon.js';
 import { css } from "lit";
-const styles = css `:host{display:flex;margin-block-end:var(--rh-space-lg);align-items:center}#signifier{display:var(--perma-signifier-display,none)}#button{height:1.75rem;background:none;border:none;border-radius:var(--rh-border-radius-default);display:none;align-items:center}#button:is(:hover,:focus){background:var(--rh-color-surface-light)}:host(:state(--rendered)) #button{display:flex}::slotted(:is(h1,h2,h3,h4,h5,h6)){display:flex;margin-block-end:0!important;align-items:center;color:inherit;text-decoration:inherit}`;
+const styles = css `:host{display:flex;margin-block-end:var(--rh-space-lg);align-items:center}#signifier{display:var(--perma-signifier-display,none)}#button{height:1.75rem;background:none;border:none;border-radius:var(--rh-border-radius-default);display:none;align-items:center}#button:is(:hover,:focus){background:var(--uxdot-copy-permalink-button-background,light-dark(var(--rh-color-surface-light),var(--rh-color-surface-dark))
+    );color:var(--rh-color-text-primary)}:host(:state(--rendered)) #button{display:flex}::slotted(:is(h1,h2,h3,h4,h5,h6)){display:flex;margin-block-end:0!important;align-items:center;color:inherit;text-decoration:inherit}`;
 let UxdotCopyPermalink = class UxdotCopyPermalink extends LitElement {
     constructor() {
         super(...arguments);
