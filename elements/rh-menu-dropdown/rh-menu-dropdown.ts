0c98dc04ac5e0b8008e300ec6927c849f82fe81e
Arathy Kumar <49025521+ArathyKumar@users.noreply.github.com>
2025-10-29 15:16:24 +0530

    Update elements/rh-menu-dropdown/rh-menu-dropdown.ts
    Co-authored-by: Adam Johnson <adjohnso@redhat.com>

--- elements/rh-menu-dropdown/rh-menu-dropdown.ts
+++ elements/rh-menu-dropdown/rh-menu-dropdown.ts
@@ -26,11 +26,8 @@ export class MenuDropdownSelectEvent extends Event {
 
 
 /**
- * The menu dropdown is a UI component made up of two parts: a menu toggle and a menu list.
- * The toggle is the element users interact with to open or close the dropdown.
- * When opened, it reveals the menu list, a compact set of options
- * that can trigger actions or navigate to different pages. It's a clean and efficient way
- * to present multiple choices without taking up much space on the screen.
+ * A menu dropdown presents a list of choices in a vertically stacked menu, appearing when
+   a user interacts with a toggle button.
  *
  * @summary A collapsible menu for presenting a list of options or actions
  *
