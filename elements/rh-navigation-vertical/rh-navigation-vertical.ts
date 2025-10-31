48fe6e28a4bd33f8662ac9b9b101dabd60de7707
Steven Spriggs <steven.spriggs@gmail.com>
2025-10-31 14:05:03 -0400

    docs(navigation-vertical): alias vertical to match primary and secondary navs

--- elements/rh-navigation-vertical/rh-navigation-vertical.ts
+++ elements/rh-navigation-vertical/rh-navigation-vertical.ts
@@ -18,7 +18,7 @@ import styles from './rh-navigation-vertical.css';
 /**
  * A vertical navigation list of top-level and grouped navigation items, typically used in a side navigation pattern.
  * @summary Vertical navigation
- * @alias navigation-vertical
+ * @alias Navigation (vertical)
  */
 @customElement('rh-navigation-vertical')
 @themable
