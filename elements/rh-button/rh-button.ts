6f69dd0bf60675077d936d4f822985a6385fabc1
Adam Johnson <adam.b.johnson1@gmail.com>
2025-11-21 15:01:40 -0500

    fix(button): check for both the `accessibleLabel` and `label` attribute in template

--- elements/rh-button/rh-button.ts
+++ elements/rh-button/rh-button.ts
@@ -44,22 +44,16 @@ export class RhButton extends LitElement {
   @property({ reflect: true }) type?: 'button' | 'submit' | 'reset';
 
   /**
-   * Accessible name for the button, use when the button does not have slotted text
+   * Accessible name for the button, use when the button does not have slotted text.
    */
   @property({ attribute: 'accessible-label' }) accessibleLabel?: string;
 
   /**
-   * @deprecated Use `accessible-label` instead
-   * This property maintains backwards compatibility for the old `label` attribute
+   * Accessible name for the button, use when the button does not have slotted text.
+   * Use `accessible-label` instead.
+   * @deprecated
    */
-  @property({ attribute: 'label' })
-  set label(value: string | undefined) {
-    this.accessibleLabel = value;
-  }
-
-  get label(): string | undefined {
-    return this.accessibleLabel;
-  }
+  @property() label?: string;
 
   /** Form value for the button */
   @property() value?: string;
@@ -116,7 +110,7 @@ export class RhButton extends LitElement {
     const hasIcon = this.#hasIcon;
     return html`
       <!-- Internal button element -->
-      <button aria-label="${ifDefined(this.accessibleLabel)}"
+      <button aria-label="${ifDefined(this.accessibleLabel || this.label)}"
               class="${classMap({
                 danger,
                 hasIcon,
@@ -138,7 +132,7 @@ export class RhButton extends LitElement {
                 part="icon"
                 name="icon">${this.#renderIcon()}</slot>
         </span>
-        <span aria-hidden=${String(!!this.accessibleLabel) as 'true' | 'false'}><!-- Contains button text --><slot id="text" ></slot></span>
+        <span aria-hidden=${String(!!this.accessibleLabel || !!this.label) as 'true' | 'false'}><!-- Contains button text --><slot id="text" ></slot></span>
       </button>
     `;
   }
