e8b69168051301d4ee06b30dad19bda2a632b1d5
HadassahYelenik <hlyelenik@gmail.com>
2025-12-02 12:58:29 +0200

    refactor: remove console warning and unused code from rh-button-group

--- elements/rh-button-group/rh-button-group.ts
+++ elements/rh-button-group/rh-button-group.ts
@@ -1,13 +1,8 @@
-import type { RhButton } from '@rhds/elements/rh-button/rh-button.js';
-
 import { LitElement, html } from 'lit';
 import { customElement } from 'lit/decorators/custom-element.js';
-import { property } from 'lit/decorators/property.js';
-import { queryAssignedElements } from 'lit/decorators/query-assigned-elements.js';
 
 import { themable } from '@rhds/elements/lib/themable.js';
 import styles from './rh-button-group.css';
-import { Logger } from '@patternfly/pfe-core/controllers/logger.js';
 import { InternalsController } from '@patternfly/pfe-core/controllers/internals-controller.js';
 
 /**
@@ -23,36 +18,13 @@ import { InternalsController } from '@patternfly/pfe-core/controllers/internals-
 export class RhButtonGroup extends LitElement {
   static readonly styles = [styles];
 
-  #logger = new Logger(this);
-
   // eslint-disable-next-line no-unused-private-class-members
   #internals = InternalsController.of(this, { role: 'group' });
-
-  @queryAssignedElements({ flatten: true })
-  private _buttons!: (HTMLButtonElement | RhButton)[];
-
-  protected override firstUpdated() {
-    if (typeof window === 'undefined') {
-      return;
-    }
-    this.#setupButtons();
-  }
-
-  #setupButtons() {
-    if (!this._buttons?.length) {
-      this.#logger.warn('rh-button-group has no slotted buttons');
-      return;
-    }
-  }
-
-  #onSlotchange() {
-    this.#setupButtons();
-  }
 
   override render() {
     return html`
       <!-- Place \`<rh-button>\` elements or native \`<button>\` elements here to organize them into a button group. -->
-      <slot @slotchange="${this.#onSlotchange}"></slot>
+      <slot></slot>
     `;
   }
 }
