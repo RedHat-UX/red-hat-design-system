428eee0859cdc2c7428b18ecbc248c5273ffe0ca
Adam Johnson <adam.b.johnson1@gmail.com>
2026-03-30 17:22:02 -0400

    style(alert): lint `rh-alert.ts`

--- elements/rh-alert/rh-alert.ts
+++ elements/rh-alert/rh-alert.ts
@@ -1,4 +1,5 @@
-import { type CSSResult, LitElement, type TemplateResult, html, isServer, render, nothing } from 'lit';
+import type { CSSResult, TemplateResult } from 'lit';
+import { LitElement, html, isServer, render, nothing } from 'lit';
 import { customElement } from 'lit/decorators/custom-element.js';
 import { property } from 'lit/decorators/property.js';
 import { repeat } from 'lit/directives/repeat.js';
