5ce08bbad9ee6012bed8ea5823be5b67a3ea2e2a
Steven Spriggs <steven.spriggs@gmail.com>
2026-05-08 10:38:24 -0400

    chore: lint

--- docs/assets/javascript/dsd-polyfill.js
+++ docs/assets/javascript/dsd-polyfill.js
@@ -1,30 +1,32 @@
-if (!("shadowRootAdoptedStyleSheets" in HTMLTemplateElement.prototype)) {
+if (!('shadowRootAdoptedStyleSheets' in HTMLTemplateElement.prototype)) {
   (function applyAdoptedStyleSheets(root) {
-    const ATTR = "shadowrootadoptedstylesheets";
+    const ATTR = 'shadowrootadoptedstylesheets';
     const sheets = new Map();
     root
-      .querySelectorAll("style[type=module][specifier]")
-      .forEach(function (style) {
+      .querySelectorAll('style[type=module][specifier]')
+      .forEach(function(style) {
         const sheet = new CSSStyleSheet();
         sheet.replaceSync(style.textContent);
-        sheets.set(style.getAttribute("specifier"), sheet);
+        sheets.set(style.getAttribute('specifier'), sheet);
       });
     (function apply(node) {
-      node.querySelectorAll("[" + ATTR + "]").forEach(function (el) {
+      node.querySelectorAll(`[${ATTR}]`).forEach(function(el) {
         if (el.shadowRoot) {
           el.shadowRoot.adoptedStyleSheets.push(
             ...el
               .getAttribute(ATTR)
               .trim()
               .split(/\s+/)
-              .flatMap(function (n) {
+              .flatMap(function(n) {
                 return sheets.get(n) ? [sheets.get(n)] : [];
               })
           );
         }
       });
-      node.querySelectorAll("*").forEach(function (el) {
-        if (el.shadowRoot) apply(el.shadowRoot);
+      node.querySelectorAll('*').forEach(function(el) {
+        if (el.shadowRoot) {
+          apply(el.shadowRoot);
+        }
       });
     })(root);
   })(document);
