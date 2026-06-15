140972b3202f446052b4af73771d82bba864af86
Adam Johnson <adam.b.johnson1@gmail.com>
2026-06-15 15:24:10 -0400

    feat(tabs): additions for unified theme tabs

--- elements/rh-tabs/rh-tab.ts
+++ elements/rh-tabs/rh-tab.ts
@@ -95,12 +95,14 @@ export class RhTab extends LitElement {
            part="button"
            ?disabled="${this.disabled}"
            class="${classMap({ active, box, vertical, first, last })}">
-        <slot name="icon"
-              part="icon">
-          <rh-icon ?hidden="${!this.icon}" icon="${ifDefined(this.icon)}" set="${ifDefined(this.iconSet)}"></rh-icon>
-        </slot>    
-        <!-- Tab title text -->
-        <slot part="text"></slot>
+        <div id="wrapper" part="wrapper">
+          <slot name="icon"
+                part="icon">
+            <rh-icon ?hidden="${!this.icon}" icon="${ifDefined(this.icon)}" set="${ifDefined(this.iconSet)}"></rh-icon>
+          </slot>
+          <!-- Tab title text -->
+          <slot part="text"></slot>
+        </div>
       </div>
     `;
   }
--- elements/rh-tabs/rh-tabs.ts
+++ elements/rh-tabs/rh-tabs.ts
@@ -174,7 +174,7 @@ export class RhTabs extends LitElement {
                   aria-label="${this.getAttribute('label-scroll-left') ?? 'Scroll left'}"
                   ?disabled="${!this.#overflow.overflowLeft}"
                   @click="${() => !this.matches(':dir(rtl)') ? this.#overflow.scrollLeft() : this.#overflow.scrollRight()}">
-            <rh-icon set="ui" icon="caret-left" loading="eager"></rh-icon>
+            <span class="overflow-icon"><rh-icon set="ui" icon="caret-left" loading="eager"></rh-icon></span>
           </button>`}
           <div id="tablist" role="tablist">
             <!--
@@ -192,7 +192,7 @@ export class RhTabs extends LitElement {
                   aria-label="${this.getAttribute('label-scroll-right') ?? 'Scroll right'}"
                   ?disabled="${!this.#overflow.overflowRight}"
                   @click="${() => !this.matches(':dir(rtl)') ? this.#overflow.scrollRight() : this.#overflow.scrollLeft()}">
-             <rh-icon set="ui" icon="caret-right" loading="eager"></rh-icon>
+            <span class="overflow-icon"><rh-icon set="ui" icon="caret-right" loading="eager"></rh-icon></span>
           </button>`}
         </div>
         <!--
