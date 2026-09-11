561beb220515198a7eae8f29aa379db7f385539f
Steven Spriggs <steven.spriggs@gmail.com>
2026-09-11 15:02:31 -0400

    chore(navigation-primary): lint

--- elements/rh-navigation-primary/rh-navigation-primary.ts
+++ elements/rh-navigation-primary/rh-navigation-primary.ts
@@ -249,9 +249,9 @@ export class RhNavigationPrimary extends LitElement {
             <div id="logo">
               <a href="${this.logoHref}">
                 <!--
-                  Accepts an inline SVG or \`<img>\` to override the default
-                  Red Hat logo.  Slotted images should be swapped on scheme
-                  change or support light-dark() such as in an inline SVG.
+                  Accepts an inline SVG or \`<img>\` to override the default Red Hat logo.
+                  Slotted images should be swapped on scheme change or support light-dark()
+                  such as in an inline SVG.
                 -->
                 <slot name="logo">
                   <svg preserveAspectRatio="xMinYMid slice" viewBox="0 0 613 145" role="img" aria-labelledby="svgtitle">
