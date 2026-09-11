2a74b491820b72335101172a44b5675e15c9db9b
Adam Johnson <adam.b.johnson1@gmail.com>
2026-09-11 13:15:17 -0400

    fix(footer): remove deprecated `--rh-footer-nojs-min-height`

--- elements/rh-footer/rh-footer.ts
+++ elements/rh-footer/rh-footer.ts
@@ -31,7 +31,6 @@ function isHeaderTagName(tagName: string) {
  *
  * @summary Site footer with navigation links, social icons, and legal content
  *
- * @cssprop --rh-footer-nojs-min-height - Minimum height when JavaScript is disabled. @deprecated target `rh-footer:not(:defined)` directly
  * @cssprop --rh-footer-icon-color - Default icon color. Uses --rh-color-gray-40 design token
  * @cssprop --rh-footer-icon-color-hover - Icon color on hover/focus. Uses --rh-color-gray-30 design token
  * @cssprop --rh-footer-border-color - Border color for section dividers. Uses --rh-color-border-subtle-on-dark design token
