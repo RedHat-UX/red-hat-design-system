00d51750f949eaa7151dc27dd77ae9b450900886
Steven Spriggs <steven.spriggs@gmail.com>
2026-09-17 13:42:03 -0400

    fix(navigation-primary): remove trailing slash

--- elements/rh-navigation-primary/rh-navigation-primary.ts
+++ elements/rh-navigation-primary/rh-navigation-primary.ts
@@ -165,7 +165,7 @@ export class RhNavigationPrimary extends LitElement {
    * unless the site requires a non-root landing page for the logo. Defaults
    * to `'/'`.
    */
-  @property({ attribute: 'logo-href' }) logoHref = 'https://www.redhat.com/en/';
+  @property({ attribute: 'logo-href' }) logoHref = 'https://www.redhat.com/en';
 
   constructor() {
     super();
